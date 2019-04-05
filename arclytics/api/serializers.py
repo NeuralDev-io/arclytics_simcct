51c3d864e94ef4035f408ec4fd790f2e96378a8d
codeninja55 <andrew@neuraldev.io>
2019-04-06 08:39:27 +1100

    Added gitignore for Django defaults

---
+++ arclytics/api/serializers.py
@@ -0,0 +1,34 @@
+# -*- coding: utf-8 -*-
+
+# ----------------------------------------------------------------------------------------------------------------------
+# arclytics_simcct_api
+# serializers.py
+# 
+# Attributions: 
+# [1] 
+# ----------------------------------------------------------------------------------------------------------------------
+
+__author__ = 'Andrew Che <@codeninja55>'
+__copyright__ = 'Copyright (C) 2019, Andrew Che <@codeninja55>'
+__credits__ = ['']
+__license__ = '{license}'
+__version__ = '{mayor}.{minor}.{rel}'
+__maintainer__ = 'Andrew Che'
+__email__ = 'andrew@neuraldev.io'
+__status__ = '{dev_status}'
+__date__ = '2019.04.06'
+
+"""serializers.py: 
+
+{Description}
+"""
+
+# Built-in/Generic Imports
+import os
+
+# Libs
+
+# Own modules
+# from {path} import {class}
+
+

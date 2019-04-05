d50492ee8c7afe5611ecbaa40c3283b6f196315a
codeninja55 <andrew@neuraldev.io>
2019-04-06 09:18:07 +1100

    Added templates for PRs and File Headers

---
+++ docs/FILE_HEADER_TEMPLATE.py
@@ -0,0 +1,46 @@
+# -*- coding: utf-8 -*-
+
+#  MIT License
+#
+#  Copyright (c) 2019, NeuralDev
+#
+#  Permission is hereby granted, free of charge, to any person obtaining a copy
+#  of this software and associated documentation files (the "Software"), to deal
+#  in the Software without restriction, including without limitation the rights
+#  to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+#  copies of the Software, and to permit persons to whom the Software is
+#  furnished to do so, subject to the following conditions:
+#
+#  The above copyright notice and this permission notice shall be included in all
+#  copies or substantial portions of the Software.
+#
+#  THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+#  IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+#  FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+#  AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+#  LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+#  OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+#  SOFTWARE.
+
+# ----------------------------------------------------------------------------------------------------------------------
+# arclytics_simcct_api
+# filename.py
+#
+# Attributions:
+# [1]
+# ----------------------------------------------------------------------------------------------------------------------
+
+__author__ = '{full name} <@{github or bitbucket username>'
+__copyright__ = 'Copyright (C) 2019, NeuralDev'
+__credits__ = ['{list of contributors by full name}']
+__license__ = '{license}'
+__version__ = '{mayor}.{minor}.{rel}'
+__maintainer__ = '{full name}'
+__email__ = '{git config email address}'
+__status__ = '{dev_status}'
+__date__ = '{date file created or updated}'
+
+"""filename.py: 
+
+{Description}
+"""

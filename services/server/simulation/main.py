378d1fcfd1416bc1321b922ba54dd6c6afae7e84
Andrew Che <andrew@codeninja55.me>
2019-06-24 21:52:13 +1000

    Adding the Ninja Logger rebranding as Arc Logger with file fixes

---
+++ services/server/simulation/main.py
@@ -0,0 +1,31 @@
+# -*- coding: utf-8 -*-
+# ----------------------------------------------------------------------------------------------------------------------
+# arclytics_sim
+# main.py
+# 
+# Attributions: 
+# [1] 
+# ----------------------------------------------------------------------------------------------------------------------
+__author__ = 'Andrew Che <@codeninja55>'
+__copyright__ = 'Copyright (C) 2019, Andrew Che <@codeninja55>'
+__credits__ = ['']
+__license__ = '{license}'
+__version__ = '{mayor}.{minor}.{rel}'
+__maintainer__ = 'Andrew Che'
+__email__ = 'andrew@neuraldev.io'
+__status__ = '{dev_status}'
+__date__ = '2019.06.24'
+
+"""main.py: 
+
+{Description}
+"""
+
+# Built-in/Generic Imports
+import os
+
+from logger.arc_logger import AppLogger
+
+if __name__ == '__main__':
+    logger = AppLogger(__name__)
+    logger.info('Simulation Start - Alpha version')

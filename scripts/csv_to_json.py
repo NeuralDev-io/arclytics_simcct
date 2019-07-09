d218086dc7ddda4837d4b7dbed4ff105215127be
Unknown <davidmatthews1004@gmail.com>
2019-07-09 21:36:27 +1000

    Fixed spelling mistake
    Fixed spelling mistake. 
    Improved argparse argument definitions.

--- scripts/csv_to_json.py
+++ scripts/csv_to_json.py
@@ -1,279 +1,165 @@
 """This script prompts the user for the name of an output file produced by
-the SSTP program and writes the file's data to a JSON file."""
+the SSTP program and writes the file's data to a JSON file.
+"""
 
 import json
+import argparse
 
-INPUT_FILE_NAME = input('Enter input file name: ')
-OUTPUT_FILE_NAME = input('Enter output file name: ')
-#INPUT_FILE_NAME = 'TTT and CCT results.csv'
-#OUTPUT_FILE_NAME = 'TTT and CCT.json'
+parser = argparse.ArgumentParser(description='Convert SSTP results CSV to JSON')
+parser.add_argument('-i', dest='input_file', type=str, metavar='',
+                    help='Name of input SSTP CSV file')
+parser.add_argument('-o', dest='output_file', type=str, metavar='',
+                    help='Name of output JSON file')
+args = parser.parse_args()
 
-with open(INPUT_FILE_NAME, 'r') as file_in:
-    #skip header information
-    for i in range(1, 11):
-        line = file_in.readline()
+def read_csv(csv_file_name: str) -> str:
+    """Read the SSTP csv file and return a list of the lines from the file."""
+    with open(csv_file_name, 'r') as fin:
+        return fin.readlines()
 
-    #Set Ae1
-    ae1 = line[5:-1]
+def create_output(lines: str) -> {}:
+    """Read a list of lines from SSTP csv and create a dictionary
+    to be dumped into json file.
+    """
+    # Create dictionary to hold output to be written to json file.
+    output = {}
 
-    #Read and Set Ae3
-    line = file_in.readline()
-    ae3 = line[5:-1]
+    # Set the values for Ae1, Ae3 ... in the dictionary
+    output['ae1'] = lines[9][5:-1]
+    output['ae3'] = lines[10][5:-1]
+    output['%nuc_at_start'] = lines[11][22:-1]
+    output['%nuc_at_finish'] = lines[12][23:-1]
+    output['astm'] = lines[14][30:-1]
+    output['diameter'] = lines[15][27:-4]
+    output['initial_cct_temp'] = lines[18][25:-3]
 
-    #Read and set nucleation at start
-    line = file_in.readline()
-    nucleation_at_start = line[22:-1]
+    # Create dictionaries for ttt nuc/comp time/temp data
+    # Initialise dictionaries as lists
+    ttt_ferrite_nuc = {}
+    ttt_ferrite_nuc['time'] = []
+    ttt_ferrite_nuc['temp'] = []
+    ttt_ferrite_comp = {}
+    ttt_ferrite_comp['time'] = []
+    ttt_ferrite_comp['temp'] = []
+    ttt_pearlite_nuc = {}
+    ttt_pearlite_nuc['time'] = []
+    ttt_pearlite_nuc['temp'] = []
+    ttt_pearlite_comp = {}
+    ttt_pearlite_comp['time'] = []
+    ttt_pearlite_comp['temp'] = []
+    ttt_bainite_nuc = {}
+    ttt_bainite_nuc['time'] = []
+    ttt_bainite_nuc['temp'] = []
+    ttt_bainite_comp = {}
+    ttt_bainite_comp['time'] = []
+    ttt_bainite_comp['temp'] = []
+    ttt_martensite_nuc = {}
+    ttt_martensite_nuc['time'] = []
+    ttt_martensite_nuc['temp'] = []
 
-    #Read and set nucleation at finish
-    line = file_in.readline()
-    nucleation_at_finish = line[23:-1]
+    # Add all time/temp data to time/temp lists within ttt dictionaries
+    line_num = 24
+    while lines[line_num][0] != ' ':
+        line = lines[line_num].split(", ")
+        ttt_ferrite_nuc['time'].append(line[0])
+        ttt_ferrite_nuc['temp'].append(line[1])
+        ttt_ferrite_comp['time'].append(line[2])
+        ttt_ferrite_comp['temp'].append(line[3])
+        ttt_pearlite_nuc['time'].append(line[4])
+        ttt_pearlite_nuc['temp'].append(line[5])
+        ttt_pearlite_comp['time'].append(line[6])
+        ttt_pearlite_comp['temp'].append(line[7])
+        ttt_bainite_nuc['time'].append(line[8])
+        ttt_bainite_nuc['temp'].append(line[9])
+        ttt_bainite_comp['time'].append(line[10])
+        ttt_bainite_comp['temp'].append(line[11])
+        ttt_martensite_nuc['time'].append(line[12])
+        ttt_martensite_nuc['temp'].append(line[13][:-1])
+        line_num += 1
 
-    #Skip blank line
-    file_in.readline()
+    # Put ttt dictionaries into main output dictionary
+    output['ttt_ferrite_nuc'] = ttt_ferrite_nuc
+    output['ttt_ferrite_comp'] = ttt_ferrite_comp
+    output['ttt_pearlite_nuc'] = ttt_pearlite_nuc
+    output['ttt_pearlite_comp'] = ttt_pearlite_comp
+    output['ttt_bainite_nuc'] = ttt_bainite_nuc
+    output['ttt_bainite_comp'] = ttt_bainite_comp
+    output['ttt_martensite_nuc'] = ttt_martensite_nuc
 
-    #Read and set ASTM median prior grain size
-    line = file_in.readline()
-    astm = line[30:-1]
+    # Create dictionaries for cct nuc/comp time/temp data
+    # Initialise dictionaries as lists
+    cct_ferrite_nuc = {}
+    cct_ferrite_nuc['time'] = []
+    cct_ferrite_nuc['temp'] = []
+    cct_ferrite_comp = {}
+    cct_ferrite_comp['time'] = []
+    cct_ferrite_comp['temp'] = []
+    cct_pearlite_nuc = {}
+    cct_pearlite_nuc['time'] = []
+    cct_pearlite_nuc['temp'] = []
+    cct_pearlite_comp = {}
+    cct_pearlite_comp['time'] = []
+    cct_pearlite_comp['temp'] = []
+    cct_bainite_nuc = {}
+    cct_bainite_nuc['time'] = []
+    cct_bainite_nuc['temp'] = []
+    cct_bainite_comp = {}
+    cct_bainite_comp['time'] = []
+    cct_bainite_comp['temp'] = []
+    cct_martensite_nuc = {}
+    cct_martensite_nuc['time'] = []
+    cct_martensite_nuc['temp'] = []
 
-    #Read and set Diameter prior grain size
-    line = file_in.readline()
-    diameter = line[27:-4]
+    # Skip cct header
+    line_num += 6
 
-    #Skip 2 blank lines
-    file_in.readline()
-    file_in.readline()
+    # Add all time/temp data to time/temp lists within cct dictionaries
+    while lines[line_num][0] != ' ' and line_num < (len(lines)-1):
+        line = lines[line_num].split(", ")
+        cct_ferrite_nuc['time'].append(line[0])
+        cct_ferrite_nuc['temp'].append(line[1])
+        cct_ferrite_comp['time'].append(line[2])
+        cct_ferrite_comp['temp'].append(line[3])
+        cct_pearlite_nuc['time'].append(line[4])
+        cct_pearlite_nuc['temp'].append(line[5])
+        cct_pearlite_comp['time'].append(line[6])
+        cct_pearlite_comp['temp'].append(line[7])
+        cct_bainite_nuc['time'].append(line[8])
+        cct_bainite_nuc['temp'].append(line[9])
+        cct_bainite_comp['time'].append(line[10])
+        cct_bainite_comp['temp'].append(line[11])
+        cct_martensite_nuc['time'].append(line[12])
+        cct_martensite_nuc['temp'].append(line[13][:-1])
+        line_num += 1
 
-    #Read and set Initial CCT temperature
-    line = file_in.readline()
-    initial_cct_temp = line[25:-3]
+    # Put cct dictionaries into main output dictionary
+    output['cct_ferrite_nuc'] = cct_ferrite_nuc
+    output['cct_ferrite_comp'] = cct_ferrite_comp
+    output['cct_pearlite_nuc'] = cct_pearlite_nuc
+    output['cct_pearlite_comp'] = cct_pearlite_comp
+    output['cct_bainite_nuc'] = cct_bainite_nuc
+    output['cct_bainite_comp'] = cct_bainite_comp
+    output['cct_martensite_nuc'] = cct_martensite_nuc
 
-    #Skip 5 lines
-    for i in range(5):
-        file_in.readline()
+    return output
 
-    #Create arrays
-    ttt_ferrite_nuc_time = []
-    ttt_ferrite_nuc_temp = []
-    ttt_ferrite_comp_time = []
-    ttt_ferrite_comp_temp = []
-    ttt_pearlite_nuc_time = []
-    ttt_pearlite_nuc_temp = []
-    ttt_pearlite_comp_time = []
-    ttt_pearlite_comp_temp = []
-    ttt_bainite_nuc_time = []
-    ttt_bainite_nuc_temp = []
-    ttt_bainite_comp_time = []
-    ttt_bainite_comp_temp = []
-    ttt_martensite_nuc_time = []
-    ttt_martensite_nuc_temp = []
+def write_json(output: {}, file_name: str):
+    """Dump data dictionary into output file in json format."""
+    with open(file_name, 'w') as fout:
+        json.dump(output, fout)
 
-    #Read time/temp data
-    line = file_in.readline()
-    while len(line) > 5:
-        line_split = line.split(', ')
-        ttt_ferrite_nuc_time.append(line_split[0])
-        ttt_ferrite_nuc_temp.append(line_split[1])
-        ttt_ferrite_comp_time.append(line_split[2])
-        ttt_ferrite_comp_temp.append(line_split[3])
-        ttt_pearlite_nuc_time.append(line_split[4])
-        ttt_pearlite_nuc_temp.append(line_split[5])
-        ttt_pearlite_comp_time.append(line_split[6])
-        ttt_pearlite_comp_temp.append(line_split[7])
-        ttt_bainite_nuc_time.append(line_split[8])
-        ttt_bainite_nuc_temp.append(line_split[9])
-        ttt_bainite_comp_time.append(line_split[10])
-        ttt_bainite_comp_temp.append(line_split[11])
-        ttt_martensite_nuc_time.append(line_split[12])
-        ttt_martensite_nuc_temp.append((line_split[13])[:-1])
-        line = file_in.readline()
+def main():
+    if args.input_file:
+        lines = read_csv(args.input_file)
+    else:
+        csv_file_name = input("Enter input file name: ")
+        lines = read_csv(csv_file_name)
+    output = create_output(lines)
+    if args.output_file:
+        write_json(output, args.output_file)
+    else:
+        json_file_name = input("Enter output file name: ")
+        write_json(output, json_file_name)
 
-    # Skip 5 lines
-    for i in range(5):
-        file_in.readline()
-
-    # Create arrays
-    cct_ferrite_nuc_time = []
-    cct_ferrite_nuc_temp = []
-    cct_ferrite_comp_time = []
-    cct_ferrite_comp_temp = []
-    cct_pearlite_nuc_time = []
-    cct_pearlite_nuc_temp = []
-    cct_pearlite_comp_time = []
-    cct_pearlite_comp_temp = []
-    cct_bainite_nuc_time = []
-    cct_bainite_nuc_temp = []
-    cct_bainite_comp_time = []
-    cct_bainite_comp_temp = []
-    cct_martensite_nuc_time = []
-    cct_martensite_nuc_temp = []
-
-    # Read time/temp data
-    line = file_in.readline()
-    while len(line) > 5:
-        line_split = line.split(', ')
-
-        cct_ferrite_nuc_time.append(line_split[0])
-        cct_ferrite_nuc_temp.append(line_split[1])
-        cct_ferrite_comp_time.append(line_split[2])
-        cct_ferrite_comp_temp.append(line_split[3])
-        cct_pearlite_nuc_time.append(line_split[4])
-        cct_pearlite_nuc_temp.append(line_split[5])
-        cct_pearlite_comp_time.append(line_split[6])
-        cct_pearlite_comp_temp.append(line_split[7])
-        cct_bainite_nuc_time.append(line_split[8])
-        cct_bainite_nuc_temp.append(line_split[9])
-        cct_bainite_comp_time.append(line_split[10])
-        cct_bainite_comp_temp.append(line_split[11])
-        cct_martensite_nuc_time.append(line_split[12])
-        cct_martensite_nuc_temp.append((line_split[13])[:-1])
-        line = file_in.readline()
-
-    file_in.close()
-
-with open(OUTPUT_FILE_NAME, 'w') as file_out:
-    file_out.write('{\n')
-
-    #Write Ae1
-    file_out.write('\t"Ae1" : ')
-    json.dump(ae1, file_out)
-    file_out.write(',\n')
-
-    #Write Ae3
-    file_out.write('\t"Ae3" : ')
-    json.dump(ae3, file_out)
-    file_out.write(',\n')
-
-    #Write %nucleation at start
-    file_out.write('\t"% nucleation at start" : ')
-    json.dump(nucleation_at_start, file_out)
-    file_out.write(',\n')
-
-    #Write %nucleation at finish
-    file_out.write('\t"% nucleation at finish" : ')
-    json.dump(nucleation_at_finish, file_out)
-    file_out.write(',\n')
-
-    #Write ASTM median prior grain size
-    file_out.write('\t"ASTM mediam prior grain size" : ')
-    json.dump(astm, file_out)
-    file_out.write(',\n')
-
-    #Write diameter prior grain size
-    file_out.write('\t"Diameter prior grain size" : ')
-    json.dump(diameter, file_out)
-    file_out.write(',\n')
-
-    #Write initial cct temp
-    file_out.write('\t"Initial CCT temperature " : ')
-    json.dump(initial_cct_temp, file_out)
-    file_out.write(',\n')
-
-    #Write TTT data
-    file_out.write('\t"TTT Data" : {\n')
-
-    #Write TTT ferrite-nuc
-    file_out.write('\t\t"Ferrite-nuc" : {\n\t\t\t"temp" : ')
-    json.dump(ttt_ferrite_nuc_temp, file_out)
-    file_out.write(',\n\t\t\t"time" : ')
-    json.dump(ttt_ferrite_nuc_time, file_out)
-    file_out.write('\n\t\t},\n')
-
-    #Write TTT ferrite-comp
-    file_out.write('\t\t"Ferrite-comp" : {\n\t\t\t"temp" : ')
-    json.dump(ttt_ferrite_comp_temp, file_out)
-    file_out.write(',\n\t\t\t"time" : ')
-    json.dump(ttt_ferrite_comp_time, file_out)
-    file_out.write('\n\t\t},\n')
-
-    #Write TTT pearlite-nuc
-    file_out.write('\t\t"Pearlite-nuc" : {\n\t\t\t"temp" : ')
-    json.dump(ttt_pearlite_nuc_temp, file_out)
-    file_out.write(',\n\t\t\t"time" : ')
-    json.dump(ttt_pearlite_nuc_time, file_out)
-    file_out.write('\n\t\t},\n')
-
-    #Write TTT pearlite-comp
-    file_out.write('\t\t"Pearlite-comp" : {\n\t\t\t"temp" : ')
-    json.dump(ttt_pearlite_comp_temp, file_out)
-    file_out.write(',\n\t\t\t"time" : ')
-    json.dump(ttt_pearlite_comp_time, file_out)
-    file_out.write('\n\t\t},\n')
-
-    #Write TTT bainite-nuc
-    file_out.write('\t\t"Bainite-nuc" : {\n\t\t\t"temp" : ')
-    json.dump(ttt_bainite_nuc_temp, file_out)
-    file_out.write(',\n\t\t\t"time" : ')
-    json.dump(ttt_bainite_nuc_time, file_out)
-    file_out.write('\n\t\t},\n')
-
-    #Write TTT bainite-comp
-    file_out.write('\t\t"Bainite-comp" : {\n\t\t\t"temp" : ')
-    json.dump(ttt_bainite_comp_temp, file_out)
-    file_out.write(',\n\t\t\t"time" : ')
-    json.dump(ttt_bainite_comp_time, file_out)
-    file_out.write('\n\t\t},\n')
-
-    #Write TTT martensite-nuc
-    file_out.write('\t\t"Martensite-nuc" : {\n\t\t\t"temp" : ')
-    json.dump(ttt_martensite_nuc_temp, file_out)
-    file_out.write(',\n\t\t\t"time" : ')
-    json.dump(ttt_martensite_nuc_time, file_out)
-    file_out.write('\n\t\t}\n')
-
-    file_out.write('\t},\n')
-
-    #Write CCT data
-    file_out.write('\t"CCT Data" : {\n')
-
-    #Write CCT ferrite-nuc
-    file_out.write('\t\t"Ferrite-nuc" : {\n\t\t\t"temp" : ')
-    json.dump(cct_ferrite_nuc_temp, file_out)
-    file_out.write(',\n\t\t\t"time" : ')
-    json.dump(cct_ferrite_nuc_time, file_out)
-    file_out.write('\n\t\t},\n')
-
-    #Write CCT ferrite-comp
-    file_out.write('\t\t"Ferrite-comp" : {\n\t\t\t"temp" : ')
-    json.dump(cct_ferrite_comp_temp, file_out)
-    file_out.write(',\n\t\t\t"time" : ')
-    json.dump(cct_ferrite_comp_time, file_out)
-    file_out.write('\n\t\t},\n')
-
-    #Write CCT pearlite-nuc
-    file_out.write('\t\t"Pearlite-nuc" : {\n\t\t\t"temp" : ')
-    json.dump(cct_pearlite_nuc_temp, file_out)
-    file_out.write(',\n\t\t\t"time" : ')
-    json.dump(cct_pearlite_nuc_time, file_out)
-    file_out.write('\n\t\t},\n')
-
-    #Write CCT pearlite-comp
-    file_out.write('\t\t"Pearlite-comp" : {\n\t\t\t"temp" : ')
-    json.dump(cct_pearlite_comp_temp, file_out)
-    file_out.write(',\n\t\t\t"time" : ')
-    json.dump(cct_pearlite_comp_time, file_out)
-    file_out.write('\n\t\t},\n')
-
-    #Write CCT bainite-nuc
-    file_out.write('\t\t"Bainite-nuc" : {\n\t\t\t"temp" : ')
-    json.dump(cct_bainite_nuc_temp, file_out)
-    file_out.write(',\n\t\t\t"time" : ')
-    json.dump(cct_bainite_nuc_time, file_out)
-    file_out.write('\n\t\t},\n')
-
-    #Write CCT bainite-comp
-    file_out.write('\t\t"Bainite-comp" : {\n\t\t\t"temp" : ')
-    json.dump(cct_bainite_comp_temp, file_out)
-    file_out.write(',\n\t\t\t"time" : ')
-    json.dump(cct_bainite_comp_time, file_out)
-    file_out.write('\n\t\t},\n')
-
-    #Write CCT martensite-nuc
-    file_out.write('\t\t"Martensite-nuc" : {\n\t\t\t"temp" : ')
-    json.dump(cct_martensite_nuc_temp, file_out)
-    file_out.write(',\n\t\t\t"time" : ')
-    json.dump(cct_martensite_nuc_time, file_out)
-    file_out.write('\n\t\t}\n')
-
-    file_out.write('\t}\n}\n')
-
-    file_out.close()
+if __name__ == '__main__':
+    main()

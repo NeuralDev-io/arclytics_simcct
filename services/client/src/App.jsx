02faa97e31aba310a5e9609b441f76c76700896e
Unknown <arvy.au@gmail.com>
2019-07-09 12:37:52 +1000

    Add TextField and Button to the form

--- services/client/src/App.jsx
+++ services/client/src/App.jsx
@@ -2,13 +2,8 @@ import React from 'react'
 import { Provider } from 'react-redux'
 import { Route, Switch } from 'react-router-dom'
 import store from './state/store'
-<<<<<<< HEAD
-import LoginPage from './views/pages/LoginPage'
-import SignupPage from './views/pages/signup/SignupPage'
-=======
 import LoginPage from './components/pages/LoginPage'
-import SignupPage from './components/pages/SignupPage'
->>>>>>> develop
+import SignupPage from './components/pages/signup/SignupPage'
 
 import './App.scss'
 
---
+++ services/client/src/components/pages/signup/SignupPage.jsx
@@ -0,0 +1,242 @@
+/**
+ * Copyright 2019, NeuralDev.
+ * All rights reserved.
+ *
+ * This source code is licensed under the MIT license found in the
+ * LICENSE file in the root directory of this repository.
+ *
+ * Text field component
+ *
+ * @version 0.0.0
+ * @author Arvy Salazar
+ * @github Xaraox
+ */
+import React, { Component } from 'react'
+import { connect } from 'react-redux'
+import { Formik } from 'formik'
+
+import Button from '../../elements/button'
+// import TextField from '../../elements/textfield/TextField';
+import TextField from '../../elements/textfield'
+
+// import styles from './SignupPage.module.scss'
+
+class SignupPage extends Component {
+  
+  // constructor(props) {
+  //   super(props)
+  //   this.state = {
+  //     value: undefined,
+  //   }
+  // }
+
+  componentDidMount = () => {
+
+  }
+
+  // handleChange = (val) => {
+  //   this.setState({
+  //     value: val,
+  //   })
+  // }
+
+  render() {
+    return (
+      //TODO: do the styles then the designss
+      <div>
+         <div>
+          <h1> ARCLYTICS </h1> 
+        </div>
+        <div>
+          <h1> Sign Up </h1> 
+        </div>
+        <Formik
+            initialValues={{ 
+              firstName:'',
+              lastName:'',
+              username:'',
+              email: '',
+              password: '',
+              passwordConfirmed: '',
+            }}
+            validate={signupValidation}
+            onSubmit={async (values, { setSubmitting, setStatus }) => {
+              setSubmitting(true)
+              const promise = new Promise((resolve, reject) => {
+                signup(values, resolve, reject)
+              })
+              promise.then(data => {
+                localStorage.setItem("auth_token", data.auth_token)
+                setSubmitting(false)
+              })
+              .catch(err => {
+                setStatus({ message: err })
+                setSubmitting(false)
+              })
+            }}
+          >
+            {({
+              values,
+              errors,
+              status,
+              touched,
+              handleBlur,
+              handleSubmit,
+              setFieldValue,
+              isSubmitting,
+              submitForm
+            }) => (
+              // FIXME: Ask about dalton about the onBlur and onChange attributes
+              <form onSubmit={handleSubmit}>
+                <div>                
+                  <TextField
+                    type="text"
+                    name="firstName"
+                    onChange={e => setFieldValue('firstName', e)}
+                    onBlur={handleBlur}
+                    value={values.firstName}
+                    placeholder="First Name"
+                    length="long"
+                  />
+                  <h6>{errors.firstName && touched.firstName && errors.firstName}</h6> 
+
+                  <TextField
+                    type="text"
+                    name="lastName"
+                    onChange={e => setFieldValue('lastName', e)}
+                    onBlur={handleBlur}
+                    value={values.lastName}
+                    placeholder="Last Name"
+                    length="long"
+                  />
+                  <h6>{errors.lastName && touched.lastName && errors.lastName}</h6> 
+                 
+                  <TextField
+                    type="text"
+                    name="username"
+                    onChange={e => setFieldValue('username', e)}
+                    onBlur={handleBlur}
+                    value={values.username}
+                    placeholder="Username"
+                    length="long"
+                  />
+                  <h6>{errors.username && touched.username && errors.username}</h6> 
+
+                  <TextField
+                    type="email"
+                    name="email"
+                    onChange={e => setFieldValue('email', e)}
+                    onBlur={handleBlur}
+                    value={values.email}
+                    placeholder="Email"
+                    length="long"
+                  />
+                  <h6>{errors.email && touched.email && errors.email}</h6>
+
+                  <TextField
+                    type="password"
+                    name="password"
+                    onChange={e => setFieldValue('password', e)}
+                    value={values.password}
+                    placeholder="Password"
+                    length="long"
+                  />
+                  <h6>{errors.password && touched.password && errors.password}</h6>
+                
+
+                  <TextField
+                    type="password" 
+                    name="passwordConfirmed"
+                    length="long"
+                    value={values.passwordConfirmed}
+                    onChange={e => setFieldValue('passwordConfirmed', e)}
+                    placeholder="Confirm password"
+                  />
+                  <h6>{status && status.message && status.message}</h6>
+
+                </div>
+                <Button name="SIGN UP" appearance="default" type="submit" length="small" disabled={isSubmitting}> SIGNUP </Button>
+              </form>
+            )}
+          </Formik>
+        <div>
+          <h6> Already have an account ?  </h6>
+        </div>
+      </div>
+    )
+  }
+}
+
+const mapStateToProps = state => ({
+
+})
+
+const mapDispatchToProps = {
+
+}
+
+export default connect(mapStateToProps, mapDispatchToProps)(SignupPage)
+
+
+//TODO: Move this function for Validation and import it. Talk to matt 
+export const signupValidation = values => {
+  //TODO: Add validation to first name and last name 
+  const {
+    email, username, password, passwordConfirmed
+  } = values
+  let errors = {}
+
+  if (!username) {
+    errors.username = 'Required'
+  }
+  
+  if (!email) {
+    errors.email = 'Required'
+  } else if (
+    !/^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,}$/i.test(values.email)
+  ) {
+    errors.email = 'Invalid email'
+  }
+
+  if (!password) {
+    errors.password = 'Required'
+  } else if (password.length < 6 || password.length > 254) {
+    errors.password = 'Password must be'
+  }
+  if (!passwordConfirmed) {
+    errors.passwordConfirmed = 'Required'
+  } else if (password !== passwordConfirmed) {
+    errors.passwordConfirmed = 'Password does not match'
+  }
+
+  return errors
+}
+
+
+//TODO: Move this function to a separate file for Authentication and import it. Talk to Matt
+export const signup = async (values, resolve, reject) => {
+  const { email, username, password, firstName, lastName } = values
+  fetch('http://localhost:8000/auth/register', {
+    method: 'POST',
+    mode: 'cors',
+    headers: {
+      "content-Type": "application/json"
+    },
+    body: JSON.stringify({
+      email,
+      username,
+      password,
+      first_name: firstName,
+      last_name: lastName
+    })
+  })
+  .then(res => {
+    const jsonResponse = res.json() //TODO: for debug only remove later
+    console.log(jsonResponse) 
+    if (res.status === 200)
+      resolve(res.json())
+    else if (res.status === 400)
+      reject("Error")
+  })
+  .catch(err => console.log(err))
+}

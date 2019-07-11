39fce36b8e88efc9d69b0cb8d112923b2972476a
Unknown <arvy.au@gmail.com>
2019-07-11 13:29:26 +1000

    Added some css
    - Most of the form css is done but might need to fix the error messages

--- services/client/src/components/pages/signup/SignupPage.jsx
+++ services/client/src/components/pages/signup/SignupPage.jsx
@@ -19,7 +19,7 @@ import Button from '../../elements/button'
 // import TextField from '../../elements/textfield/TextField';
 import TextField from '../../elements/textfield'
 
-// import styles from './SignupPage.module.scss'
+import styles from './SignupPage.module.scss'
 
 class SignupPage extends Component {
   
@@ -43,125 +43,132 @@ class SignupPage extends Component {
   render() {
     return (
       //TODO: do the styles then the designss
-      <div>
-         <div>
-          <h1> ARCLYTICS </h1> 
+      <div className={styles.outer}>
+        <div className={styles.form}>
+          <div className={styles.logo}> <h3> ARCLYTICS </h3> </div>
+          <div className={styles.signUp}> <h3> Sign Up </h3> </div>
+          <Formik
+              initialValues={{ 
+                firstName:'',
+                lastName:'',
+                username:'',
+                email: '',
+                password: '',
+                passwordConfirmed: '',
+              }}
+              validate={signupValidation}
+              onSubmit={async (values, { setSubmitting, setStatus }) => {
+                setSubmitting(true)
+                const promise = new Promise((resolve, reject) => {
+                  signup(values, resolve, reject)
+                })
+                promise.then(data => {
+                  localStorage.setItem("auth_token", data.auth_token)
+                  setSubmitting(false)
+                })
+                .catch(err => {
+                  setStatus({ message: err })
+                  setSubmitting(false)
+                })
+              }}
+            >
+              {({
+                values,
+                errors,
+                status,
+                touched,
+                handleBlur,
+                handleSubmit,
+                setFieldValue,
+                isSubmitting,
+                submitForm
+              }) => (
+                <form onSubmit={handleSubmit}>
+                  <div>
+                    {/* //FIXME: need to add wrapping divs around matching inputs and headings */}
+                    <div className={styles.name}>             
+                      <TextField
+                        type="text"
+                        name="firstName"
+                        onChange={e => setFieldValue('firstName', e)}
+                        onBlur={handleBlur}
+                        value={values.firstName}
+                        placeholder="First Name"
+                        length="long"
+                        className={styles.firstName}
+                      />
+                      <h6>{errors.firstName && touched.firstName && errors.firstName}</h6> 
+                      
+                      <TextField
+                        type="text"
+                        name="lastName"
+                        onChange={e => setFieldValue('lastName', e)}
+                        onBlur={handleBlur}
+                        value={values.lastName}
+                        placeholder="Last Name"
+                        length="long"
+                        className={styles.lastName}
+                      />
+                      <h6>{errors.lastName && touched.lastName && errors.lastName}</h6> 
+                    </div>
+                    <TextField
+                      type="text"
+                      name="username"
+                      onChange={e => setFieldValue('username', e)}
+                      onBlur={handleBlur}
+                      value={values.username}
+                      placeholder="Username"
+                      length="stretch"
+                      className = {styles.emailPassword}
+                    />
+                    <h6>{errors.username && touched.username && errors.username}</h6> 
+
+                    <TextField
+                      type="email"
+                      name="email"
+                      onChange={e => setFieldValue('email', e)}
+                      onBlur={handleBlur}
+                      value={values.email}
+                      placeholder="Email"
+                      length="stretch"
+                      className = {styles.emailPassword}
+                    />
+                    <h6>{errors.email && touched.email && errors.email}</h6>
+
+                    <TextField
+                      type="password"
+                      name="password"
+                      onChange={e => setFieldValue('password', e)}
+                      value={values.password}
+                      placeholder="Password"
+                      length="stretch"
+                      className = {styles.emailPassword}
+                    />
+                    <h6>{errors.password && touched.password && errors.password}</h6>
+                  
+                    <TextField
+                      type="password" 
+                      name="passwordConfirmed"
+                      length="stretch"
+                      value={values.passwordConfirmed}
+                      onChange={e => setFieldValue('passwordConfirmed', e)}
+                      placeholder="Confirm password"
+                      className = {styles.passwordConfirmed}
+                    />
+                    <h6>{status && status.message && status.message}</h6>
+
+                  </div>
+                  <div className={styles.signUpButton}>
+                    <Button name="SIGN UP" appearance="default" type="submit" length="small" disabled={isSubmitting}> SIGNUP </Button>
+                  </div>
+                </form>
+              )}
+            </Formik>
+          <div>
+            <h6> Already have an account? <a href="localhost:signin" className={styles.signIn}> Sign in </a> </h6>
+          </div>
         </div>
-        <div>
-          <h1> Sign Up </h1> 
-        </div>
-        <Formik
-            initialValues={{ 
-              firstName:'',
-              lastName:'',
-              username:'',
-              email: '',
-              password: '',
-              passwordConfirmed: '',
-            }}
-            validate={signupValidation}
-            onSubmit={async (values, { setSubmitting, setStatus }) => {
-              setSubmitting(true)
-              const promise = new Promise((resolve, reject) => {
-                signup(values, resolve, reject)
-              })
-              promise.then(data => {
-                localStorage.setItem("auth_token", data.auth_token)
-                setSubmitting(false)
-              })
-              .catch(err => {
-                setStatus({ message: err })
-                setSubmitting(false)
-              })
-            }}
-          >
-            {({
-              values,
-              errors,
-              status,
-              touched,
-              handleBlur,
-              handleSubmit,
-              setFieldValue,
-              isSubmitting,
-              submitForm
-            }) => (
-              // FIXME: Ask about dalton about the onBlur and onChange attributes
-              <form onSubmit={handleSubmit}>
-                <div>                
-                  <TextField
-                    type="text"
-                    name="firstName"
-                    onChange={e => setFieldValue('firstName', e)}
-                    onBlur={handleBlur}
-                    value={values.firstName}
-                    placeholder="First Name"
-                    length="long"
-                  />
-                  <h6>{errors.firstName && touched.firstName && errors.firstName}</h6> 
-
-                  <TextField
-                    type="text"
-                    name="lastName"
-                    onChange={e => setFieldValue('lastName', e)}
-                    onBlur={handleBlur}
-                    value={values.lastName}
-                    placeholder="Last Name"
-                    length="long"
-                  />
-                  <h6>{errors.lastName && touched.lastName && errors.lastName}</h6> 
-                 
-                  <TextField
-                    type="text"
-                    name="username"
-                    onChange={e => setFieldValue('username', e)}
-                    onBlur={handleBlur}
-                    value={values.username}
-                    placeholder="Username"
-                    length="long"
-                  />
-                  <h6>{errors.username && touched.username && errors.username}</h6> 
-
-                  <TextField
-                    type="email"
-                    name="email"
-                    onChange={e => setFieldValue('email', e)}
-                    onBlur={handleBlur}
-                    value={values.email}
-                    placeholder="Email"
-                    length="long"
-                  />
-                  <h6>{errors.email && touched.email && errors.email}</h6>
-
-                  <TextField
-                    type="password"
-                    name="password"
-                    onChange={e => setFieldValue('password', e)}
-                    value={values.password}
-                    placeholder="Password"
-                    length="long"
-                  />
-                  <h6>{errors.password && touched.password && errors.password}</h6>
-                
-
-                  <TextField
-                    type="password" 
-                    name="passwordConfirmed"
-                    length="long"
-                    value={values.passwordConfirmed}
-                    onChange={e => setFieldValue('passwordConfirmed', e)}
-                    placeholder="Confirm password"
-                  />
-                  <h6>{status && status.message && status.message}</h6>
-
-                </div>
-                <Button name="SIGN UP" appearance="default" type="submit" length="small" disabled={isSubmitting}> SIGNUP </Button>
-              </form>
-            )}
-          </Formik>
-        <div>
-          <h6> Already have an account ?  </h6>
-        </div>
+       
       </div>
     )
   }

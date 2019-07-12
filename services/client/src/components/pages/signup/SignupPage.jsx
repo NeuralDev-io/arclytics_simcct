2c8fc9a7f10f55799f7550d14e1926404f2b05f2
Unknown <arvy.au@gmail.com>
2019-07-12 13:05:01 +1000

    Fixed Promise bug for registration and some css

--- services/client/src/components/pages/signup/SignupPage.jsx
+++ services/client/src/components/pages/signup/SignupPage.jsx
@@ -21,6 +21,8 @@ import TextField from '../../elements/textfield'
 
 import styles from './SignupPage.module.scss'
 
+// import background from '../../../assets/background_img.png'
+
 class SignupPage extends Component {
   
   // constructor(props) {
@@ -42,10 +44,10 @@ class SignupPage extends Component {
 
   render() {
     return (
-      //TODO: do the styles then the designss
+      //(arvy@neuraldev.io -- sprint 6 or Nov 2019) TODO: do the styles then the designss
       <div className={styles.outer}>
         <div className={styles.form}>
-          <div className={styles.logo}> <h3> ARCLYTICS </h3> </div>
+          <div className={styles.logo}><h3> ARCLYTICS </h3> </div>
           <div className={styles.signUp}> <h3> Sign Up </h3> </div>
           <Formik
               initialValues={{ 
@@ -63,7 +65,7 @@ class SignupPage extends Component {
                   signup(values, resolve, reject)
                 })
                 promise.then(data => {
-                  localStorage.setItem("auth_token", data.auth_token)
+                  localStorage.setItem('token', data.token)
                   setSubmitting(false)
                 })
                 .catch(err => {
@@ -85,78 +87,71 @@ class SignupPage extends Component {
               }) => (
                 <form onSubmit={handleSubmit}>
                   <div>
-                    {/* //FIXME: need to add wrapping divs around matching inputs and headings */}
-                    <div className={styles.name}>             
-                      <TextField
-                        type="text"
-                        name="firstName"
-                        onChange={e => setFieldValue('firstName', e)}
-                        onBlur={handleBlur}
-                        value={values.firstName}
-                        placeholder="First Name"
-                        length="long"
-                        className={styles.firstName}
-                      />
-                      <h6>{errors.firstName && touched.firstName && errors.firstName}</h6> 
-                      
-                      <TextField
-                        type="text"
-                        name="lastName"
-                        onChange={e => setFieldValue('lastName', e)}
-                        onBlur={handleBlur}
-                        value={values.lastName}
-                        placeholder="Last Name"
-                        length="long"
-                        className={styles.lastName}
-                      />
-                      <h6>{errors.lastName && touched.lastName && errors.lastName}</h6> 
-                    </div>
-                    <TextField
-                      type="text"
-                      name="username"
-                      onChange={e => setFieldValue('username', e)}
-                      onBlur={handleBlur}
-                      value={values.username}
-                      placeholder="Username"
-                      length="stretch"
-                      className = {styles.emailPassword}
-                    />
-                    <h6>{errors.username && touched.username && errors.username}</h6> 
-
-                    <TextField
-                      type="email"
-                      name="email"
-                      onChange={e => setFieldValue('email', e)}
-                      onBlur={handleBlur}
-                      value={values.email}
-                      placeholder="Email"
-                      length="stretch"
-                      className = {styles.emailPassword}
-                    />
-                    <h6>{errors.email && touched.email && errors.email}</h6>
-
-                    <TextField
-                      type="password"
-                      name="password"
-                      onChange={e => setFieldValue('password', e)}
-                      value={values.password}
-                      placeholder="Password"
-                      length="stretch"
-                      className = {styles.emailPassword}
-                    />
-                    <h6>{errors.password && touched.password && errors.password}</h6>
+                    <div className={styles.name}>
+                      <div  className={styles.firstName} >
+                        <TextField
+                          type="text"
+                          name="firstName"
+                          onChange={e => setFieldValue('firstName', e)}
+                          onBlur={handleBlur}
+                          value={values.firstName}
+                          placeholder="First Name"
+                          length="long"
+                         
+                        />
+                        <h6 className={styles.errors}>{errors.firstName && touched.firstName && errors.firstName}</h6>             
                   
+                      </div> 
+                      <div className={styles.lastName}>
+                        <TextField
+                          type="text"
+                          name="lastName"
+                          onChange={e => setFieldValue('lastName', e)}
+                          onBlur={handleBlur}
+                          value={values.lastName}
+                          placeholder="Last Name"
+                          length="long"
+                        />
+                        <h6 className={styles.errors}>{errors.lastName && touched.lastName && errors.lastName}</h6> 
+                      </div> 
+                 
+                    </div>
+                    <div className = {styles.emailPassword}>
+                      <TextField
+                        type="email"
+                        name="email"
+                        onChange={e => setFieldValue('email', e)}
+                        onBlur={handleBlur}
+                        value={values.email}
+                        placeholder="Email"
+                        length="stretch"
+                      />
+                      <h6 className={styles.errors}>{errors.email && touched.email && errors.email}</h6>
+                    </div>
+                    
+                    <div className={styles.emailPassword}>
                     <TextField
-                      type="password" 
-                      name="passwordConfirmed"
-                      length="stretch"
-                      value={values.passwordConfirmed}
-                      onChange={e => setFieldValue('passwordConfirmed', e)}
-                      placeholder="Confirm password"
-                      className = {styles.passwordConfirmed}
-                    />
-                    <h6>{status && status.message && status.message}</h6>
+                        type="password"
+                        name="password"
+                        onChange={e => setFieldValue('password', e)}
+                        value={values.password}
+                        placeholder="Password"
+                        length="stretch"
+                      />
+                      <h6 className={styles.errors}>{errors.password && touched.password && errors.password}</h6>
+                    </div>
 
+                    <div className={styles.passwordConfirmed}>
+                      <TextField
+                          type="password" 
+                          name="passwordConfirmed"
+                          length="stretch"
+                          value={values.passwordConfirmed}
+                          onChange={e => setFieldValue('passwordConfirmed', e)}
+                          placeholder="Confirm password"
+                        />
+                        <h6 className={styles.errors}>{errors.passwordConfirmed && touched.passwordConfirmed && errors.passwordConfirmed}</h6>
+                    </div> 
                   </div>
                   <div className={styles.signUpButton}>
                     <Button name="SIGN UP" appearance="default" type="submit" length="small" disabled={isSubmitting}> SIGNUP </Button>
@@ -168,7 +163,6 @@ class SignupPage extends Component {
             <h6> Already have an account? <a href="localhost:signin" className={styles.signIn}> Sign in </a> </h6>
           </div>
         </div>
-       
       </div>
     )
   }
@@ -185,16 +179,18 @@ const mapDispatchToProps = {
 export default connect(mapStateToProps, mapDispatchToProps)(SignupPage)
 
 
-//TODO: Move this function for Validation and import it. Talk to matt 
+//TODO: Move this function for Validation and import it.
 export const signupValidation = values => {
-  //TODO: Add validation to first name and last name 
   const {
-    email, username, password, passwordConfirmed
+    email, firstName, lastName, password, passwordConfirmed
   } = values
   let errors = {}
 
-  if (!username) {
-    errors.username = 'Required'
+  if (!firstName) {
+    errors.firstName = 'Required'
+  }
+  if (!lastName) {
+    errors.lastName = 'Required'
   }
   
   if (!email) {
@@ -208,8 +204,9 @@ export const signupValidation = values => {
   if (!password) {
     errors.password = 'Required'
   } else if (password.length < 6 || password.length > 254) {
-    errors.password = 'Password must be'
+    errors.password = 'Password must be more than 6 characters'
   }
+
   if (!passwordConfirmed) {
     errors.passwordConfirmed = 'Required'
   } else if (password !== passwordConfirmed) {
@@ -241,7 +238,7 @@ export const signup = async (values, resolve, reject) => {
     const jsonResponse = res.json() //TODO: for debug only remove later
     console.log(jsonResponse) 
     if (res.status === 200)
-      resolve(res.json())
+      resolve(jsonResponse)
     else if (res.status === 400)
       reject("Error")
   })

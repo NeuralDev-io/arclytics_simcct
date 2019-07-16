463a69b949e45b774733fb27b42f9a95a9783dd8
Unknown <arvy.au@gmail.com>
2019-07-17 02:51:39 +1000

    Fix based on the comment
    - Add vscode to git ignore
    - Fix lint-fix errors 
    - Change headings to h3
    - Fix vertical spacing
    - Chang scss files indents to 2 spaces

--- services/client/src/components/pages/signup/SignupPage.jsx
+++ services/client/src/components/pages/signup/SignupPage.jsx
@@ -17,148 +17,169 @@ import TextField from '../../elements/textfield'
 import styles from './SignupPage.module.scss'
 
 class SignupPage extends Component {
-
   componentDidMount = () => {
-    if (localStorage.getItem("token"))
-      this.props.history.push('/')
+    if (localStorage.getItem('token')) this.props.history.push('/')
   }
 
 
   render() {
     return (
       <div className={styles.outer}>
-        <div className={styles.form}> 
+        <div className={styles.form}>
           <div className={styles.logoContainer}>
-            <Logo className={styles.logo}/>
-            <h3> ARCLYTICS </h3> 
+            <Logo className={styles.logo} />
+            <h3> ARCLYTICS </h3>
           </div>
-          <div className={styles.signUp}> <h3> Sign Up </h3> </div>
+          <div className={styles.signUp}>
+            {' '}
+            <h3> Sign up </h3>
+            {' '}
+          </div>
           <Formik
-              initialValues={{ 
-                firstName:'',
-                lastName:'',
-                username:'',
-                email: '',
-                password: '',
-                passwordConfirmed: '',
-              }}
-              validate={signupValidation}
-              onSubmit={async (values, { setSubmitting, setErrors,  setStatus }) => {
-                setSubmitting(true)
-                const promise = new Promise((resolve, reject) => {
-                  signup(values, resolve, reject)
-                })
-                promise.then(data => {
-                  //If response is successful 
-                  localStorage.setItem('token', data.token)
-                  this.props.history.push('/')
-                  setSubmitting(false)
-                })
-                .catch(err => {
-                  //If response is unsuccessful
-                  if (err === "This user already exists."){
+            initialValues={{
+              firstName: '',
+              lastName: '',
+              username: '',
+              email: '',
+              password: '',
+              passwordConfirmed: '',
+            }}
+            validate={signupValidation}
+            onSubmit={async (values, { setSubmitting, setErrors }) => {
+              setSubmitting(true)
+              const promise = new Promise((resolve, reject) => {
+                signup(values, resolve, reject)
+              })
+              promise.then((data) => {
+                // If response is successful
+                localStorage.setItem('token', data.token)
+                this.props.history.push('/')
+                setSubmitting(false)
+              })
+                .catch((err) => {
+                  // If response is unsuccessful
+                  if (err === 'This user already exists.') {
                     setErrors({
-                      email: "This email already exists"
+                      email: 'This email already exists',
                     })
                   } else {
                     setErrors({
-                      firstName: "Invalid first name",
-                      lastName: "Invalid last name",
-                      email: "Invalid email",
-                      password: "Password must contain at least 6 characters",
-                      confirmPassword: "Passwords do not match"
+                      firstName: 'Invalid first name',
+                      lastName: 'Invalid last name',
+                      email: 'Invalid email',
+                      password: 'Password must contain at least 6 characters',
+                      confirmPassword: 'Passwords do not match',
                     })
-                  }   
+                  }
                   setSubmitting(false)
                 })
-              }}
-            >
-              {({
-                values,
-                errors,
-                status,
-                touched,
-                handleBlur,
-                handleSubmit,
-                setFieldValue,
-                isSubmitting
-              }) => (
-                <form onSubmit={handleSubmit}>
-                  <div>
-                    <div className={styles.name}>
-                      <div  className={styles.firstName} >
-                        <TextField
-                          type="text"
-                          name="firstName"
-                          onChange={e => setFieldValue('firstName', e)}
-                          onBlur={handleBlur}
-                          value={values.firstName}
-                          placeholder="First Name"
-                          length="long"
-                        />
-                        <h6 className={styles.errors}>{errors.firstName && touched.firstName && errors.firstName}</h6>             
-                  
-                      </div> 
-                      <div className={styles.lastName}>
-                        <TextField
-                          type="text"
-                          name="lastName"
-                          onChange={e => setFieldValue('lastName', e)}
-                          onBlur={handleBlur}
-                          value={values.lastName}
-                          placeholder="Last Name"
-                          length="long"
-                        />
-                        <h6 className={styles.errors}>{errors.lastName && touched.lastName && errors.lastName}</h6> 
-                      </div> 
-                 
-                    </div>
-                    <div className = {styles.emailPassword}>
+            }}
+          >
+            {({
+              values,
+              errors,
+              touched,
+              handleBlur,
+              handleSubmit,
+              setFieldValue,
+              isSubmitting,
+            }) => (
+              <form onSubmit={handleSubmit}>
+                <div>
+                  <div className={styles.name}>
+                    <div className={styles.firstName}>
                       <TextField
-                        type="email"
-                        name="email"
-                        onChange={e => setFieldValue('email', e)}
+                        type="text"
+                        name="firstName"
+                        onChange={e => setFieldValue('firstName', e)}
                         onBlur={handleBlur}
-                        value={values.email}
-                        placeholder="Email"
-                        length="stretch"
-
+                        value={values.firstName}
+                        placeholder="First Name"
+                        length="long"
                       />
-                      <h6 className={styles.errors}>{errors.email && touched.email && errors.email}</h6>
-                    </div>
-                    
-                    <div className={styles.emailPassword}>
-                    <TextField
-                        type="password"
-                        name="password"
-                        onChange={e => setFieldValue('password', e)}
-                        value={values.password}
-                        placeholder="Password"
-                        length="stretch"
-                      />
-                      <h6 className={styles.errors}>{errors.password && touched.password && errors.password}</h6>
-                    </div>
+                      <h6 className={styles.errors}>
+                        {errors.firstName && touched.firstName && errors.firstName}
+                      </h6>
 
-                    <div className={styles.passwordConfirmed}>
+                    </div>
+                    <div className={styles.lastName}>
                       <TextField
-                          type="password" 
-                          name="passwordConfirmed"
-                          length="stretch"
-                          value={values.passwordConfirmed}
-                          onChange={e => setFieldValue('passwordConfirmed', e)}
-                          placeholder="Confirm password"
-                        />
-                        <h6 className={styles.errors}>{errors.passwordConfirmed && touched.passwordConfirmed && errors.passwordConfirmed}</h6>
-                    </div> 
+                        type="text"
+                        name="lastName"
+                        onChange={e => setFieldValue('lastName', e)}
+                        onBlur={handleBlur}
+                        value={values.lastName}
+                        placeholder="Last Name"
+                        length="long"
+                      />
+                      <h6 className={styles.errors}>
+                        {errors.lastName && touched.lastName && errors.lastName}
+                      </h6>
+                    </div>
+
                   </div>
+                  <div className={styles.emailPassword}>
+                    <TextField
+                      type="email"
+                      name="email"
+                      onChange={e => setFieldValue('email', e)}
+                      onBlur={handleBlur}
+                      value={values.email}
+                      placeholder="Email"
+                      length="stretch"
+                    />
+                    <h6 className={styles.errors}>
+                      {errors.email && touched.email && errors.email}
+                    </h6>
+                  </div>
+
+                  <div className={styles.emailPassword}>
+                    <TextField
+                      type="password"
+                      name="password"
+                      onChange={e => setFieldValue('password', e)}
+                      value={values.password}
+                      placeholder="Password"
+                      length="stretch"
+                    />
+                    <h6 className={styles.errors}>
+                      {errors.password && touched.password && errors.password}
+                    </h6>
+                  </div>
+
+                  <div className={styles.passwordConfirmed}>
+                    <TextField
+                      type="password"
+                      name="passwordConfirmed"
+                      length="stretch"
+                      value={values.passwordConfirmed}
+                      onChange={e => setFieldValue('passwordConfirmed', e)}
+                      placeholder="Confirm password"
+                    />
+                    <h6 className={styles.errors}>
+                      {
+                        errors.passwordConfirmed
+                        && touched.passwordConfirmed
+                        && errors.passwordConfirmed
+                      }
+                    </h6>
+                  </div>
+           
                   <div className={styles.signUpButton}>
                     <Button name="SIGN UP" appearance="default" type="submit" length="small" disabled={isSubmitting}> SIGNUP </Button>
                   </div>
-                </form>
-              )}
-            </Formik>
+                </div>
+              </form>
+            )}
+          </Formik>
           <div>
-            <h6> Already have an account? <a href="http://localhost:3000/signin" className={styles.signIn}> Sign in </a> </h6>
+            <h6>
+              {' '}
+Already have an account?
+              <a href="http://localhost:3000/signin" className={styles.signIn}> Sign in </a>
+              {' '}
+
+            </h6>
           </div>
         </div>
       </div>
@@ -175,6 +196,3 @@ const mapDispatchToProps = {
 }
 
 export default connect(mapStateToProps, mapDispatchToProps)(SignupPage)
-
-
-
--- services/client/src/utils/AuthenticationHelper.js
+++ services/client/src/utils/AuthenticationHelper.js
@@ -17,46 +17,46 @@ export const login = async (values, resolve, reject) => {
     method: 'POST',
     mode: 'cors',
     headers: {
-      "content-Type": "application/json"
+      'content-Type': 'application/json',
     },
-    body: JSON.stringify(values)
+    body: JSON.stringify(values),
   })
-  .then(res => {
-    if (res.status === 200){
-      resolve(res.json())
-    }
-    else if (res.status === 404){
-      //return an error message as string
-      res.json().then(object => reject(object.message))
-    }
-  })
-  .catch(err => console.log(err))
-}
-
-
-export const signup = async (values, resolve, reject) => {
-    const { email, password, firstName, lastName } = values
-    fetch('http://localhost:8000/auth/register', {
-      method: 'POST',
-      mode: 'cors',
-      headers: {
-        "content-Type": "application/json"
-      },
-      body: JSON.stringify({
-        email,
-        password,
-        first_name: firstName,
-        last_name: lastName
-      })
-    })
-    .then(res => {
-      if (res.status === 201){
+    .then((res) => {
+      if (res.status === 200) {
         resolve(res.json())
-      }
-      else if (res.status === 400){
-        //return an error message as string
+      } else if (res.status === 404) {
+      // return an error message as string
         res.json().then(object => reject(object.message))
       }
     })
     .catch(err => console.log(err))
-  }
+}
+
+
+export const signup = async (values, resolve, reject) => {
+  const {
+    email, password, firstName, lastName,
+  } = values
+  fetch('http://localhost:8000/auth/register', {
+    method: 'POST',
+    mode: 'cors',
+    headers: {
+      'content-Type': 'application/json',
+    },
+    body: JSON.stringify({
+      email,
+      password,
+      first_name: firstName,
+      last_name: lastName,
+    }),
+  })
+    .then((res) => {
+      if (res.status === 201) {
+        resolve(res.json())
+      } else if (res.status === 400) {
+        // return an error message as string
+        res.json().then(object => reject(object.message))
+      }
+    })
+    .catch(err => console.log(err))
+}

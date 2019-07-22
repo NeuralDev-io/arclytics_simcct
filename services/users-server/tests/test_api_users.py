75702b0d7aac24a9b7c3d9676c3c3391ee26b6e9
David <davidmatthews1004@gmail.com>
2019-07-22 18:03:19 +1000

    Added update and view user profile endpoints and tests.

--- services/users-server/tests/test_api_users.py
+++ services/users-server/tests/test_api_users.py
@@ -22,6 +22,7 @@ This script will run all tests on the Users endpoints.
 
 import json
 import unittest
+import time
 
 from bson import ObjectId
 from flask import current_app
@@ -310,6 +311,361 @@ class TestUserService(BaseTestCase):
             self.assertFalse(data['data']['users'][1]['admin'])
             self.assertIn('success', data['status'])
 
+    def test_update_user_profile(self):
+        """
+        Ensure a user profile can be updated if all fields are provided.
+        """
+        luke = User(
+            email='luke@skywalker.io',
+            first_name='Luke',
+            last_name='Skywalker'
+        )
+        luke.set_password('NeverJoinYou')
+        luke.save()
+
+        with self.client:
+            resp_login = self.client.post(
+                '/auth/login',
+                data=json.dumps(
+                    {
+                        'email': 'luke@skywalker.io',
+                        'password': 'NeverJoinYou'
+                    }
+                ),
+                content_type='application/json'
+            )
+            token = json.loads(resp_login.data.decode())['token']
+
+            resp = self.client.post(
+                '/user/updateprofile',
+                data=json.dumps(
+                    {
+                        'aim' : 'Redeem my father.',
+                        'highest_education' : 'Graduated Dagobah Highschool.',
+                        'sci_text_exp' : 'Limited.',
+                        'phase_transform_exp' : 'Limited'
+                    }
+                ),
+                headers={'Authorization': 'Bearer {}'.format(token)},
+                content_type = 'application/json'
+            )
+
+            data = json.loads(resp.data.decode())
+            self.assertEqual(resp.status_code, 200)
+            self.assertEqual(data['status'], 'success')
+            # TODO(davidmatthews1004@gmail.com -- Spring 6)
+            # Need to validate that the profile data has been stored in the
+            # database without using the get method.
+            # Currently unsure how to do that.
+            # I have queried the database whilst this test has run (using
+            # time.sleep() and the data is being saved but I need to find a
+            # way to do this within the unitteset.
+
+    def test_update_user_profile_no_data(self):
+        """
+        Ensure request fails if no data is provided.
+        """
+        anakin = User(
+            email='anakin@skywalker.io',
+            first_name='Anakin',
+            last_name='Skywalker'
+        )
+        anakin.set_password('IAmYourFather')
+        anakin.save()
+
+        with self.client:
+            resp_login = self.client.post(
+                '/auth/login',
+                data=json.dumps(
+                    {
+                        'email': 'anakin@skywalker.io',
+                        'password': 'IAmYourFather'
+                    }
+                ),
+                content_type='application/json'
+            )
+            token = json.loads(resp_login.data.decode())['token']
+
+            resp = self.client.post(
+                '/user/updateprofile',
+                data=json.dumps(''),
+                headers={'Authorization': 'Bearer {}'.format(token)},
+                content_type = 'application/json'
+            )
+
+            data = json.loads(resp.data.decode())
+            self.assertEqual(resp.status_code, 400)
+            self.assertEqual(data['message'], 'Invalid payload.')
+            self.assertEqual(data['status'], 'fail')
+
+    def test_update_user_profile_partial_data(self):
+        """
+        Ensure user profile can be updated even if not all fields are provided.
+        """
+        han = User(
+            email='han@solo.io',
+            first_name='Han',
+            last_name='Solo'
+        )
+        han.set_password('BadFeelingAboutThis')
+        han.save()
+
+        with self.client:
+            resp_login = self.client.post(
+                '/auth/login',
+                data=json.dumps(
+                    {
+                        'email': 'han@solo.io',
+                        'password': 'BadFeelingAboutThis'
+                    }
+                ),
+                content_type='application/json'
+            )
+            token = json.loads(resp_login.data.decode())['token']
+
+            resp = self.client.post(
+                '/user/updateprofile',
+                data=json.dumps(
+                    {
+                        'aim': 'Pay Jabba.',
+                        'phase_transform_exp': 'Limited'
+                    }
+                ),
+                headers={'Authorization': 'Bearer {}'.format(token)},
+                content_type = 'application/json'
+            )
+
+            # data = json.loads(resp.data.decode())
+            self.assertEqual(resp.status_code, 200)
+            # TODO(davidmatthews1004@gmail.com -- Spring 6)
+            # Need to validate that the profile data has been stored in the
+            # database without using the get method.
+
+    def test_view_user_profile(self):
+        """
+        Ensure a user can view their own profile information.
+        """
+        obiwan = User(
+            email='obiwan@jedi.io',
+            first_name='Obiwan',
+            last_name='Kenobi'
+        )
+        obiwan.set_password('HelloThere')
+        obiwan.save()
+
+        with self.client:
+            resp_login = self.client.post(
+                '/auth/login',
+                data=json.dumps(
+                    {
+                        'email': 'obiwan@jedi.io',
+                        'password': 'HelloThere'
+                    }
+                ),
+                content_type='application/json'
+            )
+            token = json.loads(resp_login.data.decode())['token']
+
+            resp_update = self.client.post(
+                '/user/updateprofile',
+                data=json.dumps(
+                    {
+                        'aim': 'Protect the Skywalker twins.',
+                        'highest_education': 'Jedi Council Member',
+                        'sci_text_exp': 'Flying is for droids.',
+                        'phase_transform_exp': 'Limited'
+                    }
+                ),
+                headers={'Authorization': 'Bearer {}'.format(token)},
+                content_type='application/json'
+            )
+
+            resp = self.client.get(
+                '/user/viewprofile',
+                headers={'Authorization': 'Bearer {}'.format(token)},
+                content_type='application/json'
+            )
+
+            data = json.loads(resp.data.decode())
+            self.assertEqual(resp.status_code, 200)
+            self.assertEqual(data['status'], 'success')
+            self.assertEqual(
+                data['data']['aim'], 'Protect the Skywalker twins.'
+            )
+
+    def test_update_user_profile_two_partial_data(self):
+        """
+        Ensure a profile can be updated through multiple update requests that
+        each update different parts of the profile.
+        """
+        leia = User(
+            email='leia@organa.io',
+            first_name='Leia',
+            last_name='Organa'
+        )
+        leia.set_password('ShortForAStormtrooper')
+        leia.save()
+
+        with self.client:
+            resp_login = self.client.post(
+                '/auth/login',
+                data=json.dumps(
+                    {
+                        'email': 'leia@organa.io',
+                        'password': 'ShortForAStormtrooper'
+                    }
+                ),
+                content_type='application/json'
+            )
+            token = json.loads(resp_login.data.decode())['token']
+
+            resp_1 = self.client.post(
+                '/user/updateprofile',
+                data=json.dumps(
+                    {
+                        'aim' : 'Defeat the Empire',
+                        'phase_transform_exp': 'Expert'
+                    }
+                ),
+                headers = {'Authorization': 'Bearer {}'.format(token)},
+                content_type = 'application/json'
+            )
+
+            resp_2 = self.client.post(
+                '/user/updateprofile',
+                data=json.dumps(
+                    {
+                        'highest_education' : 'University of Alderaan',
+                        'sci_tech_exp' : 'Expert'
+                    }
+                ),
+                headers={'Authorization': 'Bearer {}'.format(token)},
+                content_type='application/json'
+            )
+
+            resp = self.client.get(
+                '/user/viewprofile',
+                headers={'Authorization': 'Bearer {}'.format(token)},
+                content_type='application/json'
+            )
+
+            data = json.loads(resp.data.decode())
+            self.assertEqual(resp_1.status_code, 200)
+            self.assertEqual(resp_2.status_code, 200)
+            self.assertEqual(resp.status_code, 200)
+            self.assertEqual(data['status'], 'success')
+            self.assertEqual(data['data']['aim'], 'Defeat the Empire')
+            self.assertEqual(data['data']['phase_transform_exp'], 'Expert')
+            self.assertEqual(
+                data['data']['highest_education'], 'University of Alderaan'
+            )
+            self.assertEqual(data['data']['sci_tech_exp'], 'Expert')
+
+    def test_view_user_profile_none(self):
+        """
+        Ensure view profile request fails if a user profile has not been set
+        yet.
+        """
+        yoda = User(
+            email='yoda@jedi.io',
+            first_name='Yoda',
+            last_name='NoLastName'
+        )
+        yoda.set_password('DoOrDoNot')
+        yoda.save()
+
+        with self.client:
+            resp_login = self.client.post(
+                '/auth/login',
+                data=json.dumps(
+                    {
+                        'email': 'yoda@jedi.io',
+                        'password': 'DoOrDoNot'
+                    }
+                ),
+                content_type='application/json'
+            )
+            token = json.loads(resp_login.data.decode())['token']
+
+            resp = self.client.get(
+                '/user/viewprofile',
+                headers={'Authorization': 'Bearer {}'.format(token)},
+                content_type='application/json'
+            )
+
+            data = json.loads(resp.data.decode())
+            self.assertEqual(resp.status_code, 400)
+            self.assertEqual(data['status'], 'fail')
+            self.assertEqual(data['message'], 'User Profile not set yet.')
+
+    def test_update_user_profile_override(self):
+        """
+        Ensure updates to an existing user profile are successfull.
+        """
+        mace = User(
+            email='mace@jedi.io',
+            first_name='Mace',
+            last_name='Windu'
+        )
+        mace.set_password('ThisPartysOver')
+        mace.save()
+
+        with self.client:
+            resp_login = self.client.post(
+                '/auth/login',
+                data=json.dumps(
+                    {
+                        'email': 'mace@jedi.io',
+                        'password': 'ThisPartysOver'
+                    }
+                ),
+                content_type='application/json'
+            )
+            token = json.loads(resp_login.data.decode())['token']
+
+            resp_1 = self.client.post(
+                '/user/updateprofile',
+                data=json.dumps(
+                    {
+                        'aim' : 'Stop the Sif from returning.',
+                        'highest_education' : 'Jedi Council Member',
+                        'sci_tech_exp': 'Limited',
+                        'phase_transform_exp': 'Limted'
+                    }
+                ),
+                headers={'Authorization': 'Bearer {}'.format(token)},
+                content_type='application/json'
+            )
+
+            resp_2 = self.client.post(
+                '/user/updateprofile',
+                data=json.dumps(
+                    {
+                        'aim': 'Stop the Sith from returning.',
+                        'phase_transform_exp': 'Limited'
+                    }
+                ),
+                headers={'Authorization': 'Bearer {}'.format(token)},
+                content_type='application/json'
+            )
+
+            resp = self.client.get(
+                '/user/viewprofile',
+                headers={'Authorization': 'Bearer {}'.format(token)},
+                content_type='application/json'
+            )
+
+            data = json.loads(resp.data.decode())
+            self.assertEqual(resp_1.status_code, 200)
+            self.assertEqual(resp_2.status_code, 200)
+            self.assertEqual(resp.status_code, 200)
+            self.assertEqual(data['status'], 'success')
+            # self.assertEqual(data['data']['aim'], 'Stop the Sith from returning.')
+            self.assertEqual(data['data']['phase_transform_exp'], 'Limited')
+            self.assertEqual(
+                data['data']['highest_education'], 'Jedi Council Member'
+            )
+            self.assertEqual(data['data']['sci_tech_exp'], 'Limited')
 
 if __name__ == '__main__':
     unittest.main()

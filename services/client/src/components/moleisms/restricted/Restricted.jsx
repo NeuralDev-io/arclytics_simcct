467f5b597e929ada256e537a33bfe6594b9a64f8
dalton <duong.daltonle@gmail.com>
2019-08-06 09:04:04 +1000

    Add propTypes validation for Restricted

--- services/client/src/components/moleisms/restricted/Restricted.jsx
+++ services/client/src/components/moleisms/restricted/Restricted.jsx
@@ -1,11 +1,16 @@
 import React from 'react'
+import PropTypes from 'prop-types'
 import Button from '../../elements/button'
 
-const Restricted = props => (
+const Restricted = ({ history }) => (
   <div>
     Not allowed.
-    <Button onClick={() => props.history.push('/')}>GO HOME</Button> {/* eslint-disable-line */}
+    <Button onClick={() => history.push('/')}>GO HOME</Button> {/* eslint-disable-line */}
   </div>
 )
+
+Restricted.propTypes = {
+  history: PropTypes.shape({ push: PropTypes.func.isRequired }).isRequired,
+}
 
 export default Restricted

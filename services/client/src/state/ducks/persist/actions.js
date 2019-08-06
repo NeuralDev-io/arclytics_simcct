7629d568c0fd27d8074140090041bd3fedcca54f
dalton <duong.daltonle@gmail.com>
2019-08-06 17:37:51 +1000

    Fix GET user API call

--- services/client/src/state/ducks/persist/actions.js
+++ services/client/src/state/ducks/persist/actions.js
@@ -3,35 +3,7 @@ import {
 } from './types'
 
 export const getUserProfile = () => (dispatch) => { // eslint-disable-line
-  const fake = {
-    _id: 'string',
-    email: 'iron_man@avengers.io',
-    first_name: 'Tony',
-    last_name: 'Stark',
-    active: true,
-    admin: true,
-    created: '2019-08-05T21:37:14.896Z',
-    verified: false,
-    last_updated: '2019-08-05T21:37:14.896Z',
-    last_login: '2019-08-05T21:37:14.896Z',
-    profile: {
-      aim: 'string',
-      highest_education: 'string',
-      sci_tech_exp: 'string',
-      phase_transform_exp: 'string',
-    },
-    admin_profile: {
-      mobile_number: '0433333333',
-      position: 'dunno',
-      verified: true,
-    },
-  }
-  dispatch({
-    type: GET_USER_PROFILE,
-    payload: fake,
-  })
-
-  fetch('http://localhost:8000/user/profile', {
+  fetch('http://localhost:8000/user', {
     method: 'GET',
     headers: {
       'Content-Type': 'application/json',
@@ -42,9 +14,10 @@ export const getUserProfile = () => (dispatch) => { // eslint-disable-line
     .then((data) => {
       if (data.status === 'fail') throw new Error(data.message)
       if (data.status === 'success') {
+        const { status, ...user } = this.state
         dispatch({
           type: GET_USER_PROFILE,
-          payload: data,
+          payload: user,
         })
       }
     })

f1b071195d082c12cd915345df5d05e61aad88e7
dalton <duong.daltonle@gmail.com>
2019-08-06 12:09:04 +1000

    Add basic sidebar for admin dashboard and make elements tabbable

--- services/client/src/state/ducks/persist/actions.js
+++ services/client/src/state/ducks/persist/actions.js
@@ -3,6 +3,34 @@ import {
 } from './types'
 
 export const getUserProfile = () => (dispatch) => { // eslint-disable-line
+  const fake = {
+    _id: 'string',
+    email: 'iron_man@avengers.io',
+    first_name: 'Tony',
+    last_name: 'Stark',
+    active: true,
+    admin: true,
+    created: '2019-08-05T21:37:14.896Z',
+    verified: false,
+    last_updated: '2019-08-05T21:37:14.896Z',
+    last_login: '2019-08-05T21:37:14.896Z',
+    profile: {
+      aim: 'string',
+      highest_education: 'string',
+      sci_tech_exp: 'string',
+      phase_transform_exp: 'string',
+    },
+    admin_profile: {
+      mobile_number: '0433333333',
+      position: 'dunno',
+      verified: true,
+    },
+  }
+  dispatch({
+    type: GET_USER_PROFILE,
+    payload: fake,
+  })
+
   fetch('http://localhost:8000/user/profile', {
     method: 'GET',
     headers: {

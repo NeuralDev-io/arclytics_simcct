2191479a1588a260bf3f0b2f55f2e873a888c166
dalton <duong.daltonle@gmail.com>
2019-08-14 02:16:30 +1000

    Add modal for adding alloy, still need to add Save button and hook api

--- services/client/src/components/elements/periodic-table/index.js
+++ services/client/src/components/elements/periodic-table/index.js
@@ -1,2 +1,3 @@
 export { default } from './PeriodicTable'
 export { default as UncontrolledPeriodicTable } from './UncontrolledPeriodicTable'
+export { default as PeriodicTableData } from './data'
--- services/client/src/components/moleisms/admin-users/ManageUsers.jsx
+++ services/client/src/components/moleisms/admin-users/ManageUsers.jsx
@@ -1,6 +1,5 @@
 import React, { Component } from 'react'
 import { connect } from 'react-redux'
-import PeriodicTable from '../../elements/periodic-table'
 
 import styles from './ManageUsers.module.scss'
 
@@ -11,7 +10,6 @@ class ManageUsers extends Component {
     return (
       <div className={styles.container}>
         <h3>Users</h3>
-        <PeriodicTable />
       </div>
     )
   }

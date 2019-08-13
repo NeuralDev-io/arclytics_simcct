ceddf7176e562adb18e76cc7876d63279dca5773
Andrew Che <andrew@codeninja55.me>
2019-08-14 03:50:35 +1000

    Adding the user alloys table based on Daltons work

--- services/client/src/components/moleisms/user-alloys/UserAlloys.jsx
+++ services/client/src/components/moleisms/user-alloys/UserAlloys.jsx
@@ -1,22 +1,136 @@
 import React, { Component } from 'react'
+import PropTypes from 'prop-types'
+import { connect } from 'react-redux'
+import PlusIcon from 'react-feather/dist/icons/plus'
+import EditIcon from 'react-feather/dist/icons/edit-3'
+import TrashIcon from 'react-feather/dist/icons/trash-2'
+import Table from '../../elements/table'
+import TextField from '../../elements/textfield'
+import Button from '../../elements/button'
+import {
+  postUserAlloy,
+  getUserAlloys,
+  getUserAlloyDetail,
+  deleteUserAlloys,
+  putUserAlloy
+} from '../../../state/ducks/userAlloys/actions'
 
 import styles from './UserAlloys.module.scss'
 
 class UserAlloys extends Component {
-  // constructor(props) {
-  //   super(props)
-  //   this.state = {
-  //     name: '',
-  //   }
-  // }
+  constructor(props) {
+    super(props)
+    this.state = {
+      name: '',
+    }
+  }
+
+  componentDidMount() {
+    // When the component mounts, we need to get the list of alloys with API first.
+    const { alloyList, getAlloysConnect } = this.props
+    if (!alloyList || alloyList.length === 0) getAlloysConnect()
+  }
+
+  handleAlloyOperation = (option) => {
+    if (option.value === 'edit') console.log('edit')
+    if (option.value === 'delete') console.log('delete')
+  }
 
   render() {
+    const { alloyList } = this.props
+    const { name } = this.state
+
+    console.log(alloyList)
+
+    const tableData = alloyList.filter(a => a.name.includes(name))
+    const columns = [
+      {
+        Header: 'Alloy name',
+        accessor: 'name',
+      },
+      {
+        Header: '',
+        Cell: (
+          <div className={styles.actions}>
+            <Button
+              appearance="text"
+              length="short"
+              IconComponent={props => <EditIcon {...props} />}
+            >
+              Edit
+            </Button>
+            <Button
+              appearance="text"
+              color="dangerous"
+              IconComponent={props => <TrashIcon {...props} />}
+            >
+              Delete
+            </Button>
+          </div>
+        ),
+        width: 210,
+      },
+    ]
+
     return (
       <div className={styles.container}>
-        <h3>Personal Alloy database</h3>
+        <h3>Personal alloy database</h3>
+        <div className={styles.tools}>
+          <div className="input-row">
+            <span>Search</span>
+            <TextField
+              type="text"
+              length="long"
+              name="name"
+              placeholder="Alloy name"
+              value={name}
+              onChange={value => this.setState({ name: value })}
+            />
+          </div>
+          <Button
+            appearance="outline"
+            onClick={() => {}}
+            IconComponent={props => <PlusIcon {...props} />}
+            length="short"
+          >
+            Add
+          </Button>
+        </div>
+        <Table
+          className="-highlight"
+          data={tableData}
+          columns={columns}
+          pageSize={tableData.length > 10 ? 10 : tableData.length}
+          showPageSizeOptions={false}
+          showPagination={tableData.length !== 0}
+          resizable={false}
+          condensed
+        />
       </div>
     )
   }
 }
 
-export default UserAlloys
+// Type annotate props
+UserAlloys.propTypes = {
+  alloyList: PropTypes.arrayOf(PropTypes.shape({
+    _id: PropTypes.string,
+    name: PropTypes.string,
+    compositions: PropTypes.arrayOf(PropTypes.shape({
+      name: PropTypes.string,
+      symbol: PropTypes.string,
+      weight: PropTypes.number,
+    })),
+  })).isRequired,
+  getAlloysConnect: PropTypes.func.isRequired,
+}
+
+const mapStateToProps = state => ({
+  alloyList: state.alloys.list,
+})
+
+const mapDispatchToProps = {
+  getAlloysConnect: getUserAlloys,
+}
+
+export default connect(mapStateToProps, mapDispatchToProps)(UserAlloys)

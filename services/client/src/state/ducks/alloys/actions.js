c2d8f963a98e0f8052b345cd7c46c649a7e90e30
dalton <duong.daltonle@gmail.com>
2019-08-14 17:32:19 +1000

    Add actions + reducer cases for global alloys

--- services/client/src/state/ducks/alloys/actions.js
+++ services/client/src/state/ducks/alloys/actions.js
@@ -1,9 +1,11 @@
 import {
-  GET_ALLOYS,
-  CREATE_ALLOY,
+  GET_GLOBAL_ALLOYS,
+  CREATE_GLOBAL_ALLOY,
+  UPDATE_GLOBAL_ALLOY,
+  DELETE_GLOBAL_ALLOY,
 } from './types'
 
-export const getAlloys = () => (dispatch) => {
+export const getGlobalAlloys = () => (dispatch) => {
   fetch('http://localhost:8001/global/alloys', {
     method: 'GET',
     headers: {
@@ -17,7 +19,7 @@ export const getAlloys = () => (dispatch) => {
       if (data.status === 'fail') throw new Error(data.message)
       if (data.status === 'success') {
         dispatch({
-          type: GET_ALLOYS,
+          type: GET_GLOBAL_ALLOYS,
           payload: data.alloys,
         })
       }
@@ -25,7 +27,7 @@ export const getAlloys = () => (dispatch) => {
     .catch(err => console.log(err))
 }
 
-export const createAlloy = alloy => (dispatch) => {
+export const createGlobalAlloy = alloy => (dispatch) => {
   fetch('http://localhost:8001/global/alloys', {
     method: 'POST',
     headers: {
@@ -39,11 +41,54 @@ export const createAlloy = alloy => (dispatch) => {
       if (data.status === 'fail') throw new Error(data.message)
       if (data.status === 'success') {
         dispatch({
-          type: CREATE_ALLOY,
+          type: CREATE_GLOBAL_ALLOY,
           payload: {
             ...alloy,
             _id: data._id, // eslint-disable-line
           },
+        })
+      }
+    })
+    .catch(err => console.log(err))
+}
+
+export const updateGlobalAlloy = alloy => (dispatch) => {
+  fetch('http://localhost:8001/global/alloys', {
+    method: 'PUT',
+    headers: {
+      'Content-Type': 'application/json',
+      Authorization: `Bearer ${localStorage.getItem('token')}`,
+    },
+    body: JSON.stringify(alloy),
+  })
+    .then(res => res.json())
+    .then((data) => {
+      if (data.status === 'fail') throw new Error(data.message)
+      if (data.status === 'success') {
+        dispatch({
+          type: UPDATE_GLOBAL_ALLOY,
+          payload: data.data,
+        })
+      }
+    })
+    .catch(err => console.log(err))
+}
+
+export const deleteGlobalAlloy = alloyId => (dispatch) => {
+  fetch(`http://localhost:8001/global/alloys/${alloyId}`, {
+    method: 'PUT',
+    headers: {
+      'Content-Type': 'application/json',
+      Authorization: `Bearer ${localStorage.getItem('token')}`,
+    },
+  })
+    .then(res => res.json())
+    .then((data) => {
+      if (data.status === 'fail') throw new Error(data.message)
+      if (data.status === 'success') {
+        dispatch({
+          type: DELETE_GLOBAL_ALLOY,
+          payload: alloyId,
         })
       }
     })
--- services/client/src/state/ducks/alloys/types.js
+++ services/client/src/state/ducks/alloys/types.js
@@ -1,2 +1,11 @@
-export const GET_ALLOYS = 'alloys/GET_ALLOYS'
-export const CREATE_ALLOY = 'alloys/CREATE_ALLOYS'
+export const GET_GLOBAL_ALLOYS = 'alloys/GET_GLOBAL_ALLOYS'
+export const CREATE_GLOBAL_ALLOY = 'alloys/CREATE_GLOBAL_ALLOY'
+export const UPDATE_GLOBAL_ALLOY = 'alloys/UPDATE_GLOBAL_ALLOY'
+export const DELETE_GLOBAL_ALLOY = 'alloys/DELETE_GLOBAL_ALOY'
+
+export const GET_USER_ALLOYS = 'alloys/GET_USER_ALLOYS'
+export const CREATE_USER_ALLOY = 'alloys/CREATE_USER_ALLOY'
+export const UPDATE_USER_ALLOY = 'alloys/UPDATE_USER_ALLOY'
+export const DELETE_USER_ALLOY = 'alloys/DELETE_USER_ALLOY'
+
+export const GET_ALL_ALLOYS = 'alloys/GET_ALL_ALLOYS'

5bf3870615d28220e952ddcf567000d870444346
Unknown <arvy.au@gmail.com>
2019-08-14 23:01:56 +1000

    Change profile page
    - Add the 4th question
    - Extend the text field
    - Change the password reset into a button 
    - Remove delete account

--- services/client/src/components/pages/profile/ProfilePage.jsx
+++ services/client/src/components/pages/profile/ProfilePage.jsx
@@ -12,8 +12,8 @@ import { updateUserProfile } from '../../../state/ducks/persist/actions'
 import TextField from '../../elements/textfield'
 import Select from '../../elements/select'
 import AppBar from '../../moleisms/appbar'
-import Modal from '../../elements/modal'
 import Button from '../../elements/button'
+import MailIcon from 'react-feather/dist/icons/mail'
 
 import styles from './ProfilePage.module.scss'
 
@@ -30,23 +30,27 @@ class ProfilePage extends Component {
       lastName: props.user.last_name,
       question1:  props.user.profile ? { label:props.user.profile.aim , value: props.user.profile.aim } : null,
       question1Select: [
-        { label: 'Option 1', value: 'Option 1'},
-        { label: 'Option 2', value: 'Option 2'},
+        { label: 'Q1 Option 1', value: 'Q1 Option 1'},
+        { label: 'Q1 Option 2', value: 'Q1 Option 2'},
       ],
-      question2: props.user.profile ? { label:props.user.profile.sci_tech_exp , value: props.user.profile.sci_tech_exp } : null,
+      question2: props.user.profile ? { label:props.user.profile.highest_education , value: props.user.profile.highest_education } : null,
       question2Select: [
-        { label: 'Option 1', value: 'Option 1'},
-        { label: 'Option 2', value: 'Option 2'},
+        { label: 'Q2 Option 1', value: 'Q2 Option 1'},
+        { label: 'Q2 Option 2', value: 'Q2 Option 2'},
       ],
-      question3: props.user.profile ? { label:props.user.profile.phase_transform_exp , value: props.user.profile.phase_transform_exp } : null,
+      question3: props.user.profile ? { label:props.user.profile.sci_tech_exp , value: props.user.profile.sci_tech_exp } : null,
       question3Select: [
-        { label: 'Option 1', value: 'Option 1'},
-        { label: 'Option 2', value: 'Option 2'},
+        { label: 'Q3 Option 1', value: 'Q3 Option 1'},
+        { label: 'Q3 Option 2', value: 'Q3 Option 2'},
+      ],
+      question4: props.user.profile ? { label:props.user.profile.phase_transform_exp , value: props.user.profile.phase_transform_exp } : null,
+      question4Select: [
+        { label: 'Q4 Option 1', value: 'Q4 Option 1'},
+        { label: 'Q4 Option 2', value: 'Q4 Option 2'},
       ],
       currPassword: null,
       newPassword: null,
       cnfrmPassword: null,
-      showDelete: false,
       edit: false,
     }
   }
@@ -68,8 +72,9 @@ class ProfilePage extends Component {
       firstName: user.first_name, //just in case we implement demo mode
       lastName: user.last_name,
       question1:  user.profile ? {label:user.profile.aim , value:user.profile.aim} : null,
-      question2: user.profile ? {label:user.profile.sci_tech_exp , value: user.profile.sci_tech_exp} : null,
-      question3: user.profile ? {label:user.profile.phase_transform_exp , value: user.profile.phase_transform_exp} : null,
+      question2: user.profile ? {label:user.profile.highest_education, value: user.profile.highest_education} : null,
+      question3: user.profile ? {label:user.profile.sci_tech_exp , value: user.profile.sci_tech_exp} : null,
+      question4: user.profile ? {label:user.profile.phase_transform_exp , value: user.profile.phase_transform_exp} : null,
       edit: false
     }) 
     :   
@@ -84,15 +89,14 @@ class ProfilePage extends Component {
   }
 
   updateUser = () => {
-    const {firstName, lastName, question1, question2, question3} = this.state
+    const {firstName, lastName, question1, question2, question3, question4} = this.state
     this.props.updateUserProfileConnect({
       first_name: firstName,
       last_name: lastName,
-      profile:{
         aim: question1 ? question1.value : null,
-        sci_tech_exp: question2 ? question2.value : null,
-        phase_transform_exp: question3 ? question3.value : null
-      }
+        highest_education: question2 ? question2.value: null,
+        sci_tech_exp: question2 ? question3.value : null,
+        phase_transform_exp: question3 ? question4.value : null
     })
     this.setState({ edit: false })
   }
@@ -108,10 +112,8 @@ class ProfilePage extends Component {
       question2Select,
       question3,
       question3Select,
-      currPassword, 
-      newPassword, 
-      cnfrmPassword, 
-      showDelete,
+      question4,
+      question4Select,
       edit,
     } = this.state
     const { user } = this.props
@@ -128,45 +130,43 @@ class ProfilePage extends Component {
 
         <div className={styles.main}> 
           
-          <div className={styles.general}>
+          <React.Fragment>
             <h4 className={styles.header}>General</h4>
             <div className={styles.generalFields}>
               <div className={styles.row}>
-                <h6 className={styles.column}> Email </h6> 
-                <div className={styles.column}> <h6 className={styles.emailText}> {email} </h6> </div>   
+                <h6 className={styles.leftCol}> Email </h6> 
+                <div className={styles.rightCol}> <h6 className={styles.emailText}> {email} </h6> </div>   
               </div>
               <div className={styles.row}>
                 <h6  className={styles.leftCol}>First name</h6>
-                <div className={styles.column}>
+                <div className={styles.rightCol}>
                 <TextField
                     type="firstName"
                     name="firstName"
                     value={firstName}
                     placeholder="First Name"
-                    length="short"
+                    length="stretch"
                     isDisabled={!edit}
                     onChange={value => this.handleChange('firstName', value)}
                   />
                 </div>       
                 </div>
               <div className={styles.row}>
-              <h6 className={styles.column}> Last name </h6>
-              <div className={styles.column}>
+              <h6 className={styles.leftCol}> Last name </h6>
+              <div className={styles.rightCol}>
                 <TextField
                   type="lastName"
                   name="lastName"
                   value={lastName}
                   placeholder="Last Name"
-                  length="short"
+                  length="stretch"
                   isDisabled={!edit}
                   onChange={value => this.handleChange('lastName', value )}
                 />
               </div> 
             </div>     
             </div>
-       
-            <div className={styles.profilePicture}> Profile Picture </div>
-          </div>
+          </React.Fragment>
        
           <div className={styles.about}>
             <h4 className={styles.header}> About yourself </h4>
@@ -184,23 +184,23 @@ class ProfilePage extends Component {
                   onChange={value => this.handleChange('question1', value )}
                 />
               </div>
-
+              
               <div className={styles.question}>
-                <h6 className={styles.questionText}> What is your experience with solid-state phase transformation?  </h6>
-                <Select
-                  type="question2"
-                  name="question2"
-                  placeholder="---"
-                  value= {question2}
-                  options={question2Select}
-                  length="stretch"
-                  isDisabled={!edit}
-                  onChange={value => this.handleChange('question2', value )}
-                />
+              <h6 className={styles.questionText}> What is the highest level of education have you studied? </h6>
+              <Select
+                type="question2"
+                name="question2"
+                placeholder="---"
+                value= {question2}
+                options={question2Select}
+                length="stretch"
+                isDisabled={!edit}
+                onChange={value => this.handleChange('question2', value )}
+              />
               </div>
 
               <div className={styles.question}>
-                <h6 className={styles.questionText}> What is your experiece with scientific software? </h6>
+                <h6 className={styles.questionText}> What is your experience with solid-state phase transformation?  </h6>
                 <Select
                   type="question2"
                   name="question2"
@@ -212,88 +212,38 @@ class ProfilePage extends Component {
                   onChange={value => this.handleChange('question3', value )}
                 />
               </div>
+
+              <div className={styles.question}>
+                <h6 className={styles.questionText}> What is your experiece with scientific software? </h6>
+                <Select
+                  type="question3"
+                  name="question3"
+                  placeholder="---"
+                  value= {question4}
+                  options={question4Select}
+                  length="stretch"
+                  isDisabled={!edit}
+                  onChange={value => this.handleChange('question4', value )}
+                />
+              </div>
             </div>
           </div>
           
-          <div>
-              {!edit ? ( <div className={styles.editButtons}> <Button onClick={this.handleEdit}> Edit </Button> </div>) : ('')} 
+          <div className={styles.editButtons}>
+              {!edit ? ( <> <Button onClick={this.handleEdit}> Edit </Button> </>) : ('')} 
               {edit ? (
-              <div className={styles.editButtons}> 
+              <> 
                 <Button onClick={this.updateUser}> Save </Button>
                 <Button className={styles.cancel}appearance="outline" onClick={this.handleEdit}> Cancel </Button> 
-              </div>) : ('')} 
+              </>) : ('')} 
           </div>
 
 
           <div className={styles.security}>
             <h4 className={styles.header}>Security</h4>
             <h5>Change your password</h5>
-            <div className={styles.row}>
-              <h6 className={styles.currPwd}>Current password</h6>
-              <div className={StyleSheet.input}>
-                <TextField
-                  type="password"
-                  name="currPassword"
-                  placeholder="Current Password"
-                  value = {currPassword}
-                  length="long"
-                  onChange={value => this.handleChange('currPassword', value)}
-                />
-              </div>
-            </div>
-
-            <div className={styles.row}>
-              <h6 className={styles.newPwd}>New password</h6>
-              <div className={styles.input}>
-                <TextField
-                    type="password"
-                    name="newPassword"
-                    placeholder="Confirm Password"
-                    value= {newPassword}
-                    length="long"
-                    onChange={value => this.handleChange('newPassword', value)}
-                />  
-              </div>
-            </div>
-
-            <div className={styles.row}>
-             <h6 className={styles.cnfrmPwd}>Confirm password</h6> 
-             <div className={styles.input}>
-              <TextField
-                  type="password"
-                  name="cnfrmPassword"
-                  placeholder="Confirm Password"
-                  value= {cnfrmPassword}
-                  length="long"
-                  onChange={value => this.handleChange('cnfrmPassword', value)}
-                />
-             </div>
-            </div>
-          </div>
-
-          <div>
-            <h5 className={styles.deleteWarning}>Delete your account</h5>
-            <h6 className={styles.deleteWarning}>Once you delete your account, there is no going back. Please be certain</h6>
-            <h6 className={styles.delete} onClick={this.handleDeleteModal}>Delete my account</h6>  
-            <Modal  show={showDelete} clicked={this.handleDeleteModal}> 
-              <div className={styles.deleteModal} >
-              <h6>All of your data will be lost. Are you sure you want to delete your account? Please enter your password to confirm</h6>
-              <TextField
-                type="password"
-                name="cnfrmDelete"
-                placeholder="Enter Password"
-                length="stretch"
-                onChange={value => this.handleChange('cnfrmDelete', value)}
-              />
-                <div className={styles.deleteButtons}>
-                  <Button className={styles.cancelDelete} name="cancelDelete" type="button" onClick={this.handleDeleteModal} length="long">
-                    CANCEL
-                  </Button> 
-                  <h6 className={styles.delete}> Delete my account </h6> 
-                </div>
-              </div>
-
-            </Modal>
+            <h6> Click the button below to send a password reset email for this account. </h6> 
+            <Button className={styles.resetPassword} length="large" IconComponent={(props) => {return <MailIcon {...props} />}}> RESET YOUR PASSWORD </Button> 
           </div>
         </div>
       </React.Fragment>

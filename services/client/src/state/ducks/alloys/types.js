a3b66cdb420719c72a548766b6337eedb1366c19
Andrew Che <andrew@codeninja55.me>
2019-08-14 19:55:36 +1000

    JUST WANTED TO SAT DALTON EAGLE EYES CAUGHT YOU!

--- services/client/src/state/ducks/alloys/types.js
+++ services/client/src/state/ducks/alloys/types.js
@@ -1,7 +1,7 @@
 export const GET_GLOBAL_ALLOYS = 'alloys/GET_GLOBAL_ALLOYS'
 export const CREATE_GLOBAL_ALLOY = 'alloys/CREATE_GLOBAL_ALLOY'
 export const UPDATE_GLOBAL_ALLOY = 'alloys/UPDATE_GLOBAL_ALLOY'
-export const DELETE_GLOBAL_ALLOY = 'alloys/DELETE_GLOBAL_ALOY'
+export const DELETE_GLOBAL_ALLOY = 'alloys/DELETE_GLOBAL_ALLOY'
 
 export const GET_USER_ALLOYS = 'alloys/GET_USER_ALLOYS'
 export const CREATE_USER_ALLOY = 'alloys/CREATE_USER_ALLOY'

ddf201b936cc29f14e47e6e0a7cf2091a921a759
dalton <duong.daltonle@gmail.com>
2019-08-14 17:32:19 +1000

    Add actions + reducer cases for global alloys

--- services/client/src/state/ducks/alloys/reducers.js
+++ services/client/src/state/ducks/alloys/reducers.js
@@ -1,27 +1,46 @@
 import {
-  GET_ALLOYS,
-  CREATE_ALLOY,
+  GET_GLOBAL_ALLOYS,
+  CREATE_GLOBAL_ALLOY,
+  UPDATE_GLOBAL_ALLOY,
+  DELETE_GLOBAL_ALLOY,
 } from './types'
 
 const initialState = {
-  list: [],
+  global: [],
+  user: [],
 }
 
 const reducer = (state = initialState, action) => {
   switch (action.type) {
-    case GET_ALLOYS:
+    case GET_GLOBAL_ALLOYS:
       return {
         ...state,
-        list: action.payload,
+        global: action.payload,
       }
-    case CREATE_ALLOY: {
+    case CREATE_GLOBAL_ALLOY: {
       const newAlloys = [
-        ...state.list,
+        ...state.global,
         action.payload,
       ]
       return {
         ...state,
-        list: newAlloys,
+        global: newAlloys,
+      }
+    }
+    case UPDATE_GLOBAL_ALLOY: {
+      const newAlloys = [...state.global]
+      const idx = newAlloys.findIndex(a => a._id === action.payload._id) // eslint-disable-line
+      newAlloys[idx] = action.payload
+      return {
+        ...state,
+        global: newAlloys,
+      }
+    }
+    case DELETE_GLOBAL_ALLOY: {
+      const newAlloys = state.global.filter(a => a._id !== action.payload) // eslint-disable-line
+      return {
+        ...state,
+        global: newAlloys,
       }
     }
     default:

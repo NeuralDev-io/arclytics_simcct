e6a33044d14fb03aee8edec3632e7c16cfb44d3a
Andrew Che <andrew@codeninja55.me>
2019-08-14 15:00:33 +1000

    Adding delete functionality to User Alloys and cleaning up code

--- services/client/src/state/ducks/userAlloys/actions.js
+++ services/client/src/state/ducks/userAlloys/actions.js
@@ -1,3 +1,18 @@
+/**
+ *
+ * This source code is licensed under the MIT license found in the
+ * LICENSE file in the root directory of this repository.
+ *
+ * Actions used to make API requests to the `users` microservice to handle operations
+ * on User Saved Alloys including creating, retrieving, updating, and deleting an
+ * alloy. The response will be dispatched to a reducer and sent to the `UserAlloys`
+ * component.
+ *
+ *
+ * @version 0.1.0
+ * @author Andrew Che
+ */
+
 import {
   CREATE_USER_ALLOY,
   RETRIEVE_USER_ALLOYS,
@@ -9,7 +24,20 @@ import {
 // TODO(andrew@neuraldev.io): Comment on each function about the expected input
 //  and some more information about the expected response.
 
-export const postUserAlloy = () => (alloy, dispatch) => {
+export const postUserAlloy = alloy => (dispatch) => {
+  /**
+   * API call to `users` server to create a new alloy which is stored in the User's
+   * Document. The Users are identified by the JWT token passed as the Authorization
+   * in the header.
+   * @param {alloy} a valid Alloy object.
+   * @param {dispatch} the Redux `dispatch()` function which will define the `type` of
+   * reducer to use as defined in `src/state/ducks/userAlloys/reducers.js`.
+   * If successful, the response will be:
+   * {
+   *   "status": "success",
+   *   "data": {"_id": ..., "name": ..., "compositions": [...]}
+   * }
+   */
   fetch('http://localhost:8000/user/alloys', {
     method: 'POST',
     headers: {
@@ -24,16 +52,27 @@ export const postUserAlloy = () => (alloy, dispatch) => {
         // Dispatch the response to the reducer
         dispatch({
           type: CREATE_USER_ALLOY,
-          payload: data.data, // Response returns {"status": "success", "data": {}}
+          payload: data.data,
         })
       } else { throw new Error(data.message) }
-      // TODO(andrew@neuraldev.io): Should consider giving the user another change
+      // TODO(andrew@neuraldev.io): Should consider giving the user another chance
       //  to post or change the alloy rather than throwing an error.
     })
     .catch(err => console.log(err))
 }
 
 export const getUserAlloys = () => (dispatch) => {
+  /**
+   * API call to `users` server to get all alloys  stored in the User's Document.
+   * The Users are identified by the JWT token passed as the Authorization in the header.
+   * @param {dispatch} the Redux `dispatch()` function which will define the `type` of
+   * reducer to use as defined in `src/state/ducks/userAlloys/reducers.js`.
+   * If successful, the response will be:
+   * {
+   *   "status": "success",
+   *   "data": [{"_id": ..., "name": ..., "compositions": [...]}, {...}]
+   * }
+   */
   fetch('http://localhost:8000/user/alloys', {
     method: 'GET',
     headers: {
@@ -49,14 +88,14 @@ export const getUserAlloys = () => (dispatch) => {
         // Dispatch the response to the reducer
         dispatch({
           type: RETRIEVE_USER_ALLOYS,
-          payload: data.data, // Response returns {"status": "success", "data": [...]}
+          payload: data.data,
         })
       }
     })
     .catch(err => console.log(err))
 }
 
-export const getUserAlloyDetail = () => (alloyId, dispatch) => {
+export const getUserAlloyDetail = alloyId => (dispatch) => {
   fetch(`http://localhost:8000/user/alloys/${alloyId}`, {
     method: 'GET',
     headers: {
@@ -77,7 +116,7 @@ export const getUserAlloyDetail = () => (alloyId, dispatch) => {
     .catch(err => console.log(err))
 }
 
-export const putUserAlloy = () => (alloy, alloyId, dispatch) => {
+export const putUserAlloy = (alloy, alloyId) => (dispatch) => {
   fetch(`http://localhost:8000/user/alloys/${alloyId}`, {
     method: 'DELETE',
     headers: {
@@ -99,7 +138,20 @@ export const putUserAlloy = () => (alloy, alloyId, dispatch) => {
     .catch(err => console.log(err))
 }
 
-export const deleteUserAlloys = () => (alloyId, dispatch) => {
+export const deleteUserAlloy = alloyId => (dispatch) => {
+  /**
+   * API call to `users` server to delete an alloy which is stored in the User's
+   * Document. The alloy is identified by the `alloyId` parameter in the request
+   * url. The Users are identified by the JWT token passed as the Authorization
+   * in the header.
+   * @param {alloyId} a valid Alloy ObjectId.
+   * @param {dispatch} the Redux `dispatch()` function which will define the `type` of
+   * reducer to use as defined in `src/state/ducks/userAlloys/reducers.js`.
+   * If successful, the response will be:
+   * {
+   *   "status": "success",
+   * }
+   */
   fetch(`http://localhost:8000/user/alloys/${alloyId}`, {
     method: 'DELETE',
     headers: {

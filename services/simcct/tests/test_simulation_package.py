ce1e10473b40ceeb6d8872be96b5f254fa521382
Andrew Che <andrew@codeninja55.me>
2019-08-19 02:40:24 +1000

    Adding the user cooling curve algorithm

--- services/simcct/tests/test_simulation_package.py
+++ services/simcct/tests/test_simulation_package.py
@@ -1,20 +1,18 @@
 # -*- coding: utf-8 -*-
 # -----------------------------------------------------------------------------
 # arclytics_sim
-# test_simulation.py
+# test_simulation_package.py
 #
 # Attributions:
 # [1]
 # -----------------------------------------------------------------------------
-__author__ = ['Arvy Salazar <@Xaraox>, Andrew Che <@codeninja55>']
-__credits__ = ['']
-__license__ = 'TBA'
-__version__ = '0.1.0'
+__author__ = ['Andrew Che <@codeninja55>']
+__credits__ = ['Dr. Philip Bendeich', 'Dr. Ondrej Muransky']
 __maintainer__ = 'Andrew Che'
 __email__ = 'andrew@neuraldev.io'
 __status__ = 'development'
 __date__ = '2019.06.26'
-"""test_simulation.py: 
+"""test_simulation_package.py: 
 
 The test module for the Phase Simulation package.
 """
@@ -32,16 +30,25 @@ BASE_DIR = os.path.abspath(
 sys.path.append(BASE_DIR)
 
 
-class TestSimulation(unittest.TestCase):
+class TestConfigurations(unittest.TestCase):
     def setUp(self) -> None:
         self.configs = SimConfiguration(debug=True)
 
     def test_configurations(self):
         pass
 
+
+class TestUserCoolingCurve(unittest.TestCase):
+    def test_user_cooling_result(self):
+        pass
+
+
+class TestTTT(unittest.TestCase):
     def test_ttt_result(self):
         pass
 
+
+class TestCCT(unittest.TestCase):
     def test_cct_result(self):
         pass
 

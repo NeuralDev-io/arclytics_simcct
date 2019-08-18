31a5564251bbb615b774fba23dad4449219435c9
Andrew Che <andrew@codeninja55.me>
2019-08-18 19:40:56 +1000

    Changing the way we test to use a unit test for full local sim package

--- services/simcct/tests/test_simulation_package.py
+++ services/simcct/tests/test_simulation_package.py
@@ -1,9 +1,49 @@
+# -*- coding: utf-8 -*-
+# -----------------------------------------------------------------------------
+# arclytics_sim
+# test_simulation.py
+#
+# Attributions:
+# [1]
+# -----------------------------------------------------------------------------
+__author__ = ['Arvy Salazar <@Xaraox>, Andrew Che <@codeninja55>']
+__credits__ = ['']
+__license__ = 'TBA'
+__version__ = '0.1.0'
+__maintainer__ = 'Andrew Che'
+__email__ = 'andrew@neuraldev.io'
+__status__ = 'development'
+__date__ = '2019.06.26'
+"""test_simulation.py: 
+
+The test module for the Phase Simulation package.
+"""
+
+import os
+import sys
 import unittest
 
+from simulation.ae3_utilities import *
+from simulation.phasesimulation import *
 
-class MyTestCase(unittest.TestCase):
-    def test_something(self):
-        self.assertEqual(True, False)
+BASE_DIR = os.path.abspath(
+    os.path.join(os.path.dirname(os.path.abspath(__file__)), os.pardir)
+)
+sys.path.append(BASE_DIR)
+
+
+class TestSimulation(unittest.TestCase):
+    def setUp(self) -> None:
+        self.configs = SimConfiguration(debug=True)
+
+    def test_configurations(self):
+        pass
+
+    def test_ttt_result(self):
+        pass
+
+    def test_cct_result(self):
+        pass
 
 
 if __name__ == '__main__':

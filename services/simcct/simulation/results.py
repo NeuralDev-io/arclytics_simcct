3de1e975b108450ff89d531353d55bed95fc3c62
Andrew Che <andrew@neuraldev.io>
2019-08-21 02:02:44 +1000

    Refactoring quite a fair bit in results container

--- services/simcct/simulation/results.py
+++ services/simcct/simulation/results.py
@@ -16,23 +16,22 @@ __status__ = 'development'
 __date__ = '2019.07.01'
 """results.py: 
 
-This module defines two types of objects. The ResultsMatrix is a wrapper class
+This module defines two types of objects. The DynamicNdarray is a wrapper class
 on the `numpy.ndarray` filled with zeros. This class can only ever be 2 columns
 and the user must define the number of rows. We need this custom wrapper to 
 allow dynamic allocation as elements are added to the array.
 
 The other class defines a ResultsData object where we can store the 
-ResultsMatrix internally for the Simulation instance. It also has methods to 
+DynamicNdarray internally for the Simulation instance. It also has methods to 
 accomplish some simple File IO tasks such as saving to CSV, JSON, or pickle.
 """
 
-import csv
 import math
 import numpy as np
 from typing import Union, Tuple, Optional
 
 
-class ResultsMatrix(object):
+class DynamicNdarray(object):
     """
     This is a Rank-2 Array wrapper around a `numpy.ndarray` filled with zeros
     using numpy.zeros(). By default, the shape of the `ndarray` will always of
@@ -42,28 +41,42 @@ class ResultsMatrix(object):
 
     Usage:
         # This will get you [[0., 0.], [0., 0.]] `numpy.ndarray`
-        mat = ResultsMatrix(rows=2)
+        mat = DynamicNdarray(rows=2)
     """
-    def __init__(self, rows: int = 1, dtype: Optional[object] = np.float32):
+    def __init__(
+            self,
+            shape: Union[Tuple[int, int], int],
+            dtype: Optional[object] = np.float32
+    ):
         """Initialise the internal `numpy.ndarray` object with the shape passed
         in from the rows with a default of 2 columns.
 
         Args:
-            rows: the number of rows in the Rank-2 Array dimension.
+            shape: the shape as a tuple or row in the Rank-2 or Rank 1
+                   Array dimension.
             dtype: the data type of the elements in the `numpy.ndarray`.
         """
-        self.obj = np.zeros(shape=(rows, 2), dtype=dtype, order='C')
+        if isinstance(shape, tuple):
+            self.obj = np.zeros(
+                shape=(shape[0], shape[1]), dtype=dtype, order='C'
+            )
+            self.rows = shape[0]
+            self.cols = shape[1]
+        else:
+            self.obj = np.zeros(shape=(shape,), dtype=dtype, order='C')
+            self.rows = 0
+            self.cols = shape
         # add the new attributes to the created instance
         self.shape = self.obj.shape
-        self.rows = rows
-        self.cols = 2
 
-    def __getitem__(self, index: Union[Tuple[int, int], int]) -> float:
+    def __getitem__(
+            self, index: Union[Tuple[int, int], int]
+    ) -> Union[float, np.ndarray]:
         """Using the `[]` operators, this will get you the element stored at
         the index passed in.
 
         Usage:
-            mat = ResultsMatrix(rows=2)
+            mat = DynamicNdarray(rows=2)
             mat[0, 0] = 10.0
             print(mat[0, 0])  # will print 10.0
 
@@ -72,7 +85,8 @@ class ResultsMatrix(object):
                    (will get you an element) or an int (will
 
         Returns:
-            The value of the element.
+            The value of the element or a `numpy.ndarray` as a copy if
+            using slicing
         """
         return self.obj.__getitem__(index)
 
@@ -105,12 +119,15 @@ class ResultsMatrix(object):
 
             self.resize(new_rows)
             self.rows = new_rows
-            self.shape = (new_rows, 2)
+            self.shape = self.obj.shape
         # If resizing need, or otherwise, set item
         self.obj.__setitem__(key, value)
 
     def __delitem__(self, key):
         del self.obj[key]
+
+    def tolist(self):
+        return self.obj.tolist()
 
     def resize(self, n_rows: int) -> None:
         """Resize the `numpy.ndarray` so that it has n_rows of additional
@@ -124,52 +141,38 @@ class ResultsMatrix(object):
         """
         if n_rows < self.rows:
             raise Exception(f'New row size must be bigger than {self.rows}.')
-        self.obj.resize((n_rows, 2), refcheck=True)
+        self.obj.resize((n_rows, self.cols), refcheck=True)
+        self.shape = self.obj.shape
 
-    def trim_zeros(self):
-        """
-        Trim the zeros from the `numpy.zeros` `ndarray` and return each column
-        as a (column1, column2).
-        """
-        return np.trim_zeros(self.obj[:, 0]), np.trim_zeros(self.obj[:, 1])
-
-    def append(self, row: int, value1: float, value2: float) -> None:
-        """Append an element to a row in the internal `numpy.ndarray` using
-
-        Usage:
-            mat =  ResultsMatrix(rows=2)
-            mat.append(0, 1.0, 2.0)
-            # Would result in:
-            #  [[1., 2.], [0., 0.]]
+    def trim(self, shape: Union[Tuple[int, int], int]) -> None:
+        """Trim the internal `numpy.ndarray` to size.
 
         Args:
-            row: the row index to use.
-            value1: the value for the first column or [row, 0].
-            value2: the value for the second column or [row, 1].
+            shape: either a tuple to trim the array from the 0th index to the
+                   value or an int that will just trim the row.
 
         Returns:
             None
         """
-        self.obj[row, 0] = value1
-        self.obj[row, 1] = value2
-
-    def to_dict(self):
-        """
-        Return the dictionary representation of this `numpy.ndarray` as a list
-        with trimmed zeros and as a dictionary of `time` and `temp` keys.
-        """
-        return self.__dict__()
-
-    def __dict__(self):
-        """Internal __dict__ representation of object."""
-        time, temp = self.trim_zeros()
-        return {'time': time.tolist(), 'temp': temp.tolist()}
+        if isinstance(shape, tuple):
+            self.obj = self.obj[0:shape[0], 0:shape[1]]
+        else:
+            self.obj = self.obj[0:shape, :]
+        self.shape = self.obj.shape
 
     def __str__(self):
-        return f'ResultsMatrix(\n{self.obj.__str__()}\n)'
+        return f'DynamicNdarray(\n{self.obj.__str__()}\n)'
 
     def __repr__(self):
         return self.obj.__repr__()
+
+
+def to_plot_dict(array: DynamicNdarray) -> dict:
+    assert array.shape[1] == 2, 'Plot Dictionary Array shape must be (n, 2).'
+    return {
+        'time': array[:, 0],
+        'temp': array[:, 1]
+    }
 
 
 class ResultsData(object):
@@ -179,31 +182,26 @@ class ResultsData(object):
     stdout.
     """
     def __init__(self):
-        self.user_cooling_curve: Union[ResultsMatrix, None] = None
+        self.user_cooling_curve: Union[DynamicNdarray, None] = None
+        self.user_phase_fraction_data: Union[DynamicNdarray, None] = None
+        self.slider_time_field: float = 0.0
+        self.slider_temp_field: float = 0.0
 
-        self.ttt_fcs: Union[ResultsMatrix, None] = None
-        self.ttt_fcf: Union[ResultsMatrix, None] = None
-        self.ttt_pcs: Union[ResultsMatrix, None] = None
-        self.ttt_pcf: Union[ResultsMatrix, None] = None
-        self.ttt_bcs: Union[ResultsMatrix, None] = None
-        self.ttt_bcf: Union[ResultsMatrix, None] = None
-        self.ttt_msf: Union[ResultsMatrix, None] = None
+        self.ttt_fcs: Union[DynamicNdarray, None] = None
+        self.ttt_fcf: Union[DynamicNdarray, None] = None
+        self.ttt_pcs: Union[DynamicNdarray, None] = None
+        self.ttt_pcf: Union[DynamicNdarray, None] = None
+        self.ttt_bcs: Union[DynamicNdarray, None] = None
+        self.ttt_bcf: Union[DynamicNdarray, None] = None
+        self.ttt_msf: Union[DynamicNdarray, None] = None
 
-        self.cct_fcs: Union[ResultsMatrix, None] = None
-        self.cct_fcf: Union[ResultsMatrix, None] = None
-        self.cct_pcs: Union[ResultsMatrix, None] = None
-        self.cct_pcf: Union[ResultsMatrix, None] = None
-        self.cct_bcs: Union[ResultsMatrix, None] = None
-        self.cct_bcf: Union[ResultsMatrix, None] = None
-        self.cct_msf: Union[ResultsMatrix, None] = None
-
-    @classmethod
-    def from_json(cls):
-        pass
-
-    @classmethod
-    def from_csv(cls):
-        pass
+        self.cct_fcs: Union[DynamicNdarray, None] = None
+        self.cct_fcf: Union[DynamicNdarray, None] = None
+        self.cct_pcs: Union[DynamicNdarray, None] = None
+        self.cct_pcf: Union[DynamicNdarray, None] = None
+        self.cct_bcs: Union[DynamicNdarray, None] = None
+        self.cct_bcf: Union[DynamicNdarray, None] = None
+        self.cct_msf: Union[DynamicNdarray, None] = None
 
     def set_cct_plot_data(
         self, ferrite_nucleation, ferrite_completion, pearlite_nucleation,
@@ -229,69 +227,41 @@ class ResultsData(object):
         self.ttt_bcf = bainite_completion
         self.ttt_msf = martensite
 
-    def set_user_cool_plot_data(self, user_cooling_data) -> None:
-        self.user_cooling_curve = user_cooling_data
+    def set_user_cool_plot_data(
+            self, user_cooling_curve, user_phase_fraction_data,
+            slider_time_field, slider_temp_field
+    ) -> None:
+        self.user_cooling_curve = user_cooling_curve
+        self.user_phase_fraction_data = user_phase_fraction_data
+        self.slider_time_field = slider_time_field
+        self.slider_temp_field = slider_temp_field
 
     def get_cct_plot_data(self) -> dict:
         return {
-            'ferrite_nucleation': self.cct_fcs.to_dict(),
-            'ferrite_completion': self.cct_fcf.to_dict(),
-            'pearlite_nucleation': self.cct_pcs.to_dict(),
-            'pearlite_completion': self.cct_pcf.to_dict(),
-            'bainite_nucleation': self.cct_bcs.to_dict(),
-            'bainite_completion': self.cct_bcf.to_dict(),
-            'martensite': self.cct_msf.to_dict()
+            'ferrite_nucleation': to_plot_dict(self.cct_fcs.obj),
+            'ferrite_completion': to_plot_dict(self.cct_fcf),
+            'pearlite_nucleation': to_plot_dict(self.cct_pcs),
+            'pearlite_completion': to_plot_dict(self.cct_pcf),
+            'bainite_nucleation': to_plot_dict(self.cct_bcs),
+            'bainite_completion': to_plot_dict(self.cct_bcf),
+            'martensite': to_plot_dict(self.cct_msf)
         }
 
     def get_ttt_plot_data(self) -> dict:
         return {
-            'ferrite_nucleation': self.ttt_fcs.to_dict(),
-            'ferrite_completion': self.ttt_fcf.to_dict(),
-            'pearlite_nucleation': self.ttt_pcs.to_dict(),
-            'pearlite_completion': self.ttt_pcf.to_dict(),
-            'bainite_nucleation': self.ttt_bcs.to_dict(),
-            'bainite_completion': self.ttt_bcf.to_dict(),
-            'martensite': self.ttt_msf.to_dict()
+            'ferrite_nucleation': self.ttt_fcs,
+            'ferrite_completion': self.ttt_fcf,
+            'pearlite_nucleation': self.ttt_pcs,
+            'pearlite_completion': self.ttt_pcf,
+            'bainite_nucleation': self.ttt_bcs,
+            'bainite_completion': self.ttt_bcf,
+            'martensite': self.ttt_msf
         }
 
     def get_user_cool_plot_data(self) -> dict:
-        return self.user_cooling_curve.to_dict()
-
-    def plot_cct(self):
-        # plot with plotly
-        pass
-
-    def plot_ttt(self):
-        # plot with plotly
-        pass
-
-    @staticmethod
-    def to_json(data: dict):
-        pass
-
-    @staticmethod
-    def to_string(plot: str = ''):
-        if plot.lower() == 'cct':
-            # stringify cct
-            return 'cct'
-        elif plot.lower() == 'ttt':
-            # stringify ttt
-            return 'ttt'
-
-        return 'Error'
-
-    def to_csv(self, plot: str = '', path: str = '') -> None:
-        pass
-
-    @staticmethod
-    def to_pickle(plot: str = '', path: str = ''):
-        if plot.lower() == 'cct':
-            # pickle export cct
-            pass
-        elif plot.lower() == 'ttt':
-            # pickle export ttt
-            pass
-
-    def __str__(self):
-        # print something else other than data
-        pass
+        return {
+            'user_cooling_curve': to_plot_dict(self.user_cooling_curve),
+            'user_phase_fraction_data': self.user_phase_fraction_data.tolist(),
+            'slider_time_field': self.slider_time_field,
+            'slider_temp_field': self.slider_temp_field,
+        }

0785d8ed1a2dc4c7cbf75aae615867ae4fad0421
Unknown <arvy.au@gmail.com>
2019-08-21 23:52:04 +1000

    Fix import for UserPage and index.js for state

--- services/client/src/state/ducks/index.js
+++ services/client/src/state/ducks/index.js
@@ -1,3 +1,3 @@
 export { default as alloys } from './alloys/reducers'
 export { default as sim } from './sim/reducers'
-export { default as user } from './users/reducers'
+export { default as user } from './self/reducers'

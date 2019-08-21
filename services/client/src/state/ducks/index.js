2018b93cb4ebb5e94608e804fb921af32e323806
dalton <duong.daltonle@gmail.com>
2019-08-22 05:35:51 +1000

    Change user reducer to self reducer

--- services/client/src/state/ducks/index.js
+++ services/client/src/state/ducks/index.js
@@ -1,3 +1,3 @@
 export { default as alloys } from './alloys/reducers'
 export { default as sim } from './sim/reducers'
-export { default as user } from './self/reducers'
+export { default as self } from './self/reducers'

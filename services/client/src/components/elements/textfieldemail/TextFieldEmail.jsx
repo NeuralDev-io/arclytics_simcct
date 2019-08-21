0b9e6878c91c6cb0ca088e9e3fc3380042d0ffb5
dalton <duong.daltonle@gmail.com>
2019-08-22 09:35:27 +1000

    Fix TextFieldEmail styling

--- services/client/src/components/elements/textfieldemail/TextFieldEmail.jsx
+++ services/client/src/components/elements/textfieldemail/TextFieldEmail.jsx
@@ -13,6 +13,8 @@
 
 import React, { Component } from 'react'
 import PropTypes from 'prop-types'
+import XIcon from 'react-feather/dist/icons/x'
+import { buttonize } from '../../../utils/accessibility'
 
 import styles from './TextFieldEmail.module.scss'
 
@@ -27,77 +29,76 @@ class TextFieldEmail extends Component {
     }
   }
 
+  // Triggered when called in the 'handleKeyDown' function
+  isValid = (email) => {
+    let error = null
+
+    // calls isEmail function to check if what user wrote is an email
+    if (!this.isEmail(email)) {
+      error = `"${email}" is not a valid email address`
+    }
+
+    // calls isInList function to check if what user wrote already exists in emails[] array
+    if (this.isInList(email)) {
+      error = `"${email}" has already been added`
+    }
+
+    // checks if an error exists in any of the above validations
+    if (error) {
+      this.setState({ error })
+      return false
+    }
+    return true
+  }
+
+  // checks if the email contains an @ symbol and at least one period
+  isEmail = (email) => {
+    return /[\w\d\.-]+@[\w\d\.-]+\.[\w\d\.-]+/.test(email)
+  }
+
+  // checks if email already exists in the emails[] array
+  isInList = (email) => {
+    const { emails } = this.state
+    return emails.includes(email)
+  }
+
   handleChange = (e) => {
-      this.setState({
-        value: e.target.value,
-        // sets error to null so that after error when a user starts to type, the error disappears
-        error: null,
-      })
+    this.setState({
+      value: e.target.value,
+      // sets error to null so that after error when a user starts to type, the error disappears
+      error: null,
+    })
   }
 
   // triggered when one of the following keys is pressed: ('Enter', 'Tab', 'Space', 'Comma')
   handleKeyDown = (e) => {
+    const { value } = this.state
     if (['Enter', 'Tab', 'Space', ','].includes(e.key)) {
       /**
        * The preventDefault() method cancels the event if it is cancellable.
        * This essentially stops the keys from doing their original function
        * e.g. stops tab from going to next input, stops enter from submitting
        * the form, etc.
-       **/
+       */
       e.preventDefault()
-      const email = this.state.value.trim()
+      const email = value.trim()
 
       // the following validates the email and check that input is not empty
       if (email && this.isValid(email)) {
-        this.setState({
-          emails: [...this.state.emails, email],
+        this.setState(prevState => ({
+          emails: [...prevState.emails, email],
           value: '',
-        })
+        }))
       }
     }
   }
 
   // Triggered when the delete button next to an email is pressed
   handleDelete = (toBeRemoved) => {
-    this.setState({
-      //delete emails equal to the toBeRemoved argument
-      emails: this.state.emails.filter(email => email !== toBeRemoved)
-    })
-  }
-
-  // Triggered when called in the 'handleKeyDown' function
-  isValid(email){
-    let error = null
-
-    // calls isEmail function to check if what user wrote is an email
-    if (!this.isEmail(email)) {
-      error = '"' + email + '" is not a valid email address.'
-    }
-
-    // calls isInList function to check if what user wrote already exists in emails[] array
-    if (this.isInList(email)) {
-      error = '"' + email + '" has already been added.'
-    }
-
-    // checks if an error exists in any of the above validations
-    if (error) {
-      this.setState({
-        error
-      })
-      return false
-    }
-
-    return true
-  }
-
-  // checks if the email contains an @ symbol and at least one period
-  isEmail(email){
-    return /[\w\d\.-]+@[\w\d\.-]+\.[\w\d\.-]+/.test(email)
-  }
-
-  // checks if email already exists in the emails[] array
-  isInList(email){
-    return this.state.emails.includes(email)
+    this.setState(prevState => ({
+      // delete emails equal to the toBeRemoved argument
+      emails: prevState.emails.filter(email => email !== toBeRemoved),
+    }))
   }
 
   /**
@@ -105,21 +106,21 @@ class TextFieldEmail extends Component {
    *
    * validation is also done here and not above because if the email is incorrect
    * on paste, funky things will happen.
-   **/
+   */
   handlePaste = (e) => {
     // prevent default to stop email from being pasted as validation needs to be done before this.
     e.preventDefault()
 
-    let paste = e.clipboardData.getData('text') // get the clipboard data
-    let emails = paste.match(/[\w\d\.-]+@[\w\d\.-]+\.[\w\d\.-]+/g) // validate email format
+    const paste = e.clipboardData.getData('text') // get the clipboard data
+    const emails = paste.match(/[\w\d\.-]+@[\w\d\.-]+\.[\w\d\.-]+/g) // validate email format
 
     // check paste is not empty and add it to array
     if (emails) {
-      let toBeAdded = emails.filter(email => !this.isInList(email))
+      const toBeAdded = emails.filter(email => !this.isInList(email))
 
-      this.setState({
-        emails: [...this.state.emails, ...toBeAdded]
-      })
+      this.setState(prevState => ({
+        emails: [...prevState.emails, ...toBeAdded],
+      }))
     }
   }
 
@@ -129,40 +130,38 @@ class TextFieldEmail extends Component {
       isDisabled = false,
       type = 'text',
       className = '',
-      value = '',
+      // value = '',
       length = 'default',
       name,
       ...other
     } = this.props
+    const { emails, value, error } = this.state
     const classname = `${styles.input} ${length === 'default' ? '' : styles[length]} ${className || ''}`
 
     // TODO: ADD STYLING
     return (
       <div>
-        {this.state.emails.map(email => (
-          <div className="emailItem" key={email}>
-            {email}&nbsp;
-            <button
-              type="button"
-              onClick={() => this.handleDelete(email)}
-            >
-              &times;
-            </button>
-          </div>
-        ))}
+        <div className={styles.emails}>
+          {emails.map(email => (
+            <div className={styles.emailItem} key={email} {...buttonize(() => this.handleDelete(email))}>
+              <span>{email}</span>
+              <XIcon className={styles.icon} />
+            </div>
+          ))}
+        </div>
         <input
           {...other}
           type={type}
           className={classname}
           placeholder={placeholder}
           name={name}
-          value={this.state.value}
+          value={value}
           onChange={e => this.handleChange(e)}
           onKeyDown={e => this.handleKeyDown(e)}
           onPaste={e => this.handlePaste(e)}
           disabled={isDisabled}
         />
-        <span className="emailError">{this.state.error}</span>
+        <span className="emailError">{error}</span>
       </div>
     )
   }

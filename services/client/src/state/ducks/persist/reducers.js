69a25e596bab0f758a9c39fbc0aac38f16e0b867
Andrew Che <andrew@codeninja55.me>
2019-08-26 21:49:22 +1000

    Cleaning up persist

--- services/client/src/state/ducks/persist/reducers.js
+++ services/client/src/state/ducks/persist/reducers.js
@@ -1,44 +1,7 @@
-import {
-  GET_USER_PROFILE,
-  CREATE_USER_PROFILE,
-  UPDATE_USER_PROFILE, 
-} from './types'
-const initialState = {
-  user: {
-    admin: false,
-    profile: null,
-  },
-}
+const initialState = {}
 
 const reducer = (state = initialState, action) => {
   switch (action.type) {
-    case GET_USER_PROFILE:
-      return {
-        ...state,
-        user: {
-          ...state.user,
-          ...action.payload,
-        },
-      }
-    case CREATE_USER_PROFILE:
-      return {
-        ...state,
-        user: {
-          ...state.user,
-          profile: {
-            ...state.profile,
-            ...action.payload,
-          },
-        },
-      }
-    case UPDATE_USER_PROFILE:
-      return {
-        ...state,
-        user: {
-          ...state.user,
-          ...action.payload,
-        },
-      }
     default:
       return state
   }

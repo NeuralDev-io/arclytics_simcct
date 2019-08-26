2980d3094ec2a260c892fef0433e8856af0a1ea9
Andrew Che <andrew@codeninja55.me>
2019-08-27 00:37:14 +1000

    Adding user persist Redux action back into code

--- services/client/src/state/ducks/persist/actions.js
+++ services/client/src/state/ducks/persist/actions.js
@@ -1,5 +1,27 @@
+import { GET_PERSIST_USER_STATUS } from './types'
 
 // MOVED TO src/state/ducks/self
 // export const getUserProfile = () => (dispatch) => {}
 // export const createUserProfile = values => (dispatch) => {}
 // export const updateUserProfile = values => (dispatch) => {}
+
+export const getPersistUserStatus = () => (dispatch) => { // eslint-disable-line
+  return fetch('http://localhost:8000/auth/status', {
+    method: 'GET',
+    headers: {
+      'Content-Type': 'application/json',
+      Authorization: `Bearer ${localStorage.getItem('token')}`,
+    },
+  })
+    .then(res => res.json())
+    .then((data) => {
+      if (data.status === 'fail') throw new Error(data.message)
+      if (data.status === 'success') {
+        dispatch({
+          type: GET_PERSIST_USER_STATUS,
+          payload: data.data,
+        })
+      }
+    })
+    .catch(err => console.log(err))
+}
---
+++ services/client/src/state/ducks/persist/types.js
@@ -0,0 +1,2 @@
+// eslint-disable-next-line import/prefer-default-export
+export const GET_PERSIST_USER_STATUS = 'persist/user/GET_PERSIST_USER_STATUS'

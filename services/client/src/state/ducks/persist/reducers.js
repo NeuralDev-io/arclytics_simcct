7ba7a02cb8cc0c326f24d95c9e65b71f1f00aae1
Andrew Che <andrew@codeninja55.me>
2019-08-27 00:37:14 +1000

    Adding user persist Redux action back into code

--- services/client/src/state/ducks/persist/reducers.js
+++ services/client/src/state/ducks/persist/reducers.js
@@ -1,10 +1,26 @@
-const initialState = {}
+import {
+  GET_PERSIST_USER_STATUS,
+} from './types'
+
+const initialState = {
+  userStatus: {
+    admin: false,
+    isProfile: false,
+    verified: false,
+  },
+}
 
 const reducer = (state = initialState, action) => {
-  switch (action.type) {
-    default:
-      return state
+  if (action.type === GET_PERSIST_USER_STATUS) {
+    return {
+      ...state,
+      userStatus: {
+        ...state.user,
+        ...action.payload,
+      },
+    }
   }
+  return state
 }
 
 export default reducer

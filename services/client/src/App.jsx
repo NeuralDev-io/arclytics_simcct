30ff6fd5b70fb46fa9eb962e2c2217cbf4b0adfd
dalton <duong.daltonle@gmail.com>
2019-09-03 13:34:28 +1000

    Add error boundary component for App

--- services/client/src/App.jsx
+++ services/client/src/App.jsx
@@ -3,6 +3,7 @@ import { Provider } from 'react-redux'
 import { Route, Switch } from 'react-router-dom'
 import { SnackbarProvider } from 'notistack'
 import store from './state/store'
+import ErrorBoundary from './components/pages/error-boundary/ErrorBoundary'
 import LoginPage from './components/pages/login/LoginPage'
 import SignupPage from './components/pages/signup/SignupPage'
 import SimulationPage from './components/pages/simulation'
@@ -16,55 +17,57 @@ import './App.scss'
 
 function App() {
   return (
-    <SnackbarProvider
-      maxSnack={3}
-      anchorOrigin={{
-        vertical: 'top',
-        horizontal: 'center',
-      }}
-    >
-      <Provider store={store}>
-        <div className="App">
-          <Switch>
-            <Route
-              path="/signin"
-              render={props => <LoginPage {...props} />}
-            />
-            <Route
-              path="/signup"
-              render={props => <SignupPage {...props} />}
-            />
-            <Route
-              exact
-              path="/"
-              render={props => <SimulationPage {...props} />}
-            />
-            <Route
-              exact
-              path="/user/simulations"
-              render={props => <UserSimulationPage {...props} />}
-            />
-            <Route
-              exact
-              path="/user/alloys"
-              render={props => <UserAlloyPage {...props} />}
-            />
-            <Route
-              path="/profileQuestions"
-              render={props => <ProfileQuestionsPage {...props} />}
-            />
-            <Route
-              path="/admin"
-              render={props => <AdminPage {...props} />}
-            />
-            <Route
-              path="/user/profile"
-              render={props => <ProfilePage {...props} />}
-            />
-          </Switch>
-        </div>
-      </Provider>
-    </SnackbarProvider>
+    <ErrorBoundary>
+      <SnackbarProvider
+        maxSnack={3}
+        anchorOrigin={{
+          vertical: 'top',
+          horizontal: 'center',
+        }}
+      >
+        <Provider store={store}>
+          <div className="App">
+            <Switch>
+              <Route
+                path="/signin"
+                render={props => <LoginPage {...props} />}
+              />
+              <Route
+                path="/signup"
+                render={props => <SignupPage {...props} />}
+              />
+              <Route
+                exact
+                path="/"
+                render={props => <SimulationPage {...props} />}
+              />
+              <Route
+                exact
+                path="/user/simulations"
+                render={props => <UserSimulationPage {...props} />}
+              />
+              <Route
+                exact
+                path="/user/alloys"
+                render={props => <UserAlloyPage {...props} />}
+              />
+              <Route
+                path="/profileQuestions"
+                render={props => <ProfileQuestionsPage {...props} />}
+              />
+              <Route
+                path="/admin"
+                render={props => <AdminPage {...props} />}
+              />
+              <Route
+                path="/user/profile"
+                render={props => <ProfilePage {...props} />}
+              />
+            </Switch>
+          </div>
+        </Provider>
+      </SnackbarProvider>
+    </ErrorBoundary>
   )
 }
 

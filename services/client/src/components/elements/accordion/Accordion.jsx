88ecb420680e1db2cb668f1bd13620a3f49a7ffd
dalton <duong.daltonle@gmail.com>
2019-09-03 14:24:51 +1000

    Make Accordion Component manage its own expand state

--- services/client/src/components/elements/accordion/Accordion.jsx
+++ services/client/src/components/elements/accordion/Accordion.jsx
@@ -1,15 +1,44 @@
-import React, { PureComponent } from 'react'
+import React, { Component } from 'react'
 import PropTypes from 'prop-types'
 
 import styles from './Accordion.module.scss'
 
-class Accordion extends PureComponent {
+class Accordion extends Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      expand: [],
+    }
+  }
+
+  componentDidMount = () => {
+    const { expand } = this.state
+    const { children } = this.props
+    if (expand.length === 0) {
+      this.setState({ expand: new Array(children.length).fill(false) })
+    }
+  }
+
+  handleToggle = (index) => {
+    this.setState((state) => {
+      const newExpand = state.expand
+      newExpand[index] = !state.expand[index]
+      return { expand: newExpand }
+    })
+  }
+
   render() {
     const { children } = this.props
+    const { expand } = this.state
 
     return (
       <div className={styles.container}>
-        {children}
+        {React.Children.map(children, (child, i) => (
+          React.cloneElement(child, {
+            onToggle: () => this.handleToggle(i),
+            expanded: expand[i],
+          })
+        ))}
       </div>
     )
   }

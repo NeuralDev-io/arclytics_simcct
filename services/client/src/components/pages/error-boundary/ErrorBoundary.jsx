49b63496a58cdc4ab8d714d89311b6b539088f52
dalton <duong.daltonle@gmail.com>
2019-09-03 13:34:28 +1000

    Add error boundary component for App

---
+++ services/client/src/components/pages/error-boundary/ErrorBoundary.jsx
@@ -0,0 +1,50 @@
+import React from 'react'
+import PropTypes from 'prop-types'
+
+// TODO: add an error screen and add logging function
+class ErrorBoundary extends React.Component {
+  constructor(props) {
+    super(props)
+    this.state = { error: null, errorInfo: null }
+  }
+
+  static getDerivedStateFromError(error) {
+    // Update state so the next render will show the fallback UI.
+    return { error }
+  }
+
+  componentDidCatch(error, errorInfo) {
+    // Catch errors in any components below and re-render with error message
+    this.setState({
+      error,
+      errorInfo,
+    })
+    // Log error messages to logger here
+  }
+
+  render() {
+    const { error, errorInfo } = this.state
+    const { children } = this.props
+    if (errorInfo) {
+      // Error path
+      return (
+        <div>
+          <h2>Something went wrong.</h2>
+          <details style={{ whiteSpace: 'pre-wrap' }}>
+            {error && error.toString()}
+            <br />
+            {errorInfo.componentStack}
+          </details>
+        </div>
+      )
+    }
+    // Normally, just render children
+    return children
+  }
+}
+
+ErrorBoundary.propTypes = {
+  children: PropTypes.node.isRequired,
+}
+
+export default ErrorBoundary

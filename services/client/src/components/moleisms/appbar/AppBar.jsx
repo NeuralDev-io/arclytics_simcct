74294752d707e15fb2efd34b5a1fb7c187fd95dc
Xaraox <arvy.au@gmail.com>
2019-09-06 22:28:03 +1000

    Add tooltip to appbar

--- services/client/src/components/moleisms/appbar/AppBar.jsx
+++ services/client/src/components/moleisms/appbar/AppBar.jsx
@@ -2,14 +2,15 @@ import React from 'react'
 import PropTypes from 'prop-types'
 import { connect } from 'react-redux'
 import HardDriveIcon from 'react-feather/dist/icons/hard-drive'
-import {ReactComponent as AnstoLogo} from '../../../assets/ANSTO_Logo_SVG/logo.svg'
 import HelpIcon from 'react-feather/dist/icons/help-circle'
 import MonitorIcon from 'react-feather/dist/icons/monitor'
 import UserIcon from 'react-feather/dist/icons/user'
 import LogOutIcon from 'react-feather/dist/icons/log-out'
 import DatabaseIcon from 'react-feather/dist/icons/database'
 import SlidersIcon from 'react-feather/dist/icons/sliders'
+import { ReactComponent as AnstoLogo } from '../../../assets/ANSTO_Logo_SVG/logo.svg'
 import { ReactComponent as Logo } from '../../../assets/logo_20.svg'
+import Tooltip from '../../elements/tooltip'
 import { logout } from '../../../utils/AuthenticationHelper'
 import { buttonize } from '../../../utils/accessibility'
 
@@ -26,28 +27,40 @@ const AppBar = (props) => {
           className={`${styles.navIcon} ${active === 'sim' && styles.active}`}
           href="/"
         >
-          <SlidersIcon className={styles.icon} />
+          <Tooltip className={styles.tooltip} position="right">
+            <SlidersIcon className={styles.icon} />
+            <p> Simulation </p>
+          </Tooltip>
         </a>
         <a
           id="savedSimulations"
           className={`${styles.navIcon} ${active === 'savedSimulations' && styles.active}`}
           href="/user/simulations"
         >
-          <HardDriveIcon className={styles.icon} />
+          <Tooltip className={styles.tooltip} position="right">
+            <HardDriveIcon className={styles.icon} />
+            <p> Saved simulations </p>
+          </Tooltip>
         </a>
         <a
           id="alloys"
           className={`${styles.navIcon} ${active === 'userAlloys' && styles.active}`}
           href="/user/alloys"
         >
-          <DatabaseIcon className={styles.icon} />
+          <Tooltip className={styles.tooltip} position="right">
+            <DatabaseIcon className={styles.icon} />
+            <p> Alloy database </p>
+          </Tooltip>
         </a>
         <a
           id="help"
           className={`${styles.navIcon} ${active === 'edu' && styles.active}`}
           href="/"
         >
-          <HelpIcon className={styles.icon} />
+          <Tooltip className={styles.tooltip} position="right">
+            <HelpIcon className={styles.icon} />
+            <p> Help </p>
+          </Tooltip>
         </a>
         <a
           id="admin"
@@ -55,7 +68,10 @@ const AppBar = (props) => {
           style={{ display: user.admin ? 'flex' : 'none' }}
           href="/admin/analytics"
         >
-          <MonitorIcon className={styles.icon} />
+          <Tooltip className={styles.tooltip} position="right">
+            <MonitorIcon className={styles.icon} />
+            <p> Admin </p>
+          </Tooltip>
         </a>
       </div>
       <div>
@@ -64,15 +80,21 @@ const AppBar = (props) => {
           className={`${styles.navIcon} ${active === 'profile' && styles.active}`}
           href="/user/profile"
         >
-          <UserIcon className={styles.icon} />
+          <Tooltip className={styles.tooltip} position="right">
+            <UserIcon className={styles.icon} />
+            <p> Profile </p>
+          </Tooltip>
         </a>
         <div
           id="logout"
           className={styles.navIcon}
           {...buttonize(() => logout(redirect))}
         >
-          <LogOutIcon className={styles.icon} />
-
+          <Tooltip className={styles.tooltip} position="right">
+            <LogOutIcon className={styles.icon} />
+            <p> Logout </p>
+          </Tooltip>
+          
         </div>
         <Logo className={styles.logo} />
       </div>

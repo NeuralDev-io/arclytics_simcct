0fd03f43997361abc7979c4c4ca4fdfbecb90294
Andrew Che <andrew@neuraldev.io>
2019-09-07 03:36:28 +1000

    Adding mongomock and fix some user tests

--- services/simcct/tests/test_api_users.py
+++ services/simcct/tests/test_api_users.py
@@ -52,24 +52,6 @@ def log_test_user_in(self, user: User, password: str) -> str:
 
 class TestUserService(BaseTestCase):
     """Tests for the User API service."""
-    tony = None
-    tonys_password = 'iamironman'
-
-    # @classmethod
-    # def setUpClass(cls) -> None:
-    #     cls.tony = User(
-    #         **{
-    #             'email': 'ironman@avengers.com',
-    #             'first_name': 'Tony',
-    #             'last_name': 'Stark'
-    #         }
-    #     )
-    #     cls.tony.set_password('iamironman')
-    #     cls.tony.save()
-    #
-    # @classmethod
-    # def tearDownClass(cls) -> None:
-    #     cls.tony.delete()
 
     def test_ping(self):
         """Ensure the /ping route behaves correctly."""
@@ -91,7 +73,7 @@ class TestUserService(BaseTestCase):
         tony.set_password('IAmTheRealIronMan')
         tony.save()
 
-        with current_app.test_client() as client:
+        with self.client as client:
             # ALREADY SET BUT HERE IT IS FOR PRINTING IF YOU WANT
             cookie = test_login(client, tony.email, 'IAmTheRealIronMan')
 
@@ -113,6 +95,7 @@ class TestUserService(BaseTestCase):
         )
         tony.set_password('iamironman')
         tony.save()
+
         with self.client:
             test_login(self.client, tony.email, 'iamironman')
             response = self.client.get('/api/v1/sim/auth/status')
@@ -170,199 +153,147 @@ class TestUserService(BaseTestCase):
             self.assertIn('Session token is not valid.', data['message'])
             self.assertIn('fail', data['status'])
 
-    # def test_single_user_incorrect_id(self):
-    #     """Ensure error is thrown if the id does not exist."""
-    #     with self.client:
-    #         _id = ObjectId()
-    #         response = self.client.get('/api/v1/sim/user')
-    #         data = json.loads(response.data.decode())
-    #         self.assertEqual(response.status_code, 401)
-    #         self.assertIn('Provide a valid JWT auth token.', data['message'])
-    #         self.assertIn('fail', data['status'])
+    def test_get_all_users_no_cookies(self):
+        with self.client:
+            resp = self.client.get(
+                '/api/v1/sim/users', content_type='application/json'
+            )
+            data = json.loads(resp.data.decode())
+            self.assertEqual(resp.status_code, 401)
+            self.assertIn('fail', data['status'])
+            self.assertNotIn('data', data)
+            self.assertIn('Session token is not valid.', data['message'])
 
-    # def test_single_user_expired_token(self):
-    #     tony = User(
-    #         **{
-    #             'email': 'tony@starkindustries.com',
-    #             'first_name': 'Tony',
-    #             'last_name': 'Stark'
-    #         }
-    #     )
-    #     tony.set_password('IAmTheRealIronMan')
-    #     tony.save()
-    #     current_app.config['TOKEN_EXPIRATION_SECONDS'] = -1
-    #     with self.client:
-    #         resp_login = self.client.post(
-    #             '/api/v1/sim/auth/login',
-    #             data=json.dumps(
-    #                 {
-    #                     'email': 'tony@starkindustries.com',
-    #                     'password': 'IAmTheRealIronMan'
-    #                 }
-    #             ),
-    #             content_type='application/json'
-    #         )
-    #         # invalid token logout
-    #         token = json.loads(resp_login.data.decode())['token']
-    #         response = self.client.get(
-    #             '/user'.format(tony.id),
-    #             headers={
-    #                 'Authorization': 'Bearer {token}'.format(token=token)
-    #             }
-    #         )
-    #         data = json.loads(response.data.decode())
-    #         self.assertEqual('fail', data['status'])
-    #         self.assertEqual(
-    #             'Signature expired. Please login again.', data['message']
-    #         )
-    #         self.assertEqual(response.status_code, 401)
-    #
-    # def test_get_all_users_no_header(self):
-    #     with self.client:
-    #         resp = self.client.get(
-    #             '/api/v1/sim/users', content_type='application/json', headers={}
-    #         )
-    #         data = json.loads(resp.data.decode())
-    #         self.assertEqual(resp.status_code, 401)
-    #         self.assertIn('fail', data['status'])
-    #         self.assertNotIn('data', data)
-    #         self.assertIn('Provide a valid JWT auth token.', data['message'])
-    #
-    # def test_unauthorized_get_all_users(self):
-    #     """Ensure we can't get all users because we are not authorized."""
-    #     tony = User(
-    #         **{
-    #             'email': 'tony@starkindustries.com',
-    #             'first_name': 'Tony',
-    #             'last_name': 'Stark'
-    #         }
-    #     )
-    #     tony.set_password('IAmTheRealIronMan')
-    #     tony.save()
-    #     nat = User(
-    #         **{
-    #             'email': 'nat@shield.gov.us',
-    #             'first_name': 'Natasha',
-    #             'last_name': 'Romanoff'
-    #         }
-    #     )
-    #     nat.set_password('IveGotRedInMyLedger')
-    #     nat.save()
-    #
-    #     token = log_test_user_in(self, tony, 'IAmTheRealIronMan')
-    #
-    #     with self.client:
-    #         resp = self.client.get(
-    #             '/api/v1/sim/users',
-    #             content_type='application/json',
-    #             headers={'Authorization': 'Bearer {}'.format(token)}
-    #         )
-    #         data = json.loads(resp.data.decode())
-    #         self.assertEqual(resp.status_code, 403)
-    #         self.assertIn('fail', data['status'])
-    #         self.assertNotIn('data', data)
-    #         self.assertIn('Not authorized.', data['message'])
-    #
-    # def test_get_all_users_expired_token(self):
-    #     thor = User(
-    #         **{
-    #             'email': 'thor@avengers.io',
-    #             'first_name': 'Thor',
-    #             'last_name':'Odinson'
-    #         }
-    #     )
-    #     thor.set_password('StrongestAvenger')
-    #     # thor.is_admin = True
-    #     thor.admin_profile = AdminProfile(
-    #         position='Position',
-    #         mobile_number=None,
-    #         verified=True,
-    #         promoted_by=None
-    #     )
-    #     thor.save()
-    #     with self.client:
-    #         resp_login = self.client.post(
-    #             '/api/v1/sim/auth/login',
-    #             data=json.dumps(
-    #                 {
-    #                     'email': 'thor@avengers.io',
-    #                     'password': 'StrongestAvenger'
-    #                 }
-    #             ),
-    #             content_type='application/json'
-    #         )
-    #         # token = json.loads(resp_login.data.decode())['token']
-    #         token = 'KJASlkdjlkajsdlkjlkasjdlkjalosd'
-    #         resp = self.client.get(
-    #             '/users',
-    #             headers={
-    #                 'Authorization': 'Bearer {token}'.format(token=token)
-    #             }
-    #         )
-    #         data = json.loads(resp.data.decode())
-    #         self.assertEqual('fail', data['status'])
-    #         self.assertNotIn('data', data)
-    #         self.assertEqual(
-    #             'Invalid token. Please log in again.', data['message']
-    #         )
-    #         self.assertEqual(resp.status_code, 401)
-    #
-    # def test_get_all_users(self):
-    #     """Ensure we can get all users if logged in and authorized."""
-    #     tony = User(
-    #         **{
-    #             'email': 'tony@starkindustries.com',
-    #             'first_name': 'Tony',
-    #             'last_name': 'Stark'
-    #         }
-    #     )
-    #     tony.set_password('IAmTheRealIronMan')
-    #     # tony.is_admin = True
-    #     tony.admin_profile = AdminProfile(
-    #         position='Position',
-    #         mobile_number=None,
-    #         verified=True,
-    #         promoted_by=None
-    #     )
-    #     tony.verified = True
-    #     tony.save()
-    #     steve = User(
-    #         **{
-    #             'email': 'steve@avengers.io',
-    #             'first_name': 'Steve',
-    #             'last_name': 'Rogers'
-    #         }
-    #     )
-    #     steve.set_password('ICanDoThisAllDay')
-    #     steve.save()
-    #     nat = User(
-    #         **{
-    #             'email': 'nat@shield.gov.us',
-    #             'first_name': 'Natasha',
-    #             'last_name': 'Romanoff'
-    #         }
-    #     )
-    #     nat.set_password('IveGotRedInMyLedger')
-    #     nat.verified = True
-    #     nat.save()
-    #
-    #     token = log_test_user_in(self, tony, 'IAmTheRealIronMan')
-    #
-    #     with self.client:
-    #         tony.reload()
-    #         self.assertTrue(tony.active)
-    #         resp = self.client.get(
-    #             '/users',
-    #             content_type='application/json',
-    #             headers={'Authorization': 'Bearer {}'.format(token)}
-    #         )
-    #         data = json.loads(resp.data.decode())
-    #         self.assertEqual(resp.status_code, 200)
-    #         self.assertEqual(len(data['data']['users']), 3)
-    #         self.assertTrue(data['data']['users'][0]['admin'])
-    #         self.assertFalse(data['data']['users'][1]['admin'])
-    #         self.assertIn('success', data['status'])
-    #
+    def test_unauthorized_get_all_users(self):
+        """Ensure we can't get all users because we are not authorized."""
+        tony = User(
+            **{
+                'email': 'tony@starkindustries.com',
+                'first_name': 'Tony',
+                'last_name': 'Stark'
+            }
+        )
+        tony.set_password('IAmTheRealIronMan')
+        tony.save()
+        nat = User(
+            **{
+                'email': 'nat@shield.gov.us',
+                'first_name': 'Natasha',
+                'last_name': 'Romanoff'
+            }
+        )
+        nat.set_password('IveGotRedInMyLedger')
+        nat.save()
+
+        with self.client:
+            test_login(self.client, tony.email, 'IAmTheRealIronMan')
+            resp = self.client.get(
+                '/api/v1/sim/users',
+                content_type='application/json',
+            )
+            data = json.loads(resp.data.decode())
+            self.assertEqual(resp.status_code, 403)
+            self.assertIn('fail', data['status'])
+            self.assertNotIn('data', data)
+            self.assertIn('Not authorized.', data['message'])
+
+    def test_get_all_users_invalid_token(self):
+        thor = User(
+            **{
+                'email': 'thor@avengers.io',
+                'first_name': 'Thor',
+                'last_name': 'Odinson'
+            }
+        )
+        thor.set_password('StrongestAvenger')
+        # thor.is_admin = True
+        thor.admin_profile = AdminProfile(
+            position='Position',
+            mobile_number=None,
+            verified=True,
+            promoted_by=None
+        )
+        thor.save()
+        with self.client as client:
+            resp_login = client.post(
+                '/api/v1/sim/auth/login',
+                data=json.dumps({
+                    'email': thor.email,
+                    'password': 'StrongestAvenger'
+                }),
+                content_type='application/json'
+            )
+
+            cookie = next(
+                (cookie for cookie in client.cookie_jar if
+                 cookie.name == 'SESSION_TOKEN'),
+                None
+            )
+
+            cookie_value = str(cookie.value)[3:-1]
+            client.cookie_jar.clear()
+            client.set_cookie('localhost', 'BAD_KEY', cookie_value)
+
+            resp = self.client.get('/api/v1/sim/users')
+            data = json.loads(resp.data.decode())
+            self.assertEqual('fail', data['status'])
+            self.assertNotIn('data', data)
+            self.assertEqual('Session token is not valid.', data['message'])
+            self.assertEqual(resp.status_code, 401)
+
+    def test_get_all_users(self):
+        """Ensure we can get all users if logged in and authorized."""
+        tony = User(
+            **{
+                'email': 'tony@starkindustries.com',
+                'first_name': 'Tony',
+                'last_name': 'Stark'
+            }
+        )
+        tony.set_password('IAmTheRealIronMan')
+        # tony.is_admin = True
+        tony.admin_profile = AdminProfile(
+            position='Position',
+            mobile_number=None,
+            verified=True,
+            promoted_by=None
+        )
+        tony.verified = True
+        tony.save()
+        steve = User(
+            **{
+                'email': 'steve@avengers.io',
+                'first_name': 'Steve',
+                'last_name': 'Rogers'
+            }
+        )
+        steve.set_password('ICanDoThisAllDay')
+        steve.save()
+        nat = User(
+            **{
+                'email': 'nat@shield.gov.us',
+                'first_name': 'Natasha',
+                'last_name': 'Romanoff'
+            }
+        )
+        nat.set_password('IveGotRedInMyLedger')
+        nat.verified = True
+        nat.save()
+
+        with self.client as client:
+            test_login(client, tony.email, 'IAmTheRealIronMan')
+            self.assertTrue(tony.active)
+            resp = client.get(
+                '/api/v1/sim/users',
+                content_type='application/json',
+            )
+            data = json.loads(resp.data.decode())
+            self.assertEqual(resp.status_code, 200)
+            self.assertEqual(len(data['data']['users']), 3)
+            self.assertTrue(data['data']['users'][0]['admin'])
+            self.assertFalse(data['data']['users'][1]['admin'])
+            self.assertIn('success', data['status'])
+
     # def test_patch_user(self):
     #     """Test update user details"""
     #     obiwan = User(
@@ -926,6 +857,7 @@ class TestUserService(BaseTestCase):
     #     kylo.set_password('LetStarWarsDie')
     #     kylo.save()
     #
+
 
 if __name__ == '__main__':
     unittest.main()

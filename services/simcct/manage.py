6a6a9270e37053ee4db2f784e407ef6cb021681f
Andrew Che <andrew@codeninja55.me>
2019-09-07 17:49:16 +1000

    Finishing the Users API test

--- services/simcct/manage.py
+++ services/simcct/manage.py
@@ -78,7 +78,9 @@ def flush():
     """Drop all collections in the database."""
     from mongoengine.connection import get_db
     db = get_db('default')
-    print('Dropping <{}.{}> database:'.format(db.name, 'users'),file=sys.stderr)
+    print(
+        'Dropping <{}.{}> database:'.format(db.name, 'users'), file=sys.stderr
+    )
     db.users.drop()
 
 
@@ -107,7 +109,8 @@ def flush():
             port=int(os.environ.get('MONGO_PORT'))
         )
         redis_client = redis.Redis(
-            host=os.environ.get('REDIS_HOST'), port=os.environ.get('REDIS_PORT')
+            host=os.environ.get('REDIS_HOST'),
+            port=os.environ.get('REDIS_PORT')
         )
         print('Dropping <{}> database:'.format('arc_dev'), file=sys.stderr)
         client.drop_database('arc_dev')
@@ -228,7 +231,7 @@ def seed_alloy_db():
 @cli.command()
 def test():
     """Runs the tests without code coverage."""
-    tests = unittest.TestLoader().discover('tests', pattern='test_api_global*.py')
+    tests = unittest.TestLoader().discover('tests', pattern='test_api_*.py')
     result = unittest.TextTestRunner(verbosity=3).run(tests)
     if result.wasSuccessful():
         return 0

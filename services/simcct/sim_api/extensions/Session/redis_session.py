5cfac7067aef5d9de75613f10250b80e0de4c561
Andrew Che <andrew@codeninja55.me>
2019-09-08 16:14:16 +1000

    Minor fixes and cleanup

--- services/simcct/sim_api/extensions/Session/redis_session.py
+++ services/simcct/sim_api/extensions/Session/redis_session.py
@@ -118,15 +118,14 @@ class RedisSessionInterface(SessionInterface):
 
         data = json.loads(redis_value.decode())
 
-        logger.debug(f'open_session: {data}')
-
-        # if str(ip_address) != data.get('ip_address', ''):
-        #     logger.debug(
-        #         f'Some idiot tried to access from a different IP.\n'
-        #         f'Session IP: {data["ip_address"]}\n'
-        #         f'Request IP: {ip_address}'
-        #     )
-        #     return self._new_session()
+        if str(ip_address) != data.get('ip_address', ''):
+            logger.info(f'open_session: {request.environ}')
+            logger.debug(
+                f'Some idiot tried to access from a different IP.\n'
+                f'Session IP: {data["ip_address"]}\n'
+                f'Request IP: {ip_address}'
+            )
+            return self._new_session()
 
         return RedisSession(data, sid=sid)
 

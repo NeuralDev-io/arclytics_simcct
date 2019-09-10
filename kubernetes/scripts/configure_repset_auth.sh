cb028ae5d5fbfeca7323a5d463de4688fe71df59
Andrew Che <andrew@neuraldev.io>
2019-09-10 12:53:41 +1000

    Fixing authentication database to be `admin` as it should be

--- kubernetes/scripts/configure_repset_auth.sh
+++ kubernetes/scripts/configure_repset_auth.sh
@@ -51,7 +51,7 @@ APP_USER_PW=$(<"${TEMPFILE_APP_USER_PW}")
 
 # Create an application user on the main Production Database
 kubectl exec mongo-0 -c mongo-container -- mongo -u "${ROOT_USER}" -p "${ROOT_PW}" --authenticationDatabase admin \
-        --eval "db.getSiblingDB(\"${MONGO_APP_DB}\").createUser({user: \"${APP_USER}\", pwd: \"${APP_USER_PW}\", roles:[{role: \"dbOwner\", db: \"${MONGO_APP_DB}\"}]});"
+        --eval "db.getSiblingDB(\"admin\").createUser({user: \"${APP_USER}\", pwd: \"${APP_USER_PW}\", roles:[{role: \"dbOwner\", db: \"${MONGO_APP_DB}\"}]});"
 
 # Create an application user on the Development Database
 kubectl exec mongo-0 -c mongo-container -- mongo -u "${ROOT_USER}" -p "${ROOT_PW}" --authenticationDatabase admin \

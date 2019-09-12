f7ecd1c3111a342b205faed987905313ebfeaccf
dalton <duong.daltonle@gmail.com>
2019-09-12 13:19:31 +1000

    Disable appbar for non-users

--- services/client/src/components/moleisms/appbar/AppBar.jsx
+++ services/client/src/components/moleisms/appbar/AppBar.jsx
@@ -15,95 +15,101 @@ import { buttonize } from '../../../utils/accessibility'
 
 import styles from './AppBar.module.scss'
 
-const AppBar = (props) => {
-  const { active, redirect, isAdmin } = props
-  return (
-    <nav className={styles.navContainer}>
-      <div>
-        <AnstoLogo className={styles.logo} />
-        <a
-          id="sim"
-          className={`${styles.navIcon} ${active === 'sim' && styles.active}`}
-          href="/"
-        >
-          <Tooltip className={styles.tooltip} position="right">
-            <SlidersIcon className={styles.icon} />
-            <p> Simulation </p>
-          </Tooltip>
-        </a>
-        <a
-          id="savedSimulations"
-          className={`${styles.navIcon} ${active === 'savedSimulations' && styles.active}`}
-          href="/user/simulations"
-        >
-          <Tooltip className={styles.tooltip} position="right">
-            <HardDriveIcon className={styles.icon} />
-            <p> Saved simulations </p>
-          </Tooltip>
-        </a>
-        <a
-          id="alloys"
-          className={`${styles.navIcon} ${active === 'userAlloys' && styles.active}`}
-          href="/user/alloys"
-        >
-          <Tooltip className={styles.tooltip} position="right">
-            <DatabaseIcon className={styles.icon} />
-            <p> Alloy database </p>
-          </Tooltip>
-        </a>
-        <a
-          id="help"
-          className={`${styles.navIcon} ${active === 'edu' && styles.active}`}
-          href="/"
-        >
-          <Tooltip className={styles.tooltip} position="right">
-            <HelpIcon className={styles.icon} />
-            <p> Help </p>
-          </Tooltip>
-        </a>
-        <a
-          id="admin"
-          className={`${styles.navIcon} ${active === 'admin' && styles.active}`}
-          style={{ display: isAdmin ? 'flex' : 'none' }}
-          href="/admin/analytics"
-        >
-          <Tooltip className={styles.tooltip} position="right">
-            <MonitorIcon className={styles.icon} />
-            <p> Admin </p>
-          </Tooltip>
-        </a>
+const AppBar = ({
+  active,
+  redirect,
+  isAdmin,
+  isAuthenticated,
+}) => (
+  <nav className={styles.navContainer}>
+    <div>
+      <AnstoLogo className={styles.logo} />
+      <a
+        id="sim"
+        className={`${styles.navIcon} ${active === 'sim' && styles.active}`}
+        href="/"
+      >
+        <Tooltip className={styles.tooltip} position="right">
+          <SlidersIcon className={styles.icon} />
+          <p>Simulation</p>
+        </Tooltip>
+      </a>
+      <a
+        id="savedSimulations"
+        className={`${styles.navIcon} ${active === 'savedSimulations' && styles.active} ${!isAuthenticated && styles.disabled}`}
+        href={isAuthenticated ? '/user/simulations' : ''}
+      >
+        <Tooltip className={styles.tooltip} position="right">
+          <HardDriveIcon className={styles.icon} />
+          <p>Saved simulations</p>
+        </Tooltip>
+      </a>
+      <a
+        id="alloys"
+        className={`${styles.navIcon} ${active === 'userAlloys' && styles.active} ${!isAuthenticated && styles.disabled}`}
+        href={isAuthenticated ? '/user/alloys' : ''}
+      >
+        <Tooltip className={styles.tooltip} position="right">
+          <DatabaseIcon className={styles.icon} />
+          <p>Alloy database</p>
+        </Tooltip>
+      </a>
+      <a
+        id="help"
+        className={`${styles.navIcon} ${active === 'edu' && styles.active} ${!isAuthenticated && styles.disabled}`}
+        href={isAuthenticated ? '/' : ''}
+      >
+        <Tooltip className={styles.tooltip} position="right">
+          <HelpIcon className={styles.icon} />
+          <p>Help</p>
+        </Tooltip>
+      </a>
+      <a
+        id="admin"
+        className={`${styles.navIcon} ${active === 'admin' && styles.active}`}
+        style={{ display: isAdmin ? 'flex' : 'none' }}
+        href="/admin/analytics"
+      >
+        <Tooltip className={styles.tooltip} position="right">
+          <MonitorIcon className={styles.icon} />
+          <p>Admin</p>
+        </Tooltip>
+      </a>
+    </div>
+    <div>
+      <a
+        id="profile"
+        className={`${styles.navIcon} ${active === 'user' && styles.active} ${!isAuthenticated && styles.disabled}`}
+        href={isAuthenticated ? '/user/profile' : ''}
+      >
+        <Tooltip className={styles.tooltip} position="right">
+          <UserIcon className={styles.icon} />
+          <p>Account</p>
+        </Tooltip>
+      </a>
+      <div
+        id="logout"
+        className={`${styles.navIcon} ${!isAuthenticated && styles.disabled}`}
+        {...(() => {
+          if (isAuthenticated) return buttonize(() => logout(redirect))
+          return {}
+        })()}
+      >
+        <Tooltip className={styles.tooltip} position="right">
+          <LogOutIcon className={styles.icon} />
+          <p>Logout</p>
+        </Tooltip>
       </div>
-      <div>
-        <a
-          id="profile"
-          className={`${styles.navIcon} ${active === 'user' && styles.active}`}
-          href="/user/profile"
-        >
-          <Tooltip className={styles.tooltip} position="right">
-            <UserIcon className={styles.icon} />
-            <p> Account </p>
-          </Tooltip>
-        </a>
-        <div
-          id="logout"
-          className={styles.navIcon}
-          {...buttonize(() => logout(redirect))}
-        >
-          <Tooltip className={styles.tooltip} position="right">
-            <LogOutIcon className={styles.icon} />
-            <p> Logout </p>
-          </Tooltip>
-        </div>
-        <Logo className={styles.logo} />
-      </div>
-    </nav>
-  )
-}
+      <Logo className={styles.logo} />
+    </div>
+  </nav>
+)
 
 AppBar.propTypes = {
   active: PropTypes.string.isRequired,
   redirect: PropTypes.func.isRequired,
   isAdmin: PropTypes.bool.isRequired,
+  isAuthenticated: PropTypes.bool.isRequired,
 }
 
 export default AppBar

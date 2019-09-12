331174b72ccc4b25f6ccfea356d336b2748c8b12
Andrew Che <andrew@codeninja55.me>
2019-09-13 01:24:48 +1000

    Adding the celery worker container directory back

---
+++ services/celery-worker/flask_conf.py
@@ -0,0 +1,78 @@
+# -*- coding: utf-8 -*-import views, models, resources
+# ----------------------------------------------------------------------------------------------------------------------
+# arclytics_sim
+# flask_conf.py
+#
+# Attributions:
+# [1]
+# ----------------------------------------------------------------------------------------------------------------------
+__author__ = ['Andrew Che <@codeninja55>']
+
+__credits__ = ['']
+__license__ = 'TBA'
+__version__ = '0.1.0'
+__maintainer__ = 'Andrew Che'
+__email__ = 'andrew@neuraldev.io'
+__status__ = 'development'
+__date__ = '2019.06.04'
+"""flask_conf.py: 
+
+Just some configuration settings.
+"""
+
+import os
+
+
+class BaseConfig:
+    """Base configuration"""
+    TESTING = False
+    SECRET_KEY = os.environ.get('SECRET_KEY', '')
+    SECURITY_PASSWORD_SALT = os.environ.get('SECURITY_PASSWORD_SALT', '')
+
+    # Flask Email
+    MAIL_SUBJECT_PREFIX = '[Arclytics]'
+    MAIL_DEFAULT_SENDER = 'Arclytics Team <admin@arclytics.io>'
+    MAIL_SERVER = os.environ.get('MAIL_SERVER', None)
+    MAIL_PORT = os.environ.get('MAIL_PORT', None)
+    MAIL_USE_TLS = True
+    MAIL_USERNAME = os.environ.get('MAIL_USERNAME', '')
+    MAIL_PASSWORD = os.environ.get('MAIL_PASSWORD', '')
+    # Unset this to see the debug messages to logs
+    MAIL_DEBUG = False
+
+
+class DevelopmentConfig(BaseConfig):
+    """Development configuration"""
+    # CELERY REDIS
+    REDIS_HOST = os.environ.get('REDIS_HOST', None)
+    REDIS_PORT = os.environ.get('REDIS_PORT', None)
+    CELERY_BROKER_URL = f'redis://{REDIS_HOST}:{REDIS_PORT}/5'
+    CELERY_RESULT_BACKEND = f'redis://{REDIS_HOST}:{REDIS_PORT}/6'
+
+
+class TestingConfig(BaseConfig):
+    """Testing configuration"""
+    TESTING = True
+    SESSION_PERMANENT = False
+
+    # CELERY REDIS
+    REDIS_HOST = os.environ.get('REDIS_HOST', None)
+    REDIS_PORT = os.environ.get('REDIS_PORT', None)
+    CELERY_BROKER_URL = f'redis://{REDIS_HOST}:{REDIS_PORT}/5'
+    CELERY_RESULT_BACKEND = f'redis://{REDIS_HOST}:{REDIS_PORT}/6'
+
+
+class ProductionConfig(BaseConfig):
+    """Production configuration"""
+    SESSION_COOKIE_SECURE = True
+    REMEMBER_COOKIE_SECURE = True
+    MONGO_DBNAME = os.environ.get('MONGO_APP_DB')
+    BCRYPT_LOG_ROUNDS = 13
+
+    # Production Celery
+    REDIS_HOST = os.environ.get('REDIS_HOST', None)
+    REDIS_PORT = os.environ.get('REDIS_PORT', None)
+    REDIS_PASSWORD = os.environ.get('REDIS_PASSWORD', None)
+    # TODO(andrew@neuraldev.io): Change this with Kubernetes URL
+    CELERY_BROKER_URL = f'redis://{REDIS_HOST}:{REDIS_PORT}/5'
+    CELERY_RESULT_BACKEND = f'redis://{REDIS_HOST}:{REDIS_PORT}/6'

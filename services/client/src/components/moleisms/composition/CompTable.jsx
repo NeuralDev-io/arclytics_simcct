e34a015d57ad437ff9ddde62147456dc20198556
dalton <duong.daltonle@gmail.com>
2019-09-17 12:20:07 +1000

    Remove alloy 2 and mix composition

--- services/client/src/components/moleisms/composition/CompTable.jsx
+++ services/client/src/components/moleisms/composition/CompTable.jsx
@@ -10,6 +10,8 @@ import { updateComp } from '../../../state/ducks/sim/actions'
 
 import styles from './CompTable.module.scss'
 
+// Some UI elements for alloy 2 and mix composition were commented out
+// because the client no longer wants this feature
 class CompTable extends Component {
   handleChangeComp = (type, symbol, value) => {
     const { data, updateCompConnect } = this.props
@@ -37,9 +39,9 @@ class CompTable extends Component {
       onSaveButtonClick,
     } = this.props
     const {
-      alloyOption,
+      // alloyOption,
       parent,
-      weld,
+      // weld,
     } = data
 
     const tableData = parent.compositions.map(elem => ({
@@ -48,58 +50,59 @@ class CompTable extends Component {
       weld: undefined,
       mix: undefined,
     }))
-    if (alloyOption === 'mix') {
-      weld.compositions.forEach((elem) => {
-        const idx = tableData.findIndex(x => x.symbol === elem.symbol)
-        if (idx === -1) {
-          tableData.push({
-            symbol: elem.symbol,
-            parent: undefined,
-            weld: elem.weight,
-            mix: undefined,
-          })
-        } else {
-          tableData[idx] = {
-            ...tableData[idx],
-            weld: elem.weight,
-          }
-        }
-      })
-    }
-    if (alloyOption === 'mix') {
-      const dilution = parseFloat(data.dilution)
-      tableData.forEach((elem, idx) => {
-        const parentVal = elem.parent ? parseFloat(elem.parent) : 0
-        const weldVal = elem.weld ? parseFloat(elem.weld) : 0
-        // calculate mix composition and round to 2 decimals
-        const mixVal = Math.round((dilution / 100 * weldVal
-          + (100 - dilution) / 100 * parentVal) * 100) / 100
-        tableData[idx] = {
-          ...elem,
-          mix: mixVal,
-        }
-      })
-    }
+    // if (alloyOption === 'mix') {
+    //   weld.compositions.forEach((elem) => {
+    //     const idx = tableData.findIndex(x => x.symbol === elem.symbol)
+    //     if (idx === -1) {
+    //       tableData.push({
+    //         symbol: elem.symbol,
+    //         parent: undefined,
+    //         weld: elem.weight,
+    //         mix: undefined,
+    //       })
+    //     } else {
+    //       tableData[idx] = {
+    //         ...tableData[idx],
+    //         weld: elem.weight,
+    //       }
+    //     }
+    //   })
+    // }
+    // if (alloyOption === 'mix') {
+    //   const dilution = parseFloat(data.dilution)
+    //   tableData.forEach((elem, idx) => {
+    //     const parentVal = elem.parent ? parseFloat(elem.parent) : 0
+    //     const weldVal = elem.weld ? parseFloat(elem.weld) : 0
+    //     // calculate mix composition and round to 2 decimals
+    //     const mixVal = Math.round((dilution / 100 * weldVal
+    //       + (100 - dilution) / 100 * parentVal) * 100) / 100
+    //     tableData[idx] = {
+    //       ...elem,
+    //       mix: mixVal,
+    //     }
+    //   })
+    // }
 
     // calculate total weights
     let parentTotal = 0
-    let weldTotal = 0
-    let mixTotal = 0
+    // let weldTotal = 0
+    // let mixTotal = 0
     tableData.forEach((elem) => {
       if (elem.parent) parentTotal += parseFloat(elem.parent)
-      if (elem.weld) weldTotal += parseFloat(elem.weld)
-      if (elem.mix) mixTotal += parseFloat(elem.mix)
+      // if (elem.weld) weldTotal += parseFloat(elem.weld)
+      // if (elem.mix) mixTotal += parseFloat(elem.mix)
     })
     parentTotal = Math.round(parentTotal * 100) / 100
-    weldTotal = Math.round(weldTotal * 100) / 100
-    mixTotal = Math.round(mixTotal * 100) / 100
+    // weldTotal = Math.round(weldTotal * 100) / 100
+    // mixTotal = Math.round(mixTotal * 100) / 100
 
     const columns = [
       {
         Header: 'Elements',
         accessor: 'symbol',
         Cell: ({ value }) => (<span className={styles.symbol}>{value}</span>),
-        width: 80,
+        width: 120,
+        // width: 80,
         Footer: () => tableData.length !== 0 && 'Total',
       },
       {
@@ -119,34 +122,34 @@ class CompTable extends Component {
         Footer: () => tableData.length !== 0
           && <span className={styles.footerText}>{parentTotal}</span>,
       },
-      {
-        Header: 'Alloy 2',
-        accessor: 'weld',
-        Cell: ({ row, value }) => (
-          <SelfControlledTextField
-            type="text"
-            key={row.symbol}
-            name={`weld_${row.symbol}`}
-            onBlur={e => this.handleChangeComp('weld', row.symbol, e.target.value)}
-            defaultValue={value || '0.0'}
-            length="stretch"
-            isDisabled={value === undefined}
-          />
-        ),
-        Footer: () => tableData.length !== 0
-          && <span className={styles.footerText}>{weldTotal}</span>,
-      },
-      {
-        Header: 'Mix',
-        accessor: 'mix',
-        Cell: ({ value }) => {
-          if (value === undefined) return <span className="text--disabled">0.0</span>
-          return <span>{value}</span>
-        },
-        width: 40,
-        Footer: () => tableData.length !== 0
-          && <span className={styles.footerTextMix}>{mixTotal}</span>,
-      },
+      // {
+      //   Header: 'Alloy 2',
+      //   accessor: 'weld',
+      //   Cell: ({ row, value }) => (
+      //     <SelfControlledTextField
+      //       type="text"
+      //       key={row.symbol}
+      //       name={`weld_${row.symbol}`}
+      //       onBlur={e => this.handleChangeComp('weld', row.symbol, e.target.value)}
+      //       defaultValue={value || '0.0'}
+      //       length="stretch"
+      //       isDisabled={value === undefined}
+      //     />
+      //   ),
+      //   Footer: () => tableData.length !== 0
+      //     && <span className={styles.footerText}>{weldTotal}</span>,
+      // },
+      // {
+      //   Header: 'Mix',
+      //   accessor: 'mix',
+      //   Cell: ({ value }) => {
+      //     if (value === undefined) return <span className="text--disabled">0.0</span>
+      //     return <span>{value}</span>
+      //   },
+      //   width: 40,
+      //   Footer: () => tableData.length !== 0
+      //     && <span className={styles.footerTextMix}>{mixTotal}</span>,
+      // },
     ]
 
     return (

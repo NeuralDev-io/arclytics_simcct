9d34fac623259543fb37a8452b3b5c1ddd26d237
dalton <duong.daltonle@gmail.com>
2019-09-19 18:15:26 +1000

    Reset redux state tree on log out

--- services/client/src/components/moleisms/appbar/AppBar.jsx
+++ services/client/src/components/moleisms/appbar/AppBar.jsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import PropTypes from 'prop-types'
+import { connect } from 'react-redux'
 import HardDriveIcon from 'react-feather/dist/icons/hard-drive'
 import HelpIcon from 'react-feather/dist/icons/help-circle'
 import MonitorIcon from 'react-feather/dist/icons/monitor'
@@ -10,106 +11,132 @@ import SlidersIcon from 'react-feather/dist/icons/sliders'
 import { ReactComponent as AnstoLogo } from '../../../assets/ANSTO_Logo_SVG/logo.svg'
 import { ReactComponent as Logo } from '../../../assets/logo_20.svg'
 import Tooltip from '../../elements/tooltip'
+import store from '../../../state/store'
 import { logout } from '../../../api/AuthenticationHelper'
 import { buttonize } from '../../../utils/accessibility'
+import { addFlashToast } from '../../../state/ducks/toast/actions'
 
 import styles from './AppBar.module.scss'
 
-const AppBar = ({
-  active,
-  redirect,
-  isAdmin,
-  isAuthenticated,
-}) => (
-  <nav className={styles.navContainer}>
-    <div>
-      <AnstoLogo className={styles.logo} />
-      <a
-        id="sim"
-        className={`${styles.navIcon} ${active === 'sim' && styles.active}`}
-        href="/"
-      >
-        <Tooltip className={{ tooltip: styles.tooltip }} position="right">
-          <SlidersIcon className={styles.icon} />
-          <p>Simulation</p>
-        </Tooltip>
-      </a>
-      <a
-        id="savedSimulations"
-        className={`${styles.navIcon} ${active === 'savedSimulations' && styles.active} ${!isAuthenticated && styles.disabled}`}
-        href={isAuthenticated ? '/user/simulations' : ''}
-      >
-        <Tooltip className={{ tooltip: styles.tooltip }} position="right">
-          <HardDriveIcon className={styles.icon} />
-          <p>Saved simulations</p>
-        </Tooltip>
-      </a>
-      <a
-        id="alloys"
-        className={`${styles.navIcon} ${active === 'userAlloys' && styles.active} ${!isAuthenticated && styles.disabled}`}
-        href={isAuthenticated ? '/user/alloys' : ''}
-      >
-        <Tooltip className={{ tooltip: styles.tooltip }} position="right">
-          <DatabaseIcon className={styles.icon} />
-          <p>Alloy database</p>
-        </Tooltip>
-      </a>
-      <a
-        id="help"
-        className={`${styles.navIcon} ${active === 'edu' && styles.active} ${!isAuthenticated && styles.disabled}`}
-        href={isAuthenticated ? '/' : ''}
-      >
-        <Tooltip className={{ tooltip: styles.tooltip }} position="right">
-          <HelpIcon className={styles.icon} />
-          <p>Help</p>
-        </Tooltip>
-      </a>
-      <a
-        id="admin"
-        className={`${styles.navIcon} ${active === 'admin' && styles.active}`}
-        style={{ display: isAdmin ? 'flex' : 'none' }}
-        href="/admin/analytics"
-      >
-        <Tooltip className={{ tooltip: styles.tooltip }} position="right">
-          <MonitorIcon className={styles.icon} />
-          <p>Admin</p>
-        </Tooltip>
-      </a>
-    </div>
-    <div>
-      <a
-        id="profile"
-        className={`${styles.navIcon} ${active === 'user' && styles.active} ${!isAuthenticated && styles.disabled}`}
-        href={isAuthenticated ? '/user/profile' : ''}
-      >
-        <Tooltip className={{ tooltip: styles.tooltip }} position="right">
-          <UserIcon className={styles.icon} />
-          <p>Account</p>
-        </Tooltip>
-      </a>
-      <div
-        id="logout"
-        className={`${styles.navIcon} ${!isAuthenticated && styles.disabled}`}
-        {...(() => {
-          if (isAuthenticated) return buttonize(() => logout(redirect))
-          return {}
-        })()}
-      >
-        <Tooltip className={{ tooltip: styles.tooltip }} position="right">
-          <LogOutIcon className={styles.icon} />
-          <p>Logout</p>
-        </Tooltip>
-      </div>
-      <Logo className={styles.logo} />
-    </div>
-  </nav>
-)
+class AppBar extends React.Component {
+  handleLogout = () => {
+    const { addFlashToastConnect, redirect } = this.props
+    logout()
+      .then(() => {
+        redirect('/signin')
+        store.dispatch({ type: 'USER_LOGOUT' })
+      })
+      .catch(() => addFlashToastConnect({
+        message: 'We couldn\'t log you out. Please try again',
+        options: { variant: 'error' },
+      }, true))
+  }
+
+  render() {
+    const {
+      active,
+      isAdmin,
+      isAuthenticated,
+    } = this.props
+
+    return (
+      <nav className={styles.navContainer}>
+        <div>
+          <AnstoLogo className={styles.logo} />
+          <a
+            id="sim"
+            className={`${styles.navIcon} ${active === 'sim' && styles.active}`}
+            href="/"
+          >
+            <Tooltip className={{ tooltip: styles.tooltip }} position="right">
+              <SlidersIcon className={styles.icon} />
+              <p>Simulation</p>
+            </Tooltip>
+          </a>
+          <a
+            id="savedSimulations"
+            className={`${styles.navIcon} ${active === 'savedSimulations' && styles.active} ${!isAuthenticated && styles.disabled}`}
+            href={isAuthenticated ? '/user/simulations' : ''}
+          >
+            <Tooltip className={{ tooltip: styles.tooltip }} position="right">
+              <HardDriveIcon className={styles.icon} />
+              <p>Saved simulations</p>
+            </Tooltip>
+          </a>
+          <a
+            id="alloys"
+            className={`${styles.navIcon} ${active === 'userAlloys' && styles.active} ${!isAuthenticated && styles.disabled}`}
+            href={isAuthenticated ? '/user/alloys' : ''}
+          >
+            <Tooltip className={{ tooltip: styles.tooltip }} position="right">
+              <DatabaseIcon className={styles.icon} />
+              <p>Alloy database</p>
+            </Tooltip>
+          </a>
+          <a
+            id="help"
+            className={`${styles.navIcon} ${active === 'edu' && styles.active} ${!isAuthenticated && styles.disabled}`}
+            href={isAuthenticated ? '/' : ''}
+          >
+            <Tooltip className={{ tooltip: styles.tooltip }} position="right">
+              <HelpIcon className={styles.icon} />
+              <p>Help</p>
+            </Tooltip>
+          </a>
+          <a
+            id="admin"
+            className={`${styles.navIcon} ${active === 'admin' && styles.active}`}
+            style={{ display: isAdmin ? 'flex' : 'none' }}
+            href="/admin/analytics"
+          >
+            <Tooltip className={{ tooltip: styles.tooltip }} position="right">
+              <MonitorIcon className={styles.icon} />
+              <p>Admin</p>
+            </Tooltip>
+          </a>
+        </div>
+        <div>
+          <a
+            id="profile"
+            className={`${styles.navIcon} ${active === 'user' && styles.active} ${!isAuthenticated && styles.disabled}`}
+            href={isAuthenticated ? '/user/profile' : ''}
+          >
+            <Tooltip className={{ tooltip: styles.tooltip }} position="right">
+              <UserIcon className={styles.icon} />
+              <p>Account</p>
+            </Tooltip>
+          </a>
+          <div
+            id="logout"
+            className={`${styles.navIcon} ${!isAuthenticated && styles.disabled}`}
+            {...(() => {
+              if (isAuthenticated) return buttonize(this.handleLogout)
+              return {}
+            })()}
+          >
+            <Tooltip className={{ tooltip: styles.tooltip }} position="right">
+              <LogOutIcon className={styles.icon} />
+              <p>Logout</p>
+            </Tooltip>
+          </div>
+          <Logo className={styles.logo} />
+        </div>
+      </nav>
+    )
+  }
+}
 
 AppBar.propTypes = {
   active: PropTypes.string.isRequired,
   redirect: PropTypes.func.isRequired,
   isAdmin: PropTypes.bool.isRequired,
   isAuthenticated: PropTypes.bool.isRequired,
+  // from connect
+  addFlashToastConnect: PropTypes.func.isRequired,
 }
 
-export default AppBar
+const mapDispatchToProps = {
+  addFlashToastConnect: addFlashToast,
+}
+
+export default connect(null, mapDispatchToProps)(AppBar)

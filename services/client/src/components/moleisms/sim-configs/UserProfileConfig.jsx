cb06fb303e41ae04275e6e9b9540bc15eca11f62
dalton <duong.daltonle@gmail.com>
2019-09-19 17:02:44 +1000

    Validate input for user config

--- services/client/src/components/moleisms/sim-configs/UserProfileConfig.jsx
+++ services/client/src/components/moleisms/sim-configs/UserProfileConfig.jsx
@@ -1,55 +1,75 @@
-import React from 'react'
+import React, { Component } from 'react'
 import PropTypes from 'prop-types'
 import { connect } from 'react-redux'
 import { TextFieldExtra } from '../../elements/textfield'
 import Checkbox from '../../elements/checkbox'
 import { updateConfig, toggleDisplayUserCurve } from '../../../state/ducks/sim/actions'
+import { validate } from '../../../utils/validator'
+import { constraints } from './utils/constraints'
 
 import styles from './UserProfileConfig.module.scss'
 
-const UserProfileConfig = ({
-  configurations,
-  displayUserCurve,
-  updateConfigConnect,
-  toggleDisplayUserCurveConnect,
-  isAuthenticated,
-}) => (
-  <React.Fragment>
-    <Checkbox
-      className={styles.checkbox}
-      name="displayUserCurve"
-      onChange={val => toggleDisplayUserCurveConnect(val)}
-      isChecked={displayUserCurve}
-      label="Show user profile"
-      isDisabled={!isAuthenticated}
-    />
-    <div className={`input-row ${styles.config}`}>
-      <span>Start temperature</span>
-      <TextFieldExtra
-        type="text"
-        name="start_temp"
-        onChange={val => updateConfigConnect('start_temp', val)}
-        value={configurations.start_temp}
-        length="short"
-        suffix="°C"
-        isDisabled={!isAuthenticated}
-      />
-    </div>
-    <div className={`input-row ${styles.config}`}>
-      <span>CCT cooling rate</span>
-      <TextFieldExtra
-        type="text"
-        name="cct_cooling_rate"
-        onChange={val => updateConfigConnect('cct_cooling_rate', val)}
-        value={configurations.cct_cooling_rate}
-        length="short"
-        suffix="°C/sec"
-        className={styles.textfield}
-        isDisabled={!isAuthenticated}
-      />
-    </div>
-  </React.Fragment>
-)
+class UserProfileConfig extends Component {
+  handleUpdateInput = (name, value) => {
+    const { updateConfigConnect } = this.props
+    let err
+    if (name === 'start_temp') {
+      err = validate(value, constraints.startTemp)
+    } else err = validate(value, constraints.cctRate)
+
+    if (err !== undefined) {
+      updateConfigConnect(name, value, { [name]: err })
+    } else updateConfigConnect(name, value, {})
+  }
+
+  render() {
+    const {
+      configurations,
+      displayUserCurve,
+      toggleDisplayUserCurveConnect,
+      isAuthenticated,
+    } = this.props
+    return (
+      <React.Fragment>
+        <Checkbox
+          className={styles.checkbox}
+          name="displayUserCurve"
+          onChange={val => toggleDisplayUserCurveConnect(val)}
+          isChecked={displayUserCurve}
+          label="Show user profile"
+          isDisabled={!isAuthenticated}
+        />
+        <div className={`input-row ${styles.config}`}>
+          <span>Start temperature</span>
+          <TextFieldExtra
+            type="text"
+            name="start_temp"
+            onChange={val => this.handleUpdateInput('start_temp', val)}
+            value={configurations.start_temp}
+            length="short"
+            suffix="°C"
+            isDisabled={!isAuthenticated}
+            error={configurations.error.start_temp}
+          />
+        </div>
+        <div className={`input-row ${styles.config}`}>
+          <span>CCT cooling rate</span>
+          <TextFieldExtra
+            type="text"
+            name="cct_cooling_rate"
+            onChange={val => this.handleUpdateInput('cct_cooling_rate', val)}
+            value={configurations.cct_cooling_rate}
+            length="short"
+            suffix="°C/sec"
+            className={styles.textfield}
+            isDisabled={!isAuthenticated}
+            error={configurations.error.cct_cooling_rate}
+          />
+        </div>
+      </React.Fragment>
+    )
+  }
+}
 
 UserProfileConfig.propTypes = {
   configurations: PropTypes.shape({

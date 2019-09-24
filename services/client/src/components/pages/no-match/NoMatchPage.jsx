5c42c46b75ee5810706a50bb04be47dab585e584
Arvy Salazar <arvy.au@gmail.com>
2019-09-24 14:32:51 +1000

    Add UI and css

--- services/client/src/components/pages/no-match/NoMatchPage.jsx
+++ services/client/src/components/pages/no-match/NoMatchPage.jsx
@@ -1,18 +1,24 @@
 import React, { Component } from 'react'
 import PropTypes from 'prop-types'
+import Button from '../../elements/button'
 import { ReactComponent as PageNotFoundImage } from '../../../assets/undraw_page_not_found_su7k.svg'
 
-class NoMatchPage extends Component{
+import styles from './NoMatchPage.module.scss'
+
+class NoMatchPage extends Component {
   render() {
-    const { location } = this.props
+    const { history, location } = this.props
     return (
       <div>
-        <div>
+        <div className={styles.container}>
           <PageNotFoundImage className={styles.pageNotFoundImage}/>
           <h3>
-            No match for
-            <code>{location.pathname}</code>
+            The URL <code>{location.pathname}</code> doesn&apos;t exist in our server.
           </h3>
+          <div className={styles.buttonContainer}>
+            <Button onClick={() => history.goBack()}>Go back</Button>
+            <Button onClick={() => history.push('/')}>Go home</Button>
+          </div>
         </div>
         )
       </div>
@@ -21,6 +27,7 @@ class NoMatchPage extends Component{
 }
 
 NoMatchPage.propTypes = {
+  history: PropTypes.string.isRequired,
   location: PropTypes.shape({
     pathname: PropTypes.string.isRequired,
   }),

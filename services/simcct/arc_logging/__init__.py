89c2b3b0e7f6781af4069aea370cb970a615d181
Andrew Che <andrew@neuraldev.io>
2019-09-25 18:41:17 +1000

    Adding a stdout and file handlers to logger by default

--- services/simcct/arc_logging/__init__.py
+++ services/simcct/arc_logging/__init__.py
@@ -1,1 +1,11 @@
-from .arc_logging import AppLogging
+from .arc_logging import (
+    AppLogger, CRITICAL, DEBUG, ERROR, FATAL, WARN, WARNING, NOTSET,
+    FluentdHandler, StreamHandler, FileHandler, Handler, get_level_name,
+    EventRecord, LogRecord,
+)
+
+__all__ = [
+    'AppLogger', 'CRITICAL', 'DEBUG', 'ERROR', 'FATAL', 'WARN', 'WARNING',
+    'NOTSET', 'FluentdHandler', 'StreamHandler', 'FileHandler', 'Handler',
+    'get_level_name', 'EventRecord', 'LogRecord'
+]

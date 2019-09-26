0b74e4e42747aecda00c02d51769ee80d1e69113
Andrew Che <andrew@neuraldev.io>
2019-09-26 13:35:30 +1000

    Fixing namespace for mongo

--- kubernetes/mongo_import.sh
+++ kubernetes/mongo_import.sh
@@ -12,7 +12,7 @@ TEMPFILE_USER=$(mktemp)
 TEMPFILE_PW=$(mktemp)
 
 # Get the decoded values from the credentials secrets store.
-kubectl get secret credentials -o jsonpath="{.data.mongo_root_user}" | base64 -d > "${TEMPFILE_USER}"
+kubectl get secret credentials -o jsonpath="{.data.mongo_root_user}" --namespace=arclytics | base64 -d > "${TEMPFILE_USER}"
 kubectl get secret credentials -o jsonpath="{.data.mongo_root_password}" | base64 -d > "${TEMPFILE_PW}"
 
 # Get the decoded password back from the temp files.
@@ -20,10 +20,10 @@ MONGO_ROOT_USER=$(<"${TEMPFILE_USER}")
 MONGO_ROOT_PASSWORD=$(<"${TEMPFILE_PW}")
 
 TEMPFILE_DB=$(mktemp)
-kubectl get secret credentials -o jsonpath="{.data.mongo_app_db}" | base64 -d > ${TEMPFILE_DB}
+kubectl get secret credentials -o jsonpath="{.data.mongo_app_db}" --namespace=arclytics | base64 -d > ${TEMPFILE_DB}
 MONGO_APP_DB=$(<"${TEMPFILE_DB}")
 
-kubectl exec mongo-0 -c mongo-container -- \
+kubectl exec mongo-0 -c mongo-container --namespace=arclytics -- \
     mongoimport --host localhost \
         --port 27017 \
         -u "${MONGO_ROOT_USER}" \
@@ -34,7 +34,7 @@ kubectl exec mongo-0 -c mongo-container -- \
         --drop \
         --file /data/backups/production_user_data.json
 
-kubectl exec mongo-0 -c mongo-container -- \
+kubectl exec mongo-0 -c mongo-container --namespace=arclytics -- \
     mongoimport --host localhost \
         --port 27017 \
         -u "${MONGO_ROOT_USER}" \

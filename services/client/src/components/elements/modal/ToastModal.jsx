a1c7489ff158f636dfab5aad035991b00943ab78
dalton <duong.daltonle@gmail.com>
2019-09-26 12:57:41 +1000

    Add withCloseIcon prop for ToastModal

--- services/client/src/components/elements/modal/ToastModal.jsx
+++ services/client/src/components/elements/modal/ToastModal.jsx
@@ -1,20 +1,36 @@
 import React from 'react'
 import PropTypes from 'prop-types'
+import XIcon from 'react-feather/dist/icons/x'
+import { IconButton } from '../button'
 
 import styles from './ToastModal.module.scss'
 
 const ToastModal = ({
   show = false,
+  withCloseIcon = false,
+  onClose = () => {},
   className,
   children,
 }) => (
-  <div className={`${styles.modal} ${show ? styles.visible : ''} ${className}`}>
-    {children}
+  <div className={`${styles.modalContainer} ${show ? styles.visible : ''} ${withCloseIcon && styles.withCloseIcon}`}>
+    <div className={`${styles.modal} ${className}`}>
+      {children}
+    </div>
+    { withCloseIcon
+    && (
+      <IconButton
+        onClick={onClose}
+        Icon={props => <XIcon {...props} />}
+        className={styles.closeButton}
+      />
+    )}
   </div>
 )
 
 ToastModal.propTypes = {
   show: PropTypes.bool.isRequired,
+  withCloseIcon: PropTypes.bool,
+  onClose: PropTypes.func,
   className: PropTypes.string,
   children: PropTypes.node,
 }
@@ -22,6 +38,8 @@ ToastModal.propTypes = {
 ToastModal.defaultProps = {
   className: '',
   children: null,
+  withCloseIcon: false,
+  onClose: () => {},
 }
 
 export default ToastModal

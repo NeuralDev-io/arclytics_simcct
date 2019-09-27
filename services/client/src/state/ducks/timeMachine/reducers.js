6f4d4e11e46937162941cd64542399e9b5f1309b
dalton <duong.daltonle@gmail.com>
2019-09-27 14:56:33 +1000

    Temp commit working on Time Heist

---
+++ services/client/src/state/ducks/timeMachine/reducers.js
@@ -0,0 +1,57 @@
+import {
+  ADD_SIM,
+  REMOVE_SIM,
+  GO_FORWARD,
+  GO_BACKWARD,
+  SET_SIM_DATA,
+} from './types'
+
+const initialState = {
+  capacity: 10,
+  data: [],
+  current: -1,
+}
+
+const reducer = (state = initialState, action) => {
+  switch (action.type) {
+    case ADD_SIM: {
+      const newData = [...state.data]
+      // remove all sim after current position
+      newData.splice(state.current + 1)
+      // add new sim
+      newData.push(action.payload)
+      return {
+        ...state,
+        data: newData,
+        current: state.current + 1,
+      }
+    }
+    case REMOVE_SIM: {
+      const newData = [...state.data]
+      newData.shift()
+      return {
+        ...state,
+        data: newData,
+      }
+    }
+    case SET_SIM_DATA:
+      return {
+        ...state,
+        data: action.payload,
+      }
+    case GO_FORWARD:
+      return {
+        ...state,
+        current: state.current + 1,
+      }
+    case GO_BACKWARD:
+      return {
+        ...state,
+        current: state.current - 1,
+      }
+    default:
+      return state
+  }
+}
+
+export default reducer

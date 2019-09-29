e2225adbaacd90fa6755e374a4ea1756c0dc5283
Andrew Che <andrew@neuraldev.io>
2019-09-30 00:36:58 +1000

    Bumping Node versions and removing rum completely

--- services/client/src/constants.js
+++
@@ -1,4 +0,0 @@
-export const CLIENT_ENV = `${process.env.NODE_ENV}`
-export const APM_URL = `${process.env.REACT_APP_APM_URL}`
-export const FLUENTD_URL = `${process.env.REACT_APP_FLUENTD_URL}`
-export const ARC_URL = `${process.env.REACT_APP_SIM_HOST}:${process.env.REACT_APP_SIM_PORT}/api/v1/sim`

eb410cab2a578b4ac60884a8f5606b109d75ba58
Andrew Che <andrew@neuraldev.io>
2019-09-30 00:00:53 +1000

    Removing the RUM

--- services/client/src/rum.js
+++ services/client/src/rum.js
@@ -13,8 +13,8 @@
  * @author Andrew Che
  */
 
-import { init as initAPM } from '@elastic/apm-rum'
-import { APM_URL } from './constants'
+// import { init as initAPM } from '@elastic/apm-rum'
+// import { APM_URL, CLIENT_ENV } from './constants'
 
 /*
 *
@@ -30,30 +30,31 @@ import { APM_URL } from './constants'
 * */
 
 // eslint-disable-next-line import/no-mutable-exports
-let apm = null
-
-if (process.env.NODE_ENV === 'development') {
-  // The APM Real User Monitoring service
-  apm = initAPM({
-    // Set required service name (allowed characters: a-z, A-Z, 0-9, -, _, and space)
-    serviceName: 'client',
-    // Set custom APM Server URL (default: http://localhost:8200)
-    // serverUrl: `${process.env.REACT_APP_APM_URL}`,
-    serverUrl: APM_URL,
-    // Set service version (required for sourcemap feature)
-    // serviceVersion: '0.9',
-
-    // Environment where the service being monitored is deployed, e.g. "production" or "development"
-    environment: process.env.NODE_ENV,
-
-    // This option sets the name for the page load transaction.
-    // Note:
-    // This is not working with `window.location.pathname` as we are using
-    // React-Router which uses a Shadow DOM and doesn't have windows.
-    // Pass the pageName into the APM agent as a configuration option
-    // pageLoadTransactionName: pageName,
-    pageLoadTransactionName: 'client',
-  })
-}
-
-export default apm
+// let apm = null
+//
+// if (CLIENT_ENV === 'development') {
+//   // The APM Real User Monitoring service
+//   apm = initAPM({
+//     // Set required service name (allowed characters: a-z, A-Z, 0-9, -, _, and space)
+//     serviceName: 'client',
+//     // Set custom APM Server URL (default: http://localhost:8200)
+//     // serverUrl: `${process.env.REACT_APP_APM_URL}`,
+//     serverUrl: APM_URL,
+//     // Set service version (required for sourcemap feature)
+//     // serviceVersion: '0.9',
+//
+//     // Environment where the service being monitored is deployed,
+//     // e.g. "production" or "development"
+//     environment: CLIENT_ENV,
+//
+//     // This option sets the name for the page load transaction.
+//     // Note:
+//     // This is not working with `window.location.pathname` as we are using
+//     // React-Router which uses a Shadow DOM and doesn't have windows.
+//     // Pass the pageName into the APM agent as a configuration option
+//     // pageLoadTransactionName: pageName,
+//     pageLoadTransactionName: 'client',
+//   })
+// }
+//
+// export default apm

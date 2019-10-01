4632545b09c00b9be801e7d1e42cbb334aa15199
Andrew Che <andrew@neuraldev.io>
2019-10-02 00:47:07 +1000

    Completely changing the way last sim behaves with invalid data

--- services/simcct/sim_api/resources/last_simulation.py
+++ services/simcct/sim_api/resources/last_simulation.py
@@ -8,29 +8,32 @@
 # -----------------------------------------------------------------------------
 __author__ = ['David Matthews <@tree1004>', 'Dinol Shrestha <@dinolsth>']
 __license__ = 'MIT'
-__version__ = '1.0.0'
+__version__ = '1.2.0'
 __status__ = 'production'
 __date__ = '2019.08.11'
 """last_simulation.py: 
 
 This module defines the Resource for saving and retrieving a user's last 
 simulation they ran with all the configurations, alloy, and results if it was
-successfully simulated.
+successfully simulated or an invalid set of data if it not.
 """
 
-from flask import Blueprint, json, request
+from flask import Blueprint, request
 from flask_restful import Resource
-from mongoengine import FieldDoesNotExist, ValidationError
+from marshmallow import ValidationError
+from mongoengine import ValidationError as MEValidationError
 
 from arc_logging import AppLogger
 from sim_api.extensions import api, apm
 from sim_api.extensions.SimSession.sim_session_service import SimSessionService
 from sim_api.extensions.utilities import (
     DuplicateElementError, ElementInvalid, ElementSymbolInvalid,
-    MissingElementError
+    MissingElementError, ElementWeightInvalid
 )
 from sim_api.middleware import authenticate_user_cookie_restful
-from sim_api.models import AlloyStore, Configuration, SimulationResults
+from sim_api.schemas import (
+    ConfigurationsSchema, AlloyStoreSchema, SimulationResultsSchema
+)
 
 logger = AppLogger(__name__)
 
@@ -38,7 +41,15 @@ last_sim_blueprint = Blueprint('user_last_simulation', __name__)
 
 
 class LastSimulation(Resource):
+    """
+    This Resource defines methods to POST and GET the long-term saved last
+    set of Configurations, Alloys, and Results (if any) used by the User.
+    This allows the client front-end to load this data on login or set it
+    on logout.
 
+    Note that an invalid set of configurations and alloys returned must first
+    be validated by the front-end before we can run simulation on it.
+    """
     method_decorators = {
         'post': [authenticate_user_cookie_restful],
         'get': [authenticate_user_cookie_restful]
@@ -47,8 +58,8 @@ class LastSimulation(Resource):
     # noinspection PyMethodMayBeStatic
     def post(self, user):
         """Exposes the POST method to save the last configurations and alloy
-        storage to the `sim_api.models.User` document in the fields
-        `last_configurations` and `last_alloy_store`.
+        storage to the `sim_api.models.User` document in the appropriate fields
+        for each of these.
 
         Args:
             user: a valid `models.User` object verified and passed by the
@@ -60,18 +71,19 @@ class LastSimulation(Resource):
         response = {'status': 'fail', 'message': 'Invalid payload.'}
 
         post_data = request.get_json()
-
         # First we validate the request data
         if not post_data:
             return response, 400
 
-        validity_check = post_data.get('is_valid', None)
+        is_valid = post_data.get('is_valid', None)
         post_invalid_fields = post_data.get('invalid_fields', None)
         post_configs = post_data.get('configurations', None)
         post_alloy_store = post_data.get('alloy_store', None)
         post_results = post_data.get('simulation_results', None)
 
-        if not validity_check:
+        # We enforce the client tells us this last set of configurations and
+        # alloys are either valid or not so that we can save appropriately.
+        if not is_valid:
             response['message'] = 'Validity must be defined.'
             return response, 400
 
@@ -81,31 +93,66 @@ class LastSimulation(Resource):
         ):
             return response, 400
 
+        # ===== # Saving of Invalid Configurations/Alloy # ===== #
+        # If the client says that the data is not valid, we simply save
+        # it as a dictionary whatever it is.
+        if not is_valid:
+            # Save everything that we're given which has no validation since
+            # `models.User` is using `mongoengine.DictField` for all these.
+            user.last_configuration = post_configs if post_configs else {}
+            user.last_alloy_store = post_alloy_store if post_alloy_store else {}
+            user.last_simulation_results = post_results if post_results else {}
+            user.last_simulation_invalid_fields = (
+                post_invalid_fields if post_invalid_fields else {}
+            )
+
+            try:
+                user.save()
+            except MEValidationError as e:
+                # This is just to check that our schema validation matches
+                # everything expected with Mongoengine, however, this is
+                # strictly to fail gracefully as the mongoengine model uses
+                # `mongoengine.DictField` which has no requirement for
+                # validation.
+                response['message'] = 'Mongoengine Validation error.'
+                logger.exception(
+                    {
+                        'error': e.message,
+                        'message': response['message']
+                    }
+                )
+                apm.capture_exception()
+                return response, 500
+
+            # Update the final response dict with everything for a
+            # successful POST method.
+            response.update(
+                {
+                    'message': 'Saved Last Simulation Data.',
+                    'data': {
+                        'last_configuration': post_configs,
+                        'last_alloy_store': post_alloy_store
+                    },
+                    'status': 'success'
+                }
+            )
+            return response, 201
+
+        # ===== # Validation of Valid Configurations/Alloy # ===== #
         valid_configs = None
         valid_store = None
         valid_results = None
 
+        # At this point, we have been told by the client that the data is
+        # valid so we do our own internal validity check and deserialize
+        # everything to a Python Dict
         try:
             if post_configs:
-                valid_configs = Configuration.from_json(
-                    json.dumps(post_configs)
-                )
-                valid_configs.validate(clean=True)
+                valid_configs = ConfigurationsSchema().load(post_configs)
             if post_alloy_store:
-                valid_store = AlloyStore.from_json(
-                    json.dumps(post_alloy_store)
-                )
-                valid_store.validate(clean=True)
+                valid_store = AlloyStoreSchema().load(post_alloy_store)
             if post_results:
-                valid_results = SimulationResults.from_json(
-                    json.dumps(post_results)
-                )
-                valid_results.validate(clean=True)
-        except FieldDoesNotExist as e:
-            # In case the request has fields we do not expect.
-            response['error'] = str(e)
-            response['message'] = 'Field does not exist error.'
-            return response, 400
+                valid_results = SimulationResultsSchema().load(post_results)
         except ElementSymbolInvalid as e:
             # Where the symbol used for the element is not valid meaning it
             # does not exist in a Periodic Table.
@@ -116,6 +163,11 @@ class LastSimulation(Resource):
             # If no "symbol" or "weight" passed as an Element object.
             response['error'] = str(e)
             response['message'] = 'Invalid element error.'
+            return response, 400
+        except ElementWeightInvalid as e:
+            # If no "symbol" or "weight" passed as an Element object.
+            response['error'] = str(e)
+            response['message'] = 'Invalid element weight error.'
             return response, 400
         except MissingElementError as e:
             # Where the alloy is missing elements we expect to always be
@@ -133,49 +185,71 @@ class LastSimulation(Resource):
             # All other validation errors
             response['error'] = str(e)
             response['message'] = 'Model schema validation error.'
-            log_message = {'message': response['message'], 'error': str(e)}
-            logger.exception(log_message)
             apm.capture_exception()
             return response, 400
 
-        response['data'] = {}
+        # Response data to send back.
+        data = {}
         # Passing all the validations built into the models so we can save
         if post_configs and valid_configs:
             user.last_configuration = valid_configs
-            response['data']['last_configuration'] = (
-                user.last_configuration.to_dict()
-            )
+            data.update({'last_configuration': valid_configs})
         else:
-            user.last_configuration = None
+            user.last_configuration = {}
 
         if post_alloy_store and valid_store:
             user.last_alloy_store = valid_store
-            response['data']['last_alloy_store'] = (
-                user.last_alloy_store.to_dict()
-            )
+            data.update({'last_alloy_store': valid_configs})
         else:
-            user.last_alloy_store = None
+            user.last_alloy_store = {}
 
         if post_results and valid_results:
             user.last_simulation_results = valid_results
-            response['data']['last_simulation_results'] = (
-                user.last_simulation_results.to_dict()
-            )
+            data.update({'last_simulation_results': valid_results})
         else:
-            user.last_simulation_results = None
+            user.last_simulation_results = {}
 
         if post_invalid_fields:
             user.last_simulation_invalid_fields = post_invalid_fields
-            response['data']['last_simulation_invalid_fields'] = (
-                user.last_simulation_invalid_fields
+            data.update(
+                {'last_simulation_invalid_fields': post_invalid_fields}
             )
         else:
-            user.last_simulation_invalid_fields = None
+            user.last_simulation_invalid_fields = {}
 
-        user.save()
+        if valid_results or is_valid:
+            # If we're told that the configurations are valid or they have a
+            # set of results, we set that in our storage of the data.
+            # Note: a set of results means the data is valid for a simulation
+            # run so therefore it is valid in that sense.
+            valid_configs['is_valid'] = is_valid
 
-        response['status'] = 'success'
-        response['message'] = 'Saved Last Simulation Data.'
+        try:
+            user.save()
+        except MEValidationError as e:
+            # This is just to check that our schema validation matches
+            # everything expected with Mongoengine, however, this is strictly
+            # to fail gracefully as the mongoengine model uses
+            # `mongoengine.DictField` which has no requirement for validation.
+            response['message'] = 'Mongoengine Validation error.'
+            logger.exception(
+                {
+                    'error': e.message,
+                    'message': response['message']
+                }
+            )
+            apm.capture_exception()
+            return response, 500
+
+        # Update the final response dict with everything for a successful
+        # POST method.
+        response.update(
+            {
+                'message': 'Saved Last Simulation Data.',
+                'data': data,
+                'status': 'success'
+            }
+        )
         return response, 201
 
     # noinspection PyMethodMayBeStatic
@@ -196,48 +270,51 @@ class LastSimulation(Resource):
             response['message'] = (
                 'User does not have a last configurations or alloy store.'
             )
-            logger.info(response['message'])
-            apm.capture_message(response['message'])
             return response, 404
 
-        response['data'] = {}
+        # Build up our response data as we go.
+        data = {}
 
         session_store = SimSessionService().load_session()
 
         if not user.last_configuration:
             session_store['configurations'] = None
         else:
-            session_store['configurations'] = user.last_configuration.to_dict()
-            response['data']['last_configurations'] = (
-                user.last_configuration.to_dict()
+            session_store['configurations'] = user.last_configuration
+            is_valid = user.last_configuration['is_valid']
+            data.update(
+                {
+                    'configurations': user.last_configuration,
+                    'is_valid': is_valid
+                }
             )
 
         if not user.last_alloy_store:
             session_store['alloy_store'] = None
         else:
-            session_store['alloy_store'] = user.last_alloy_store.to_dict()
-            response['data']['last_alloy_store'
-                             ] = user.last_alloy_store.to_dict()
+            session_store['alloy_store'] = user.last_alloy_store
+            data.update({'last_alloy_store': user.last_alloy_store})
 
         if not user.last_simulation_results:
             # We don't need to do anything here so we can just keep going
             pass
         else:
-            session_store['simulation_results'] = (
-                user.last_simulation_results.to_dict()
-            )
-            response['data']['last_simulation_results'] = (
-                user.last_simulation_results.to_dict()
+            session_store['simulation_results'] = user.last_simulation_results
+            data.update(
+                {'last_simulation_results': user.last_simulation_results}
             )
 
         if user.last_simulation_invalid_fields:
-            response['data']['last_simulation_invalid_fields'] = (
-                user.last_simulation_invalid_fields
+            data.update(
+                {
+                    'last_simulation_invalid_fields':
+                    user.last_simulation_invalid_fields
+                }
             )
 
         SimSessionService().save_session(session_store)
 
-        response['status'] = 'success'
+        response.update({'status': 'success', 'data': data})
         return response, 200
 
 

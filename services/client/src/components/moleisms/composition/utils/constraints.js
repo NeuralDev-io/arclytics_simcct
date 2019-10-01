4c1940e152b6d2103ac3efcdf60f910757fbbf6b
dalton <duong.daltonle@gmail.com>
2019-10-01 18:43:51 +1000

    Update constraints for carbon

--- services/client/src/components/moleisms/composition/utils/constraints.js
+++ services/client/src/components/moleisms/composition/utils/constraints.js
@@ -1,16 +1,27 @@
+const weightConstraints = [
+  {
+    check: val => val.trim() !== '',
+    message: 'Can\'t be empty',
+  },
+  {
+    check: val => !isNaN(val),
+    message: 'Must be a number',
+  },
+  {
+    check: val => parseFloat(val) >= 0,
+    message: 'Can\'t be negative',
+  },
+]
+
 export const constraints = {
   weight: [
+    ...weightConstraints,
+  ],
+  carbon: [
+    ...weightConstraints,
     {
-      check: val => val.trim() !== '',
-      message: 'Can\'t be empty',
-    },
-    {
-      check: val => !isNaN(val),
-      message: 'Must be a number',
-    },
-    {
-      check: val => parseFloat(val) >= 0,
-      message: 'Can\'t be negative',
+      check: val => parseFloat(val) <= 0.8,
+      message: 'Carbon can\'t weigh more than 0.8',
     },
   ],
 }

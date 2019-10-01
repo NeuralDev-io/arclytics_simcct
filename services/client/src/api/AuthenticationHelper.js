32a63e6aa695925dbda8c83656b50c33f0008cdc
Arvy Salazar <ags531@gmail.com>
2019-10-01 16:52:01 +1000

    Add resend auth

--- services/client/src/api/AuthenticationHelper.js
+++ services/client/src/api/AuthenticationHelper.js
@@ -179,3 +179,31 @@ export const resetPassword = (resolve, reject, values, token) => {
       err.toString(), err.message, 'AuthenticationHelper.reset', err.stack,
     ))
 }
+
+export const resendVerify = (resolve, reject, email) => {
+  fetch(`${ARC_URL}/confirm/register/resend`, {
+    method: 'PUT',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+    body: JSON.stringify({
+      email: email,
+    })
+  })
+    .then(res => res.json())
+    .then((res) => {
+        if (res.status === 'success'){
+          addFlashToast({
+            message: 'An email has been successfully sent too '+ email,
+            options: { variant: 'success' },
+          }, true)
+          resolve(res.message)
+        } else {
+          reject(res)
+        }
+      }
+    )
+    .catch(err => {
+      console.log(err)
+    })
+}

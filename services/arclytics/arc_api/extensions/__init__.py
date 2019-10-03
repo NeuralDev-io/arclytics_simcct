91b17acdd984205d82a5f058861bc02ac4a653b9
Andrew Che <andrew@neuraldev.io>
2019-10-03 15:26:43 +1000

    Adding additional pandas and flask_redis dependencies

--- services/arclytics/arc_api/extensions/__init__.py
+++ services/arclytics/arc_api/extensions/__init__.py
@@ -20,18 +20,21 @@ context of the app.
 from flask_restful import Api
 from flask_bcrypt import Bcrypt
 from flask_caching import Cache
+from flask_redis import FlaskRedis
 from elasticapm.contrib.flask import ElasticAPM
 
 apm = ElasticAPM()
 api = Api()
 bcrypt = Bcrypt()
 cache = Cache()
+redis_client = FlaskRedis()
 
 # Used by the __init__ module to loop and init_app() for each element
-flask_extensions = [apm, api, bcrypt, cache]
+flask_extensions = [api, bcrypt, cache, redis_client]
 
 from .utilities import JSONEncoder, API_TOKEN_NAME
 
 __all__ = [
-    'apm', 'api', 'bcrypt', 'JSONEncoder', 'flask_extensions', 'API_TOKEN_NAME'
+    'apm', 'api', 'bcrypt', 'JSONEncoder', 'flask_extensions', 'API_TOKEN_NAME',
+    'redis_client', 'cache'
 ]

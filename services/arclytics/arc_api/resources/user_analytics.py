ecfc0f92ed2c0359a67d30098260151ac0dcf619
Andrew Che <andrew@neuraldev.io>
2019-10-03 15:26:57 +1000

    Getting all keys for a hash pattern from Redis for session

--- services/arclytics/arc_api/resources/user_analytics.py
+++ services/arclytics/arc_api/resources/user_analytics.py
@@ -18,15 +18,25 @@ __date__ = '2019.10.02'
 """
 
 from flask import Blueprint, request
+from flask import current_app as app
 from flask_restful import Resource
 
-from arc_api.extensions import api
+from arc_api.extensions import api, redis_client
+from arc_api.middleware import authorize_admin_cookie_restful
+from arc_logging import AppLogger
 
 user_analytics_blueprint = Blueprint('user_analytics', __name__)
+logger = AppLogger(__name__)
 
 
+# noinspection PyMethodMayBeStatic
 class UserLoginData(Resource):
+    # method_decorators = {'get': [authorize_admin_cookie_restful]}
+
     def get(self):
-        pass
+        keys = redis_client.keys(pattern=u'session*')
+        logger.debug(keys)
+        return {'keys': len(keys)}, 200
+
 
 api.add_resource(UserLoginData, '/v1/arc/users/login/live')

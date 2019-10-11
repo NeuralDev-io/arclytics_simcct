8ea8b3e0a78bd82354105af9ffccdbcf30044dea
Andrew Che <andrew@neuraldev.io>
2019-10-11 12:45:59 +1100

    Fixing Ae3 Equilibrium endpoint

--- services/simcct/sim_api/resources/simulation.py
+++ services/simcct/sim_api/resources/simulation.py
@@ -34,9 +34,8 @@ from sim_api.extensions.utilities import (
 )
 from sim_api.middleware import authenticate_user_cookie_restful
 from sim_api.schemas import AlloyStoreSchema, ConfigurationsSchema
-from sim_api.extensions.SimSession import SimSessionService
 from simulation.phasesimulation import PhaseSimulation
-from simulation.simconfiguration import SimConfiguration
+from simulation.simconfiguration import SimConfiguration as SimConfig
 from simulation.utilities import ConfigurationError, SimulationError
 
 logger = AppLogger(__name__)
@@ -155,7 +154,7 @@ class Simulation(Resource):
         # structured `numpy.ndarray` that the simulation package users.
         # It also prepares a Integral matrix for the simulation algorithm.
         try:
-            sim_configs = SimConfiguration(
+            sim_configs = SimConfig(
                 configs=valid_configs, compositions=alloy['compositions']
             )
         except ValueError as e:
@@ -283,86 +282,130 @@ class Ae3Equilibrium(Resource):
     method_decorators = {'get': [authenticate_user_cookie_restful]}
 
     def get(self, _):
+        """Run an Ae3 Equilibrium simulation using the para-equilibrium
+        methodology to predict the Ae3 values with increasing carbon content
+        to find the intercept with Ae1 (from simplified method) to determine
+        the eutectic carbon content wt%.
+
+        Args:
+            _: an instance of a `models.User` from the middleware not used.
+
+        Returns:
+            A valid HTTP Response with the results and a status code.
+        """
         response = {'status': 'fail'}
 
-        # First we need to make sure they logged in and are in a current session
-        session_store = SimSessionService().load_session()
+        post_data = request.get_json()
 
-        if isinstance(session_store, str):
-            response['message'] = session_store
-            logger.error(response['message'])
-            apm.capture_message(response['message'])
-            return response, 500
-
-        session_configs = session_store.get('configurations')
-        if not session_configs:
-            response['message'] = 'No previous session configurations was set.'
-            return response, 404
-
-        configs = ConfigurationsSchema().load(session_configs)
-
-        # By default, the session alloy store is single and parent but the
-        # parent alloy is set to none.
-        sess_alloy_store = session_store.get('alloy_store')
-        if (
-            not sess_alloy_store['alloys']['parent']
-            and not sess_alloy_store['alloys']['weld']
-            and not sess_alloy_store['alloys']['mix']
-        ) or not sess_alloy_store:
-            response['message'] = 'No previous session alloy was set.'
-            return response, 404
-
-        alloy_store = AlloyStoreSchema().load(sess_alloy_store)
-
-        # We need to validate ae1, ae3, ms, and bs temperatures because if we do
-        # the calculations for CCT/TTT will cause many problems.
-        if not configs['ae1_temp'] > 0.0 or not configs['ae3_temp'] > 0.0:
-            response['message'] = 'Ae1 and Ae3 value cannot be less than 0.0.'
+        if not post_data:
             return response, 400
 
-        if not configs['ms_temp'] > 0.0 or not configs['bs_temp'] > 0.0:
-            response['message'] = 'MS and BS value cannot be less than 0.0.'
+        alloy_store = post_data.get('alloy_store', None)
+        ae1_temp = post_data.get('ae1_temp', None)
+
+        # ===== # Validation Checks of POST BODY # ===== #
+        if not alloy_store:
+            response.update({'message': 'Alloy store required.'})
+            return response, 400
+
+        if not alloy_store.get('alloy_option') in {'single', 'mix'}:
+            response.update(
+                {'message': 'Alloy option must be one of ["single" | "mix"]'}
+            )
+            return response, 400
+
+        if not ae1_temp:
+            response.update({'message': 'Ae1 temperature required.'})
+            return response, 400
+
+        if ae1_temp <= 0:
+            response.update({'message': 'Ae1 temperature must be more than 0.'})
+            return response, 400
+
+        # We need to validate the Alloy Store that matches our expected schema
+        try:
+            valid_store = AlloyStoreSchema().load(alloy_store)
+        except ElementSymbolInvalid as e:
+            # Where the symbol used for the element is not valid meaning it
+            # does not exist in a Periodic Table.
+            response['error'] = str(e)
+            response['message'] = 'Invalid element symbol error.'
+            return response, 400
+        except ElementInvalid as e:
+            # If no "symbol" or "weight" passed as an Element object.
+            response['error'] = str(e)
+            response['message'] = 'Invalid element error.'
+            return response, 400
+        except ElementWeightInvalid as e:
+            # If `C` weight is more than 0.8
+            response['error'] = str(e)
+            response['message'] = 'Invalid element weight error.'
+            return response, 400
+        except MissingElementError as e:
+            # Where the alloy is missing elements we expect to always be
+            # available as they are required downstream in the algorithm.
+            response['error'] = str(e)
+            response['message'] = 'Missing element error.'
+            return response, 400
+        except DuplicateElementError as e:
+            # One of the alloys contains two or more elements with the same
+            # chemical symbol.
+            response['error'] = str(e)
+            response['message'] = 'Alloy contains a duplicate element.'
+            return response, 400
+        except ValidationError as e:
+            # All other validation errors
+            response['error'] = str(e.messages)
+            response['message'] = 'Model schema validation error.'
+            apm.capture_exception()
             return response, 400
 
         alloy = None
         if alloy_store.get('alloy_option') == 'single':
             alloy = alloy_store['alloys']['parent']
-        else:
+        elif valid_store['alloy_option'] == 'mix':
             # DECISION:
             # We will not implement this as it adds too much complexity to
             # the logical path of the system state. This was not a core
             # requirement and Dr. Bendeich often said he did not want this
             # implemented at all.
-            pass
+            response.update({'message': 'Mix option not allowed.'})
+            return response, 400
 
-        # Create a configuration object based off the session config and comp
-        # so we can perform ae3 equilibrium calculations on it.
-        sim_configs = SimConfiguration(
-            configs=configs, compositions=alloy['compositions']
-        )
+        try:
+            comp_np_arr = SimConfig.get_compositions(alloy['compositions'])
+        except ConfigurationError as e:
+            response.update({'message': str(e), 'error': str(e)})
+            logger.exception(response['message'], exc_info=True)
+            apm.capture_exception()
+            return response, 500
 
         # Call the xfe_method2 function to calculate the ae3 equilibrium
         # graph data.
         try:
             # xfe is ferrite_phase_frac and ceut is eutectic_composition_carbon
-            results_plot, results_mat, xfe, ceut = \
-                sim_configs.xfe_method2(
-                    comp_list=alloy['compositions'], ae1=configs['ae1_temp'],
-                    plot=True
-                )
+            results_plot, xfe, ceut = SimConfig.xfe_method2(
+                comp=comp_np_arr,
+                ae1=ae1_temp,
+                plot=True
+            )
         except ZeroDivisionError as e:
-            response['error'] = str(e)
-            response['message'] = 'Zero Division Error.'
-            response['configs'] = sim_configs.__dict__
+            response.update(
+                {
+                    'message': 'Zero Division Error.',
+                    'error': str(e)
+                }
+            )
             logger.exception(response['message'], exc_info=True)
             apm.capture_exception()
             return response, 500
-        # TODO(davidmatthews1004@gmail.com) Find if its possible for any other
-        #  kind of exceptions to occur and handle them appropriately.
         except Exception as e:
-            response['error'] = str(e)
-            response['message'] = 'Exception.'
-            response['configs'] = sim_configs.__dict__
+            response.update(
+                {
+                    'message': 'General exception in Xfe method 2.',
+                    'error': str(e)
+                }
+            )
             logger.exception(response['message'], exc_info=True)
             apm.capture_exception()
             return response, 500
@@ -372,7 +415,6 @@ class Ae3Equilibrium(Resource):
                 'ferrite_phase_frac': xfe,
                 'eutectic_composition_carbon': ceut,
                 'results_plot': results_plot
-                # 'results_mat': results_mat.tolist()
             }
         # Not sure if this is possible for the ae3 equilibrium results but
         # putting it in just in case.
@@ -386,9 +428,8 @@ class Ae3Equilibrium(Resource):
             apm.capture_exception()
             return response, 500
 
-        response['status'] = 'success'
-        response['data'] = data
-        return response, 200
+        # Just overwrite the response rather than update
+        return {'status': 'success', 'data': data}, 200
 
 
 api.add_resource(Simulation, '/v1/sim/simulate')

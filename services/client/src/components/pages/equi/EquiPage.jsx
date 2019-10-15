b1b6e7a75bb759233b2fd36d061917a062887a84
dalton <duong.daltonle@gmail.com>
2019-10-15 18:26:26 +1100

    Edit styling for equi chart

--- services/client/src/components/pages/equi/EquiPage.jsx
+++ services/client/src/components/pages/equi/EquiPage.jsx
@@ -12,25 +12,60 @@
  */
 import React from 'react'
 import PropTypes from 'prop-types'
+import { connect } from 'react-redux'
 import AppBar from '../../moleisms/appbar'
 import Equilibrium from '../../moleisms/charts/Equilibrium'
 
 import styles from './EquiPage.module.scss'
 
-const EquiPage = ({ history }) => (
+const EquiPage = ({
+  history,
+  xfe,
+  ceut,
+  cf,
+}) => (
   <React.Fragment>
     <AppBar active="equilibrium" redirect={history.push} isAdmin isAuthenticated />
     <div className={styles.main}>
-      <h4>Ae3 equilibrium</h4>
-      <div className={styles.chart}>
-        <Equilibrium />
-      </div>
+      <section>
+        <h4>Ae3 equilibrium</h4>
+        <div className={styles.chart}>
+          <Equilibrium />
+        </div>
+      </section>
+      <section className={styles.configs}>
+        <h4>Configurations</h4>
+        <div className={styles.configs}>
+          <div>
+            <h6 className={styles.configLabel}>Xfe</h6>
+            <span>{xfe}</span>
+          </div>
+          <div>
+            <h6 className={styles.configLabel}>Ceut</h6>
+            <span>{ceut}</span>
+          </div>
+          <div>
+            <h6 className={styles.configLabel}>Cf</h6>
+            <span>{cf}</span>
+          </div>
+        </div>
+      </section>
     </div>
   </React.Fragment>
 )
 
 EquiPage.propTypes = {
   history: PropTypes.shape({ push: PropTypes.func.isRequired }).isRequired,
+  // given by connect()
+  xfe: PropTypes.number.isRequired,
+  ceut: PropTypes.number.isRequired,
+  cf: PropTypes.number.isRequired,
 }
 
-export default EquiPage
+const mapStateToProps = (state) => ({
+  xfe: state.equi.xfe,
+  ceut: state.equi.ceut,
+  cf: state.equi.cf,
+})
+
+export default connect(mapStateToProps, {})(EquiPage)

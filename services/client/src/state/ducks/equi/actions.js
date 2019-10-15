03c4f6652500bf6adaaf622997146683bedf024e
dalton <duong.daltonle@gmail.com>
2019-10-15 15:53:48 +1100

    Fix equilibrium chart

--- services/client/src/state/ducks/equi/actions.js
+++ services/client/src/state/ducks/equi/actions.js
@@ -59,7 +59,6 @@ export const getEquilibriumValues = () => (dispatch, getState) => {
     }),
   })
     .then((res) => {
-      console.log(res)
       if (res.status !== 200) {
         return {
           status: 'fail',

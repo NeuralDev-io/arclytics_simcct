938fdd7c3ac526ee8c4544f74467652671c5ea4e
Andrew Che <andrew@neuraldev.io>
2019-10-16 12:58:04 +1100

    Adding an extra card

--- services/client/src/components/moleisms/analytics-app/AppAnalytics.jsx
+++ services/client/src/components/moleisms/analytics-app/AppAnalytics.jsx
@@ -9,12 +9,13 @@
  */
 import React, { Component } from 'react'
 import PropTypes from 'prop-types'
-import { logError, logInfo } from '../../../api/LoggingHelper'
-import { getColor } from '../../../utils/theming'
-import Card from '../../elements/card'
 import DatabaseIcon from 'react-feather/dist/icons/database'
 import PlayCircleIcon from 'react-feather/dist/icons/play-circle'
 import StarIcon from 'react-feather/dist/icons/star'
+import CodeIcon from 'react-feather/dist/icons/code'
+import { logError, logInfo } from '../../../api/LoggingHelper'
+import { getColor } from '../../../utils/theming'
+import Card from '../../elements/card'
 import LiveLoginTimeSeries from '../charts/LiveLoginTimeSeries'
 
 import styles from './AppAnalytics.module.scss'
@@ -25,7 +26,7 @@ class AppAnalytics extends Component {
   constructor(props) {
     super(props)
     this.state = {
-      statsData: undefined
+      statsData: undefined,
     }
   }
 
@@ -38,15 +39,14 @@ class AppAnalytics extends Component {
       this.setState({ statsData: res.data })
     })
       .catch((err) => logError(
-          err.toString(),
-          err.message,
-          'UsersAnalytics.getGeneralStatsAnalytics',
-          err.stack
+        err.toString(),
+        err.message,
+        'UsersAnalytics.getGeneralStatsAnalytics',
+        err.stack,
       ))
   }
 
   render() {
-
     const { statsData } = this.state
 
     return (
@@ -58,23 +58,30 @@ class AppAnalytics extends Component {
 
           <Card className={styles.generalDataCard}>
             <PlayCircleIcon color={getColor('--arc500')} size={36} />
-            <h5>{(statsData !== undefined) ? statsData.count.simulations : "0"}</h5>
+            <h5>{(statsData !== undefined) ? statsData.count.simulations : '0'}</h5>
             <p>Run simulation</p>
           </Card>
 
           <Card className={styles.generalDataCard}>
             <DatabaseIcon color={getColor('--arc500')} size={36} />
-            <h5>{(statsData !== undefined) ? statsData.count.global_alloys : "0"}</h5>
+            <h5>{(statsData !== undefined) ? statsData.count.global_alloys : '0'}</h5>
             <p>Global alloys</p>
           </Card>
 
           <Card className={styles.generalDataCard}>
             <StarIcon color={getColor('--arc500')} size={36} />
-            <h5>{
-              (statsData !== undefined) ? roundTo(parseFloat(statsData.average.ratings), 2) : "0"
+            <h5>
+              {
+              (statsData !== undefined) ? roundTo(parseFloat(statsData.average.ratings), 2) : '0'
             }
             </h5>
             <p>Ratings</p>
+          </Card>
+
+          <Card className={styles.generalDataCard}>
+            <CodeIcon color={getColor('--arc500')} size={36} />
+            <h5>0</h5>
+            <p>More stuff</p>
           </Card>
 
 
@@ -83,9 +90,7 @@ class AppAnalytics extends Component {
         <h5>Users currently logged in</h5>
         <div className={styles.chart}>
           <Card className={styles.liveLoginCard}>
-            {/*<div className={styles.liveLoginChart}>*/}
-              <LiveLoginTimeSeries />
-            {/*</div>*/}
+            <LiveLoginTimeSeries />
           </Card>
         </div>
       </div>

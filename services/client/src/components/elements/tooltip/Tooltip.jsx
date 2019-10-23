a53cd871e71b7c878b42c8248032d92918f74d10
dalton <duong.daltonle@gmail.com>
2019-10-23 20:15:26 +1100

    aye

--- services/client/src/components/elements/tooltip/Tooltip.jsx
+++ services/client/src/components/elements/tooltip/Tooltip.jsx
@@ -29,6 +29,62 @@ class Tooltip extends Component {
     this.space = props.space || 16
   }
 
+  // calculate position of the tooltip after it renders
+  componentDidMount = () => {
+    const { position } = this.props
+    // this style object will be passed as the tooltip's 'style' prop
+    const style = { width: this.width }
+    // where on the screen is the target
+    const dimensions = this.el.getBoundingClientRect()
+
+    // position the tooltip based on whether we should display it horizontally
+    // or vertically
+    if (position === 'vertical') {
+      // center align the tooltip by taking both the target and tooltip widths into account
+      style.left = dimensions.left + dimensions.width / 2 - this.width / 2
+      // make sure it doesn't poke off the left side of the page
+      style.left = Math.max(this.space, style.left)
+      style.left = Math.min(
+        style.left,
+        document.body.clientWidth - this.width - this.space,
+      ) // or off the right
+
+      if (dimensions.top < window.innerHeight / 2) {
+        // the top half of the page
+        // when on the top half of the page, position the top of the tooltip
+        // just below the target (it will stretch downwards)
+        style.top = dimensions.top + dimensions.height + this.space
+      } else {
+        // when on the bottom half, set the bottom of the tooltip
+        // just above the top of the target (it will stretch upwards)
+        style.bottom = window.innerHeight - dimensions.top + this.space
+      }
+    }
+    if (position === 'horizontal') {
+      // center align the tooltip by taking both the target and tooltip heights into account
+      style.top = dimensions.top + dimensions.height / 2 - this.tooltipEl.clientHeight / 2
+      // make sure it doesn't poke off the top side of the page
+      style.top = Math.max(this.space, style.top)
+      style.top = Math.min(
+        style.top,
+        window.innerHeight - this.tooltipEl.clientHeight - this.space,
+      ) // or off the bottom
+
+      if (dimensions.left < window.innerWidth / 2) {
+        // the top half of the page
+        style.left = dimensions.left + dimensions.width + this.space
+      } else {
+        style.right = window.innerWidth - dimensions.left + this.space
+      }
+    }
+
+    window.onscroll = this.hideTooltip
+
+    this.setState({
+      style,
+    })
+  }
+
   showTooltip = () => {
     const { position } = this.props
     // this style object will be passed as the tooltip's 'style' prop
